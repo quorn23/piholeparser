82e670f39c6cbed25a269ee12e0546e5e4d77dc1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-07 19:27:20 -0400

    Update bigparser.sh

--- bigparser.sh
+++ bigparser.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-for source in `cat /etc/piholeparser/lists/$NAMEOFLIST.lst`; do
+FILES=/etc/piholeparser/lists/*
+
+for f in $FILES
+for source in `cat $f`; 
+do
     echo $source;
-    sudo curl --silent $source >> /etc/piholeparser/temp/"$NAMEOFLIST"ads.txt
-    echo -e "\t`wc -l /etc/piholeparser/temp/"$NAMEOFLIST"ads.txt | cut -d " " -f 1` lines downloaded"
+    sudo curl --silent $source >> /etc/piholeparser/temp/"$f"ads.txt
+    echo -e "\t`wc -l /etc/piholeparser/temp/"$f"ads.txt | cut -d " " -f 1` lines downloaded"
 done
 
+
 echo -e "\nFiltering non-url content..."
-sudo perl /etc/piholeparser/parser.pl /etc/piholeparser/temp/"$NAMEOFLIST"ads.txt > /etc/piholeparser/temp/"$NAMEOFLIST"ads_parsed.txt
-sudo rm /etc/piholeparser/temp/"$NAMEOFLIST"ads.txt
-echo -e "\t`wc -l /etc/piholeparser/temp/"$NAMEOFLIST"ads_parsed.txt | cut -d " " -f 1` lines after parsing"
+sudo perl /etc/piholeparser/parser.pl /etc/piholeparser/temp/"$f"ads.txt > /etc/piholeparser/temp/"$f"ads_parsed.txt
+sudo rm /etc/piholeparser/temp/"$f"ads.txt
+echo -e "\t`wc -l /etc/piholeparser/temp/"$f"ads_parsed.txt | cut -d " " -f 1` lines after parsing"
 
 echo -e "\nRemoving duplicates..."
-sort -u /etc/piholeparser/temp/"$NAMEOFLIST"ads_parsed.txt > /etc/piholeparser/temp/"$NAMEOFLIST"ads_unique.txt
-sudo rm /etc/piholeparser/temp/"$NAMEOFLIST"ads_parsed.txt
-echo -e "\t`wc -l /etc/piholeparser/temp/"$NAMEOFLIST"ads_unique.txt | cut -d " " -f 1` lines after deduping"
+sort -u /etc/piholeparser/temp/"$f"ads_parsed.txt > /etc/piholeparser/temp/"$f"ads_unique.txt
+sudo rm /etc/piholeparser/temp/"$f"ads_parsed.txt
+echo -e "\t`wc -l /etc/piholeparser/temp/"$f"ads_unique.txt | cut -d " " -f 1` lines after deduping"
 
-sudo cat /etc/piholeparser/temp/"$NAMEOFLIST"ads_unique.txt >> /etc/piholeparser/filtered/"$NAMEOFLIST"filtered.txt
-sudo rm /etc/piholeparser/temp/"$NAMEOFLIST"ads_unique.txt
+sudo cat /etc/piholeparser/temp/"$f"ads_unique.txt >> /etc/piholeparser/filtered/"$f"filtered.txt
+sudo rm /etc/piholeparser/temp/"$f"ads_unique.txt
+done
