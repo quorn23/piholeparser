98a9540b69677baf2c9478b5363d8d1e9dab45a6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-07 19:42:09 -0400

    Update bigparser.sh

--- bigparser.sh
+++ bigparser.sh
@@ -7,21 +7,21 @@ do
 for source in `cat $f`; 
 do
     echo $source;
-    sudo curl --silent $source >> /etc/piholeparser/temp/"$f"ads.txt
-    echo -e "\t`wc -l /etc/piholeparser/temp/"$f"ads.txt | cut -d " " -f 1` lines downloaded"
+    sudo curl --silent $source >> "$f"ads.txt
+    echo -e "\t`wc -l "$f"ads.txt | cut -d " " -f 1` lines downloaded"
 done
 
 
 echo -e "\nFiltering non-url content..."
-sudo perl /etc/piholeparser/parser.pl /etc/piholeparser/temp/"$f"ads.txt > /etc/piholeparser/temp/"$f"ads_parsed.txt
-sudo rm /etc/piholeparser/temp/"$f"ads.txt
-echo -e "\t`wc -l /etc/piholeparser/temp/"$f"ads_parsed.txt | cut -d " " -f 1` lines after parsing"
+sudo perl /etc/piholeparser/parser.pl "$f"ads.txt > "$f"ads_parsed.txt
+sudo rm "$f"ads.txt
+echo -e "\t`wc -l "$f"ads_parsed.txt | cut -d " " -f 1` lines after parsing"
 
 echo -e "\nRemoving duplicates..."
-sort -u /etc/piholeparser/temp/"$f"ads_parsed.txt > /etc/piholeparser/temp/"$f"ads_unique.txt
-sudo rm /etc/piholeparser/temp/"$f"ads_parsed.txt
-echo -e "\t`wc -l /etc/piholeparser/temp/"$f"ads_unique.txt | cut -d " " -f 1` lines after deduping"
+sort -u "$f"ads_parsed.txt > "$f"ads_unique.txt
+sudo rm "$f"ads_parsed.txt
+echo -e "\t`wc -l "$f"ads_unique.txt | cut -d " " -f 1` lines after deduping"
 
-sudo cat /etc/piholeparser/temp/"$f"ads_unique.txt >> /etc/piholeparser/filtered/"$f"filtered.txt
-sudo rm /etc/piholeparser/temp/"$f"ads_unique.txt
+sudo cat "$f"ads_unique.txt >> "$f"filtered.txt
+sudo rm "$f"ads_unique.txt
 done
