14d35870255590d8805478a433565ca52da2b696
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-07 15:35:14 -0400

    Create runparser.sh

---
+++ runparser.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+for source in `cat lists.lst`; do
+    echo $source;
+    sudo curl --silent $source >> ads.txt
+    echo -e "\t`wc -l ads.txt | cut -d " " -f 1` lines downloaded"
+done
+
+echo -e "\nFiltering non-url content..."
+sudo perl parser.pl ads.txt > ads_parsed.txt
+sudo rm ads.txt
+echo -e "\t`wc -l ads_parsed.txt | cut -d " " -f 1` lines after parsing"
+
+echo -e "\nRemoving duplicates..."
+sort -u ads_parsed.txt > ads_unique.txt
+sudo rm ads_parsed.txt
+echo -e "\t`wc -l ads_unique.txt | cut -d " " -f 1` lines after deduping"
+
+sudo cat ads_unique.txt >> filteredlists.txt
+sudo rm ads_unique.txt
