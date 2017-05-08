9c195e643c99a10eeebecbcae518b2f330bcdcfa
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-08 08:26:51 -0400

    Update bigparser.sh

--- bigparser.sh
+++ bigparser.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 sudo rm -r /etc/piholeparser/lists/*.txt
 sudo rm -r /etc/piholeparser/parsed/*.txt
-FILES=/etc/piholeparser/lists/*.lst
+FILES=*.lst
 
 for f in $FILES
 do
 
-#for source in `cat /etc/piholeparser/lists/$f`; 
-#do
+for source in `cat /etc/piholeparser/lists/$f`; 
+do
     echo $f;
-    #sudo curl --silent $source >> /etc/piholeparser/lists/"$f"ads.txt
+    sudo curl --silent $source >> /etc/piholeparser/lists/"$f"ads.txt
     sudo curl --silent $f >> "$f"ads.txt
     echo -e "\t`wc -l "$f"ads.txt | cut -d " " -f 1` lines downloaded"
 done
-#done
 
-#echo -e "\nFiltering non-url content..."
-#sudo perl /etc/piholeparser/parser.pl /etc/piholeparser/lists/"$f"ads.txt > /etc/piholeparser/lists/"$f"ads_parsed.txt
-#sudo rm /etc/piholeparser/lists/"$f"ads.txt
-#echo -e "\t`wc -l /etc/piholeparser/lists/"$f"ads_parsed.txt | cut -d " " -f 1` lines after parsing"
 
-#echo -e "\nRemoving duplicates..."
-#sort -u /etc/piholeparser/lists/"$f"ads_parsed.txt > /etc/piholeparser/lists/"$f"ads_unique.txt
-#sudo rm /etc/piholeparser/lists/"$f"ads_parsed.txt
-#echo -e "\t`wc -l /etc/piholeparser/lists/"$f"ads_unique.txt | cut -d " " -f 1` lines after deduping"
+echo -e "\nFiltering non-url content..."
+sudo perl /etc/piholeparser/parser.pl "$f"ads.txt > "$f"ads_parsed.txt
+sudo rm "$f"ads.txt
+echo -e "\t`wc -l "$f"ads_parsed.txt | cut -d " " -f 1` lines after parsing"
 
-#sudo cat /etc/piholeparser/lists/"$f"ads_unique.txt >> /etc/piholeparser/parsed/"$f".txt
-#sudo rm /etc/piholeparser/lists/"$f"ads_unique.txt
+echo -e "\nRemoving duplicates..."
+sort -u "$f"ads_parsed.txt > "$f"ads_unique.txt
+sudo rm "$f"ads_parsed.txt
+echo -e "\t`wc -l "$f"ads_unique.txt | cut -d " " -f 1` lines after deduping"
+
+sudo cat "$f"ads_unique.txt >> /etc/piholeparser/parsed/"$f".txt
+sudo rm "$f"ads_unique.txt
+done
