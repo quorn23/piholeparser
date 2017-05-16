7707cbc2c17fc9547058b76bbc8e30db3d63962c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-16 15:50:28 -0400

    Update advancedparser.sh

--- advancedparser.sh
+++ advancedparser.sh
@@ -13,24 +13,24 @@ do
 echo ""
 echo $f;
 echo $source;
-sudo curl --silent $source >> "$f"ads.txt
-echo -e "\t`wc -l "$f"ads.txt | cut -d " " -f 1` lines downloaded"
+sudo curl --silent $source >> "$f".ads.txt
+echo -e "\t`wc -l "$f".ads.txt | cut -d " " -f 1` lines downloaded"
 done
 
 ## Filter
 echo -e "\nFiltering non-url content..."
-sudo perl /etc/piholeparser/parser.pl "$f"ads.txt > "$f"ads_parsed.txt
-sudo mv "$f"ads.txt /etc/piholeparser/mirroredlists/
-#sudo rm "$f"ads.txt
-echo -e "\t`wc -l "$f"ads_parsed.txt | cut -d " " -f 1` lines after parsing"
+sudo perl /etc/piholeparser/parser.pl "$f".ads.txt > "$f".ads_parsed.txt
+sudo mv "$f".ads.txt /etc/piholeparser/mirroredlists/
+#sudo rm "$f".ads.txt
+echo -e "\t`wc -l "$f".ads_parsed.txt | cut -d " " -f 1` lines after parsing"
 
 ## Duplicate Removal
 echo -e "\nRemoving duplicates..."
-sort -u "$f"ads_parsed.txt > "$f"ads_unique.txt
-sudo rm "$f"ads_parsed.txt
-echo -e "\t`wc -l "$f"ads_unique.txt | cut -d " " -f 1` lines after deduping"
-sudo cat "$f"ads_unique.txt >> "$f".txt
-sudo rm "$f"ads_unique.txt
+sort -u "$f".ads_parsed.txt > "$f".ads_unique.txt
+sudo rm "$f".ads_parsed.txt
+echo -e "\t`wc -l "$f".ads_unique.txt | cut -d " " -f 1` lines after deduping"
+sudo cat "$f".ads_unique.txt >> "$f".txt
+sudo rm "$f".ads_unique.txt
 
 ## End File Loop
 done
