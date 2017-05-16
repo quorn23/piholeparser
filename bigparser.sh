f01c2c7a746b15ea842bf786429ce380a5b8a957
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-16 13:12:04 -0400

    Update bigparser.sh

--- bigparser.sh
+++ bigparser.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+## This is the Parsing Process
 
 ## Set File Directory
 FILES=/etc/piholeparser/lists/*.lst
 
 ## Start File Loop
-echo ""
 for f in $FILES
 do
 
 for source in `cat $f`;
 do
-    echo $source;
-    sudo curl --silent $source >> "$f"ads.txt
-    echo -e "\t`wc -l "$f"ads.txt | cut -d " " -f 1` lines downloaded"
+echo ""
+echo $source;
+sudo curl --silent $source >> "$f"ads.txt
+echo -e "\t`wc -l "$f"ads.txt | cut -d " " -f 1` lines downloaded"
 done
 
 ## Filter
@@ -31,11 +32,3 @@ sudo rm "$f"ads_unique.txt
 
 ## End File Loop
 done
-
-## Move Files, Rename, and Delete Empty Files
-mv /etc/piholeparser/lists/*.txt /etc/piholeparser/parsed/
-sudo rename "s/.lst.txt/.txt/" /etc/piholeparser/parsed/*.txt
-sudo find /etc/piholeparser/parsed/ -size 0 -delete
-
-## Unset FILES variable
-unset FILES
