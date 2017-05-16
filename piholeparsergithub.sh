e748e9ec23729c10a7b1e473e0b4cd782735e2b1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-16 15:00:24 -0400

    Update piholeparsergithub.sh

--- piholeparsergithub.sh
+++ piholeparsergithub.sh
@@ -53,11 +53,3 @@ sudo git remote set-url origin https://USERNAME:PASSWORD@github.com/deathbybanda
 sudo git add .
 sudo git commit -m "Update lists $timestamp"
 sudo git push -u origin master
-
-
-
-## Notes
-## copy this file out of the main directory and edit credentials
-##
-## Create Cron with
-# 20 0 * * * sudo bash /etc/piholeparsergithub.sh
