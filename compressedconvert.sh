150185cf79497b81c868a6533a144404e8a04aa1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-17 09:35:30 -0400

    Update compressedconvert.sh

--- compressedconvert.sh
+++ compressedconvert.sh
@@ -5,6 +5,14 @@
 ## Download, Extract, and Name List Files.                              ##
 ##########################################################################
 
+## Colors
+red='\e[1;31m%s\e[0m\n'
+green='\e[1;32m%s\e[0m\n'
+yellow='\e[1;33m%s\e[0m\n'
+blue='\e[1;34m%s\e[0m\n'
+magenta='\e[1;35m%s\e[0m\n'
+cyan='\e[1;36m%s\e[0m\n'
+
 ## Airelle's Anti-Sex Hosts
 printf "$blue"    "___________________________________________________________"
 echo ""
