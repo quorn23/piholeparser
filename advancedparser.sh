d2ab1a8586bfb4a9cc846211b39be137327413ac
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-17 09:25:53 -0400

    Update advancedparser.sh

--- advancedparser.sh
+++ advancedparser.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 ## This is the Parsing Process
 
+## Set Colors
+red='\e[1;31m%s\e[0m\n'
+green='\e[1;32m%s\e[0m\n'
+yellow='\e[1;33m%s\e[0m\n'
+blue='\e[1;34m%s\e[0m\n'
+magenta='\e[1;35m%s\e[0m\n'
+cyan='\e[1;36m%s\e[0m\n'
+
 ## Set File Directory
 FILES=/etc/piholeparser/lists/*.lst
 
@@ -9,25 +17,25 @@ for f in $FILES
 do
 
 echo ""
-echo "___________________________________________________________"
+printf "$blue"    "___________________________________________________________"
 echo ""
-echo "$f"
+printf "$green"   "Processing list from $f"
 
 for source in `cat $f`;
 do
 echo ""
-echo "$source"
+printf "$cyan"    "$source"
 sudo curl --silent $source >> "$f".ads.txt
 echo -e "\t`wc -l "$f".ads.txt | cut -d " " -f 1` lines downloaded"
 done
 
 ## Filter
-echo -e "\nFiltering non-url content..."
+printf "$yellow"  "Filtering non-url content..."
 sudo perl /etc/piholeparser/parser.pl "$f".ads.txt > "$f".ads_parsed.txt
 echo -e "\t`wc -l "$f".ads_parsed.txt | cut -d " " -f 1` lines after parsing"
 
 ## Duplicate Removal
-echo -e "\nRemoving duplicates..."
+printf "$yellow"  "Removing duplicates..."
 sort -u "$f".ads_parsed.txt > "$f".ads_unique.txt
 sudo rm "$f".ads_parsed.txt
 echo -e "\t`wc -l "$f".ads_unique.txt | cut -d " " -f 1` lines after deduping"
@@ -39,12 +47,12 @@ if
 [ -s "$f".txt ]
 then
 echo ""
-echo "File will be moved to the parsed directory."
+printf "$yellow"  "File will be moved to the parsed directory."
 sudo mv "$f".txt /etc/piholeparser/parsed/
 sudo rename "s/.lst.txt/.txt/" /etc/piholeparser/parsed/*.txt
 else
 echo ""
-echo "File Empty. It will be deleted."
+printf "$red"     "File Empty. It will be deleted."
 rm -rf "$f".txt
 fi
 
@@ -53,17 +61,17 @@ if
 test $(stat -c%s "$f".ads.txt) -ge 100000000
 then
 echo ""
-echo "File Too Large For Github. Deleting."
+printf "$red"     "File Too Large For Github. Deleting."
 sudo rm "$f".ads.txt
 else
 echo ""
-echo "Creating Mirror of Unparsed File."
+printf "$yellow"  "Creating Mirror of Unparsed File."
 sudo mv "$f".ads.txt /etc/piholeparser/mirroredlists/
 sudo rename "s/.lst.ads.txt/.txt/" /etc/piholeparser/mirroredlists/*.txt
 fi
 
 echo ""
-echo "___________________________________________________________"
+printf "$magenta" "___________________________________________________________"
 echo ""
 
 ## End File Loop
