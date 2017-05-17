044c929929326986ed065ba79196cdccc48bddc0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-16 20:45:13 -0400

    Update advancedparser.sh

--- advancedparser.sh
+++ advancedparser.sh
@@ -32,5 +32,22 @@ echo -e "\t`wc -l "$f".ads_unique.txt | cut -d " " -f 1` lines after deduping"
 sudo cat "$f".ads_unique.txt >> "$f".txt
 sudo rm "$f".ads_unique.txt
 
+## Remove Empty Files
+if 
+[ -s "$f".txt ]
+then
+echo ""
+echo "File will be moved to the parsed directory."
+sudo mv "$f".txt /etc/piholeparser/parsed/
+echo ""
+echo "File will be renamed."
+sudo rename "s/.lst.txt/.txt/" /etc/piholeparser/parsed/*.txt
+continue
+else
+echo ""
+echo "File Empty. It will be deleted."
+rm -rf "$f".txt
+fi
+
 ## End File Loop
 done
