57a0e721191f1a11dbaa760144e1f97d9f4b09ec
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-16 21:04:45 -0400

    Update mirrorlists.sh

--- mirrorlists.sh
+++ mirrorlists.sh
@@ -8,3 +8,15 @@ sudo rename "s/.lst.ads.txt/.txt/" /etc/piholeparser/mirroredlists/*.txt
 
 ## Remove Giant Mirror File
 sudo rm /etc/piholeparser/mirroredlists/1111ALLPARSEDLISTS1111.txt
+
+if 
+[ -s "$f".txt ]
+then
+echo ""
+echo ""
+
+else
+echo ""
+echo ""
+
+fi
