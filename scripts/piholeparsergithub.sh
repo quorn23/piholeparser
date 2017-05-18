17d5df0bd3734b6f8910104579472e55be831cba
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-18 13:37:59 -0400

    Update piholeparsergithub.sh

--- scripts/piholeparsergithub.sh
+++ scripts/piholeparsergithub.sh
@@ -7,12 +7,6 @@ timestamp=`date`
 ## Colors
 source /etc/piholeparser/scripts/colors.var
 
-## Dependency check
-sudo bash /etc/piholeparser/scripts/dependencycheck.sh
-
-## Clean directories to avoid collisions
-sudo bash /etc/piholeparser/scripts/collisionavoid.sh
-
 ## Make sure we are in the correct directory
 cd /etc/piholeparser
 
@@ -23,6 +17,12 @@ printf "$green"   "Updating Repository."
 sudo git pull
 printf "$magenta" "___________________________________________________________"
 echo ""
+
+## Dependency check
+sudo bash /etc/piholeparser/scripts/dependencycheck.sh
+
+## Clean directories to avoid collisions
+sudo bash /etc/piholeparser/scripts/collisionavoid.sh
 
 ## Re-Build 1111ALLPARSEDLISTS1111.lst
 sudo bash /etc/piholeparser/scripts/rebuildALLPARSEDLISTS1111.sh
