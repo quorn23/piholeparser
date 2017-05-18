1a44486cb31e05d353023b5782eb253de36dc75b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-18 10:01:15 -0400

    Update advancedparser.sh

--- scripts/advancedparser.sh
+++ scripts/advancedparser.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 ## This is the Parsing Process
 
-## Set Colors
-red='\e[1;31m%s\e[0m\n'
-green='\e[1;32m%s\e[0m\n'
-yellow='\e[1;33m%s\e[0m\n'
-blue='\e[1;34m%s\e[0m\n'
-magenta='\e[1;35m%s\e[0m\n'
-cyan='\e[1;36m%s\e[0m\n'
+echo ""
+echo "Parsing Individual Lists."
 
 ## Set File Directory
 FILES=/etc/piholeparser/lists/*.lst
@@ -78,3 +73,6 @@ echo ""
 
 ## End File Loop
 done
+
+printf "$magenta" "___________________________________________________________"
+echo ""
