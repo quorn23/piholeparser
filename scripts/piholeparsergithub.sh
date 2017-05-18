917995f847cfdd9372675edd8a33806a5c231997
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-18 09:28:59 -0400

    Update piholeparsergithub.sh

--- scripts/piholeparsergithub.sh
+++ scripts/piholeparsergithub.sh
@@ -124,21 +124,21 @@ echo ""
 printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Downloading and Extracting Compressed Lists."
-sudo bash /etc/piholeparser/compressedconvert.sh
+sudo bash /etc/piholeparser/scripts/compressedconvert.sh
 printf "$magenta" "___________________________________________________________"
 echo ""
 
 ## Parse Lists
 echo ""
 echo "Parsing Lists."
-sudo bash /etc/piholeparser/advancedparser.sh
+sudo bash /etc/piholeparser/scripts/advancedparser.sh
 printf "$magenta" "___________________________________________________________"
 echo ""
 
 ## Parse Big List
 echo ""
 echo "Parsing All Lists."
-sudo bash /etc/piholeparser/advancedparserlocal.sh
+sudo bash /etc/piholeparser/scripts/advancedparserlocal.sh
 printf "$magenta" "___________________________________________________________"
 echo ""
 sudo cp /etc/piholeparser/parsedall/*.txt /etc/piholeparser/parsed/
