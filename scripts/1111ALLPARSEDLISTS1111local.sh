4c9ace3278dfeae0392e07ceefe487ec93cf82b1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-18 11:05:46 -0400

    Update 1111ALLPARSEDLISTS1111local.sh

--- scripts/1111ALLPARSEDLISTS1111local.sh
+++ scripts/1111ALLPARSEDLISTS1111local.sh
@@ -12,7 +12,14 @@ else
 sudo mkdir /var/www/html/lists/
 fi
 
+if 
+ls /var/www/html/lists/1111ALLPARSEDLISTS1111.txt &> /dev/null; 
+then
 sudo rm /var/www/html/lists/1111ALLPARSEDLISTS1111.txt
+else
+echo ""
+fi
+
 sudo cp -p /etc/piholeparser/parsed/1111ALLPARSEDLISTS1111.txt /var/www/html/lists/1111ALLPARSEDLISTS1111.txt
 printf "$magenta" "___________________________________________________________"
 echo ""
