96dbb7d17d7791c7f462f4cc9c31735be2333d83
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-19 13:35:27 -0400

    Update updaterunpiholeparser.sh

--- scripts/updaterunpiholeparser.sh
+++ scripts/updaterunpiholeparser.sh
@@ -17,20 +17,5 @@ sudo git pull
 printf "$magenta" "___________________________________________________________"
 echo ""
 
-printf "$blue"    "___________________________________________________________"
-echo ""
-printf "$green"   "Checking Run Script to see if Outdated."
-if 
-cmp -s /etc/piholeparser/scripts/runpiholeparser.sh /etc/runpiholeparser.sh
-then
-printf "$green"   "runparser is up to date!"
-else
-printf "$red"   "runparser is outdated"
-sudo rm /etc/runpiholeparser.sh
-sudo cp /etc/piholeparser/scripts/runpiholeparser.sh /etc/runpiholeparser.sh
-fi
-printf "$magenta" "___________________________________________________________"
-echo ""
-
 ## RunParser
-sudo bash /etc/runpiholeparser.sh
+sudo bash /etc/piholeparser/scripts/runpiholeparser.sh
