27e6d5879930ba875cc8ea50078e6949f99dda3b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-19 13:08:14 -0400

    Update runpiholeparser.sh

--- scripts/runpiholeparser.sh
+++ scripts/runpiholeparser.sh
@@ -17,6 +17,7 @@ printf "$green"   "Updating Repository."
 sudo git pull
 printf "$magenta" "___________________________________________________________"
 echo ""
+echo ""
 
 ## Dependency check
 sudo bash /etc/piholeparser/scripts/dependencycheck.sh
