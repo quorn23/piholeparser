66e3e34fb6ef13129e98ceaed8f833fc0e73af8d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-19 12:11:17 -0400

    Update piholeparserinstall.sh

--- scripts/piholeparserinstall.sh
+++ scripts/piholeparserinstall.sh
@@ -3,6 +3,7 @@
 ## Colors
 source /etc/piholeparser/scripts/colors.var
 
+## Check for whiptail
 { if
 which whiptail >/dev/null;
 then
@@ -10,25 +11,46 @@ then
 else
 sudo apt-get install -y whiptail
 fi }
-{ if 
-(whiptail --title "piholeparser" --yes-button "yes" --no-button "No" --yesno "Do you want to install piholeparser?" 10 80) 
+
+## Check if already there
+{ if
+[ -d "/etc/piholeparser/" ] 
 then
-sudo git clone https://github.com/deathbybandaid/piholeparser.git /etc/piholeparser/
-{ if 
-(whiptail --title "piholeparser" --yes-button "Local Only" --no-button "I'll be uploading to Github" --yesno "What version do you plan on using" 10 80) 
+{ if
+(whiptail --title "piholeparser" --yes-button "Remove beore install" --no-button "Abort" --yesno "piholeparser is already installed?" 10 80) 
 then
-sudo cp /etc/piholeparser/scripts/piholeparserlocal.sh /etc/piholeparserlocal.sh
-(crontab -l ; echo "## piholeparser") | crontab -
-(crontab -l ; echo "20 0 * * * sudo bash /etc/piholeparserlocal.sh") | crontab -
-(crontab -l ; echo "") | crontab -
-else
-sudo cp /etc/piholeparser/scripts/piholeparsergithub.sh /etc/piholeparsergithub.sh
-(crontab -l ; echo "## piholeparser") | crontab -
-(crontab -l ; echo "20 0 * * * sudo bash /etc/piholeparsergithub.sh") | crontab -
-(crontab -l ; echo "") | crontab -
-## End of Version Installs
-fi }
-## User said no to installing
+sudo rm -r /etc/piholeparser
+sudo rm /etc/piholeparser.var
+sudo rm /etc/runpiholeparser.sh
+sudo rm /etc/piholeparserlocal.sh
+sudo rm /etc/piholeparsergithub.sh
 else
 exit
+fi }
+fi }
+
+## obvious question
+{ if
+(whiptail --title "piholeparser" --yes-button "yes" --no-button "no" --yesno "Do You want to install piholeparser?" 10 80) 
+then
+sudo git clone https://github.com/deathbybandaid/piholeparser.git /etc/piholeparser/
+sudo cp /etc/piholeparser/scripts/runpiholeparser.sh /etc/runpiholeparser.sh
+sudo cp /etc/piholeparser/scripts/piholeparser.var /etc/piholeparser.var
+else
+exit
+fi }
+
+## What version?
+{ if 
+(whiptail --title "piholeparser" --yes-button "Local Only" --no-button "I'll be uploading to Github" --yesno "What Version of piholeparser to install?" 10 80) 
+then
+sudo echo "version=local" | sudo tee --append /etc/piholeparser.var
+else
+sudo echo "version=github" | sudo tee --append /etc/piholeparser.var
+GITHUBUSERNAME=$(whiptail --inputbox "Github Username" 10 80 "" 3>&1 1>&2 2>&3)
+GITHUBPASSWORD=$(whiptail --inputbox "Github Password" 10 80 "" 3>&1 1>&2 2>&3)
+GITHUBEMAIL=$(whiptail --inputbox "Github Email Address" 10 80 "" 3>&1 1>&2 2>&3)
+sudo echo "GITHUBUSERNAME="GITHUBUSERNAME"" | sudo tee --append /etc/piholeparser.var
+sudo echo "GITHUBPASSWORD="GITHUBPASSWORD"" | sudo tee --append /etc/piholeparser.var
+sudo echo "GITHUBEMAIL="GITHUBEMAIL"" | sudo tee --append /etc/piholeparser.var
 fi }
