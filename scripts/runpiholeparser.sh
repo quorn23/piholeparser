4ece8ef15932d96b2d5b29fdca387204843585a6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-20 13:49:07 -0400

    Update runpiholeparser.sh

--- scripts/runpiholeparser.sh
+++ scripts/runpiholeparser.sh
@@ -25,31 +25,8 @@ sudo bash /etc/piholeparser/scripts/parser.sh
 ## Cleanup
 sudo bash /etc/piholeparser/scripts/cleanup.sh
 
-## Push 1111ALLPARSEDLISTS1111 to localhost
-sudo bash /etc/piholeparser/scripts/listlocal.sh
-
-## Make sure we are in the correct directory
-cd /etc/piholeparser
-
-## Push Changes up to Github
-printf "$blue"    "___________________________________________________________"
-echo ""
-{ if 
-[ "$version" = "github" ]
-then
-printf "$green"   "Pushing Lists to Github"
-sudo git config --global user.name ""$GITHUBUSERNAME""
-sudo git config --global user.email $GITHUBEMAIL
-sudo git remote set-url origin https://"$GITHUBUSERNAME":"$GITHUBPASSWORD"@github.com/deathbybandaid/piholeparser.git
-sudo git add .
-timestamp=`date`
-sudo git commit -m "Update lists $timestamp"
-sudo git push -u origin master
-else
-printf "$red"   "Not Pushing Lists to Github"
-fi }
-printf "$magenta" "___________________________________________________________"
-echo ""
+## Push lists
+sudo bash /etc/piholeparser/scripts/pushlists.sh
 
 printf "$blue"    "___________________________________________________________"
 echo ""
