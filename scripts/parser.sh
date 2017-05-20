733b97b4d3ffe5d8273b0dd396dcc471c1fc740d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-20 13:46:00 -0400

    Update parser.sh

--- scripts/parser.sh
+++ scripts/parser.sh
@@ -118,3 +118,27 @@ elif
 then
 sudo rm /etc/piholeparser/parsed/*.txt
 fi }
+
+printf "$blue"    "___________________________________________________________"
+echo ""
+printf "$green"   "Rebuilding the complete list file."
+
+if 
+ls /etc/piholeparser/1111ALLPARSEDLISTS1111.lst &> /dev/null; 
+then
+sudo rm /etc/piholeparser/1111ALLPARSEDLISTS1111.lst
+else
+echo ""
+fi
+
+if 
+ls /etc/piholeparser/parsedall/1111ALLPARSEDLISTS1111.lst &> /dev/null; 
+then
+sudo rm /etc/piholeparser/parsedall/1111ALLPARSEDLISTS1111.lst
+else
+echo ""
+fi
+
+sudo cat /etc/piholeparser/lists/*.lst | sort > /etc/piholeparser/parsedall/1111ALLPARSEDLISTS1111.lst
+printf "$magenta" "___________________________________________________________"
+echo ""
