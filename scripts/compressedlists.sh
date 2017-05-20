ad86ce92f4a804c1665414d6266b4e71c53a88f6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-20 14:43:30 -0400

    Update compressedlists.sh

--- scripts/compressedlists.sh
+++ scripts/compressedlists.sh
@@ -14,6 +14,7 @@ source /etc/piholeparser/scripts/colors.var
 printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Downloading and Extracting Compressed Lists."
+echo ""
 
 ## Airelle's Anti-Sex Hosts
 if ping -c 1 rlwpx.free.fr &> /dev/null
@@ -23,9 +24,11 @@ echo ""
 printf "$green"   "Downloading Airelle's Anti-Sex Hosts"
 echo ""
 sudo wget http://rlwpx.free.fr/WPFF/hsex.7z -P /etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Extracting"
 echo ""
 sudo 7za e /etc/piholeparser/compressedconvert/hsex.7z -o/etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Renaming"
 echo ""
 sudo mv /etc/piholeparser/compressedconvert/Hosts.sex /etc/piholeparser/compressedconvert/AirellesAntiSexHosts.txt
@@ -45,9 +48,11 @@ echo ""
 printf "$green"   "Downloading Airelle's Malware Hosts"
 echo ""
 sudo wget http://rlwpx.free.fr/WPFF/hrsk.7z -P /etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Extracting"
 echo ""
 sudo 7za e /etc/piholeparser/compressedconvert/hrsk.7z -o/etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Renaming"
 echo ""
 sudo mv /etc/piholeparser/compressedconvert/Hosts.rsk /etc/piholeparser/compressedconvert/AirellesMalwareHosts.txt
@@ -67,9 +72,11 @@ echo ""
 printf "$green"   "Downloading Airelle's Anti-Trackers Hosts"
 echo ""
 sudo wget http://rlwpx.free.fr/WPFF/htrc.7z -P /etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Extracting"
 echo ""
 sudo 7za e /etc/piholeparser/compressedconvert/htrc.7z -o/etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Renaming"
 echo ""
 sudo mv /etc/piholeparser/compressedconvert/Hosts.trc /etc/piholeparser/compressedconvert/AirellesAntiTrackersHosts.txt
@@ -89,9 +96,11 @@ echo ""
 printf "$green"   "Downloading Airelle's Anti-Advertisements Hosts"
 echo ""
 sudo wget http://rlwpx.free.fr/WPFF/hpub.7z -P /etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Extracting"
 echo ""
 sudo 7za e /etc/piholeparser/compressedconvert/hpub.7z -o/etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Renaming"
 echo ""
 sudo mv /etc/piholeparser/compressedconvert/Hosts.pub /etc/piholeparser/compressedconvert/AirellesAntiAdvertisementsHosts.txt
@@ -111,9 +120,11 @@ echo ""
 printf "$green"   "Downloading Airelle's Anti-Miscellaneous Hosts"
 echo ""
 sudo wget http://rlwpx.free.fr/WPFF/hmis.7z -P /etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Extracting"
 echo ""
 sudo 7za e /etc/piholeparser/compressedconvert/hmis.7z -o/etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Renaming"
 echo ""
 sudo mv /etc/piholeparser/compressedconvert/Hosts.mis /etc/piholeparser/compressedconvert/AirellesAntiMiscellaneousHosts.txt
@@ -132,9 +143,11 @@ printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Airelle's Phishing Hosts"
 sudo wget http://rlwpx.free.fr/WPFF/hblc.7z -P /etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Extracting"
 echo ""
 sudo 7za e /etc/piholeparser/compressedconvert/hblc.7z -o/etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Renaming"
 echo ""
 sudo mv /etc/piholeparser/compressedconvert/Hosts.blc /etc/piholeparser/compressedconvert/AirellesPhishingHosts.txt
@@ -154,9 +167,11 @@ echo ""
 printf "$green"   "Downloading Blackweb List"
 echo ""
 sudo wget https://github.com/maravento/blackweb/raw/master/blackweb.tar.gz -P /etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Extracting"
 echo ""
-sudo tar -xvf /etc/piholeparser/compressedconvert/blackweb.tar.gz -C /etc/piholeparser/compressedconvert
+sudo tar -xvf /etc/piholeparser/compressedconvert/blackweb.tar.gz -C /etc/piholeparser/compressedconvert/
+echo ""
 printf "$yellow"  "Renaming"
 echo ""
 sudo mv /etc/piholeparser/compressedconvert/blackweb.txt /etc/piholeparser/compressedconvert/Blackweb.txt
