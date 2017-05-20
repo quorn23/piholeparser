8b0843f3ad14b0dec5b523a030b0706679a8ea82
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-20 10:31:17 -0400

    Update parseralltest.sh

--- scripts/parseralltest.sh
+++ scripts/parseralltest.sh
@@ -7,6 +7,16 @@ source /etc/piholeparser/scripts/colors.var
 echo ""
 echo "Creating Single Big List."
 
-sudo cat /etc/piholeparser/parsed/*.txt | sort > /etc/piholeparser/parsedall/1111ALLPARSEDLISTS1111.txt
+## merge individuals
+sudo cat /etc/piholeparser/parsed/*.txt | sort > /etc/piholeparser/parsedall/ALLPARSEDLISTS.txt
+
+## Duplicate Removal
+echo ""
+printf "$yellow"  "Removing duplicates..."
+
+echo -e "\t`wc -l /etc/piholeparser/parsedall/ALLPARSEDLISTS.txt | cut -d " " -f 1` lines after deduping"
+sudo cat /etc/piholeparser/parsedall/ALLPARSEDLISTS.txt >> /etc/piholeparser/parsedall/1111ALLPARSEDLISTS1111.txt
+sudo rm /etc/piholeparser/parsedall/ALLPARSEDLISTS.txt
+
 printf "$magenta" "___________________________________________________________"
 echo ""
