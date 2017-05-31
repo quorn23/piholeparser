c4de74cc910e4b528db9ca8e1c877227475b2c56
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-31 13:01:31 -0400

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -17,7 +17,7 @@ ls /etc/piholeparser/lists/*.txt &> /dev/null;
 then
 sudo rm /etc/piholeparser/lists/*.txt
 else
-echo ""
+:
 fi
 
 if 
@@ -25,7 +25,7 @@ ls /etc/piholeparser/compressedconvert/*.7z &> /dev/null;
 then
 sudo rm /etc/piholeparser/compressedconvert/*.7z
 else
-echo ""
+:
 fi
 
 if 
@@ -33,7 +33,7 @@ ls /etc/piholeparser/mirroredlists/*.ads.txt &> /dev/null;
 then
 sudo rm /etc/piholeparser/mirroredlists/*.ads.txt
 else
-echo ""
+:
 fi
 
 if 
@@ -41,7 +41,7 @@ ls /etc/piholeparser/compressedconvert/*.tar.gz &> /dev/null;
 then
 sudo rm /etc/piholeparser/compressedconvert/*.tar.gz
 else
-echo ""
+:
 fi
 
 if 
@@ -49,7 +49,7 @@ ls /etc/piholeparser/compressedconvert/*.txt &> /dev/null;
 then
 sudo rm /etc/piholeparser/compressedconvert/*.txt
 else
-echo ""
+:
 fi
 
 if 
@@ -57,7 +57,15 @@ ls /var/www/html/compressedconvert/*.txt &> /dev/null;
 then
 sudo rm /var/www/html/compressedconvert/*.txt
 else
-echo ""
+:
+fi
+
+if 
+[ "$version" = "local" ]
+then
+sudo rm /etc/piholeparser/parsed/*.txt
+else
+:
 fi
 
 printf "$magenta" "___________________________________________________________"
