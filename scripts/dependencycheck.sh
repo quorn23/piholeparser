8557608eb8bdde551f3fe5a2eb7ad08379295f19
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-05 10:50:10 -0400

    Update dependencycheck.sh

--- scripts/dependencycheck.sh
+++ scripts/dependencycheck.sh
@@ -12,7 +12,8 @@ source /etc/piholeparser/scripts/colors.var
 printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Checking Dependencies"
-{ if
+
+if
 which p7zip >/dev/null;
 then
 echo ""
@@ -20,6 +21,47 @@ printf "$yellow"  "p7zip is installed"
 else
 printf "$yellow"  "Installing p7zip"
 sudo apt-get install -y p7zip-full
-fi }
+fi
+
+if
+which python >/dev/null;
+then
+echo ""
+printf "$yellow"  "python is installed"
+else
+printf "$yellow"  "Installing python"
+sudo apt-get install -y python
+fi
+
+if
+which python-pip >/dev/null;
+then
+echo ""
+printf "$yellow"  "python-pip is installed"
+else
+printf "$yellow"  "Installing python-pip"
+sudo apt-get install -y python-pip
+fi
+
+if
+pip list | grep -F requests >/dev/null;
+then
+echo ""
+printf "$yellow"  "pip module requests is installed"
+else
+printf "$yellow"  "Installing pip module requests"
+sudo pip install requests
+fi
+
+if
+pip list | grep -F beautifulsoup4 >/dev/null;
+then
+echo ""
+printf "$yellow"  "pip module requests is installed"
+else
+printf "$yellow"  "Installing pip module requests"
+sudo pip install beautifulsoup4
+fi
+
 printf "$magenta" "___________________________________________________________"
 echo ""
