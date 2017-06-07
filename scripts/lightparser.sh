c8c924b87bad4e274cb7869ebb310a32a388f1ed
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-07 15:10:47 -0400

    Update lightparser.sh

--- scripts/lightparser.sh
+++ scripts/lightparser.sh
@@ -84,10 +84,11 @@ echo ""
 ## Copy original, one for mirror, one for next step
 sudo cp $ORIGFILE $MFILENAME
 
+##test $(stat -c%s $MFILENAME) -eq 0
+#test $(stat -c%s $MFILENAME) -ge 104857600
 ## Github has a 100mb limit, and empty files are useless
 MFILESIZE=$(stat -c%s "$MFILENAME")
 if 
-#test $(stat -c%s $MFILENAME) -ge 104857600
 test $(stat $MFILENAME) -ge 104857600
 then
 echo ""
@@ -96,7 +97,6 @@ printf "$red"     "Mirror File Too Large For Github. Deleting."
 sudo rm $MFILENAME
 elif
 test $(stat $MFILENAME) -eq 0
-#test $(stat -c%s $MFILENAME) -eq 0
 then
 echo ""
 printf "$red"     "Size of $MFILENAME = $MFILESIZE bytes. Deleting."
@@ -259,10 +259,11 @@ echo ""
 printf "$green"   "Attempting Creation of Parsed List."
 echo ""
 
+#test $(stat -c%s $PFILENAME) -ge 104857600
+#test $(stat -c%s $PFILENAME) -eq 0
 ## Github has a 100mb limit, and empty files are useless
 PFILESIZE=$(stat -c%s "$PFILENAME")
 if
-#test $(stat -c%s $PFILENAME) -ge 104857600
 test $(stat $PFILENAME) -ge 104857600
 then
 echo ""
@@ -271,7 +272,6 @@ printf "$red"     "Parsed File Too Large For Github. Deleting."
 sudo rm $PFILENAME
 elif
 test $(stat $PFILENAME) -eq 0
-#test $(stat -c%s $PFILENAME) -eq 0
 then
 echo ""
 printf "$red"     "Size of $PFILENAME = $PFILESIZE bytes. Deleting."
