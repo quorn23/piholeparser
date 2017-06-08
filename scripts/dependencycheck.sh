9a871baa8c6b294fddaaed745f46525dfca229ab
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-07 22:35:46 -0400

    Create dependencycheck.sh

--- scripts/dependencycheck.sh
+++ scripts/dependencycheck.sh
@@ -23,45 +23,46 @@ printf "$yellow"  "Installing p7zip"
 sudo apt-get install -y p7zip-full
 fi
 
-if
-which python >/dev/null;
-then
-echo ""
-printf "$yellow"  "python is installed"
-else
-printf "$yellow"  "Installing python"
-sudo apt-get install -y python
-fi
 
-if
-which python-pip >/dev/null;
-then
-echo ""
-printf "$yellow"  "python-pip is installed"
-else
-printf "$yellow"  "Installing python-pip"
-sudo apt-get install -y python-pip
-fi
+#if
+#which python >/dev/null;
+#then
+#echo ""
+#printf "$yellow"  "python is installed"
+#else
+#printf "$yellow"  "Installing python"
+#sudo apt-get install -y python
+#fi
 
-if
-pip list --format=legacy | grep -F requests >/dev/null;
-then
-echo ""
-printf "$yellow"  "pip module requests is installed"
-else
-printf "$yellow"  "Installing pip module requests"
-sudo pip install requests
-fi
+#if
+#which python-pip >/dev/null;
+#then
+#echo ""
+#printf "$yellow"  "python-pip is installed"
+#else
+#printf "$yellow"  "Installing python-pip"
+#sudo apt-get install -y python-pip
+#fi
 
-if
-pip list --format=legacy | grep -F beautifulsoup4 >/dev/null;
-then
-echo ""
-printf "$yellow"  "pip module requests is installed"
-else
-printf "$yellow"  "Installing pip module requests"
-sudo pip install beautifulsoup4
-fi
+#if
+#pip list --format=legacy | grep -F requests >/dev/null;
+#then
+#echo ""
+#printf "$yellow"  "pip module requests is installed"
+#else
+#printf "$yellow"  "Installing pip module requests"
+#sudo pip install requests
+#fi
+
+#if
+#pip list --format=legacy | grep -F beautifulsoup4 >/dev/null;
+#then
+#echo ""
+#printf "$yellow"  "pip module beautifulsoup4 is installed"
+#else
+#printf "$yellow"  "Installing pip module beautifulsoup4"
+#sudo pip install beautifulsoup4
+#fi
 
 printf "$magenta" "___________________________________________________________"
 echo ""
