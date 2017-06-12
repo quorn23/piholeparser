0a117ac5304effff07204969a26ebd921a3aaf8d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-12 15:48:15 -0400

    Create tarlists.sh

--- scripts/tarlists.sh
+++ scripts/tarlists.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 ## This will download and extract tar lists
 
-## Version
-source /etc/piholeparser.var
-
 ## Variables
 source /etc/piholeparser/scriptvars/variables.var
 
-## Set File .lst
-FILES=/etc/piholeparser/lists/tar/*.lst
-
 ## Start 7zip File Loop
-for f in $FILES
-do
-
-## Process sources within file.lst
-for source in `cat $f`;
+for f in $TARLISTS
 do
 
 ## Variables
 FNAME=`echo $f | cut -f 1 -d '.'` ## Used for better filenaming
 FNAMEDONE="$FNAME".txt
 TEMPFILE="$FNAME".temp.7z ## Temp File
+
+## Process sources within file.lst
+for source in `cat $f`;
+do
+
+## Variables
 UPCHECK=`echo $source | awk -F/ '{print $3}'` ## used to filter domain name
 
 printf "$blue"    "___________________________________________________________"
@@ -54,13 +50,3 @@ printf "$magenta" "___________________________________________________________"
 ## End looping
 done
 done
-
-## cleanup
-
-if 
-ls /etc/piholeparser/compressedconvert/*.tar.gz &> /dev/null; 
-then
-sudo rm /etc/piholeparser/compressedconvert/*.tar.gz
-else
-:
-fi
