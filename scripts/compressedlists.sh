7839d6efa66a74458cafb8da6e4b01ab885f8375
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-12 12:18:31 -0400

    Create compressedlists.sh

--- scripts/compressedlists.sh
+++ scripts/compressedlists.sh
@@ -16,143 +16,113 @@ echo ""
 printf "$green"   "Downloading and Extracting Compressed Lists."
 echo ""
 
-## Airelle's Anti-Sex Hosts
+## Airelle UpCheck Start
 if ping -c 1 rlwpx.free.fr &> /dev/null
 then
+
+## Airelle's Anti-Sex Hosts
 printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Downloading Airelle's Anti-Sex Hosts"
 echo ""
-sudo wget http://rlwpx.free.fr/WPFF/hsex.7z -P /etc/piholeparser/compressedconvert/
+sudo wget http://rlwpx.free.fr/WPFF/hsex.7z -P $COMPRESSEDDIR
 echo ""
 printf "$yellow"  "Extracting"
 echo ""
-sudo 7za e /etc/piholeparser/compressedconvert/hsex.7z -o/etc/piholeparser/compressedconvert/
+sudo 7za e "$COMPRESSEDDIR"hsex.7z -o"$COMPRESSEDDIR"
 echo ""
 printf "$yellow"  "Renaming"
 echo ""
-sudo mv /etc/piholeparser/compressedconvert/Hosts.sex /etc/piholeparser/compressedconvert/AirellesAntiSexHosts.txt
+sudo mv "$COMPRESSEDDIR"Hosts.sex "$COMPRESSEDDIR"AirellesAntiSexHosts.txt
 echo ""
 printf "$magenta" "___________________________________________________________"
-else
-echo ""
-printf "$red"     "Airelle's lists are unavailable right now"
-echo ""
-fi
+
 
 ## Airelle's Malware Hosts
-if ping -c 1 rlwpx.free.fr &> /dev/null
-then
 printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Downloading Airelle's Malware Hosts"
 echo ""
-sudo wget http://rlwpx.free.fr/WPFF/hrsk.7z -P /etc/piholeparser/compressedconvert/
+sudo wget http://rlwpx.free.fr/WPFF/hrsk.7z -P $COMPRESSEDDIR
 echo ""
 printf "$yellow"  "Extracting"
 echo ""
-sudo 7za e /etc/piholeparser/compressedconvert/hrsk.7z -o/etc/piholeparser/compressedconvert/
+sudo 7za e "$COMPRESSEDDIR"hrsk.7z -o"$COMPRESSEDDIR"
 echo ""
 printf "$yellow"  "Renaming"
 echo ""
-sudo mv /etc/piholeparser/compressedconvert/Hosts.rsk /etc/piholeparser/compressedconvert/AirellesMalwareHosts.txt
+sudo mv "$COMPRESSEDDIR"Hosts.rsk "$COMPRESSEDDIR"AirellesMalwareHosts.txt
 echo ""
 printf "$magenta" "___________________________________________________________"
-else
-echo ""
-printf "$red"     "Airelle's lists are unavailable right now"
-echo ""
-fi
 
 ## Airelle's Anti-Trackers Hosts
-if ping -c 1 rlwpx.free.fr &> /dev/null
-then
 printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Downloading Airelle's Anti-Trackers Hosts"
 echo ""
-sudo wget http://rlwpx.free.fr/WPFF/htrc.7z -P /etc/piholeparser/compressedconvert/
+sudo wget http://rlwpx.free.fr/WPFF/htrc.7z -P $COMPRESSEDDIR
 echo ""
 printf "$yellow"  "Extracting"
 echo ""
-sudo 7za e /etc/piholeparser/compressedconvert/htrc.7z -o/etc/piholeparser/compressedconvert/
+sudo 7za e "$COMPRESSEDDIR"htrc.7z -o"$COMPRESSEDDIR"
 echo ""
 printf "$yellow"  "Renaming"
 echo ""
-sudo mv /etc/piholeparser/compressedconvert/Hosts.trc /etc/piholeparser/compressedconvert/AirellesAntiTrackersHosts.txt
+sudo mv "$COMPRESSEDDIR"Hosts.trc "$COMPRESSEDDIR"AirellesAntiTrackersHosts.txt
 echo ""
 printf "$magenta" "___________________________________________________________"
-else
-echo ""
-printf "$red"     "Airelle's lists are unavailable right now"
-echo ""
-fi
 
 ## Airelle's Anti-Advertisements Hosts
-if ping -c 1 rlwpx.free.fr &> /dev/null
-then
 printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Downloading Airelle's Anti-Advertisements Hosts"
 echo ""
-sudo wget http://rlwpx.free.fr/WPFF/hpub.7z -P /etc/piholeparser/compressedconvert/
+sudo wget http://rlwpx.free.fr/WPFF/hpub.7z -P $COMPRESSEDDIR
 echo ""
 printf "$yellow"  "Extracting"
 echo ""
-sudo 7za e /etc/piholeparser/compressedconvert/hpub.7z -o/etc/piholeparser/compressedconvert/
+sudo 7za e "$COMPRESSEDDIR"hpub.7z -o"$COMPRESSEDDIR"
 echo ""
 printf "$yellow"  "Renaming"
 echo ""
-sudo mv /etc/piholeparser/compressedconvert/Hosts.pub /etc/piholeparser/compressedconvert/AirellesAntiAdvertisementsHosts.txt
+sudo mv "$COMPRESSEDDIR"Hosts.pub "$COMPRESSEDDIR"AirellesAntiAdvertisementsHosts.txt
 echo ""
 printf "$magenta" "___________________________________________________________"
-else
-echo ""
-printf "$red"     "Airelle's lists are unavailable right now"
-echo ""
-fi
 
 ## Airelle's Anti-Miscellaneous Hosts
-if ping -c 1 rlwpx.free.fr &> /dev/null
-then
 printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Downloading Airelle's Anti-Miscellaneous Hosts"
 echo ""
-sudo wget http://rlwpx.free.fr/WPFF/hmis.7z -P /etc/piholeparser/compressedconvert/
+sudo wget http://rlwpx.free.fr/WPFF/hmis.7z -P $COMPRESSEDDIR
 echo ""
 printf "$yellow"  "Extracting"
 echo ""
-sudo 7za e /etc/piholeparser/compressedconvert/hmis.7z -o/etc/piholeparser/compressedconvert/
+sudo 7za e "$COMPRESSEDDIR"hmis.7z -o"$COMPRESSEDDIR"
 echo ""
 printf "$yellow"  "Renaming"
 echo ""
-sudo mv /etc/piholeparser/compressedconvert/Hosts.mis /etc/piholeparser/compressedconvert/AirellesAntiMiscellaneousHosts.txt
+sudo mv "$COMPRESSEDDIR"Hosts.mis "$COMPRESSEDDIR"AirellesAntiMiscellaneousHosts.txt
 echo ""
 printf "$magenta" "___________________________________________________________"
-else
-echo ""
-printf "$red"     "Airelle's lists are unavailable right now"
-echo ""
-fi
 
 ## Airelle's Phishing Hosts
-if ping -c 1 rlwpx.free.fr &> /dev/null
-then
 printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Airelle's Phishing Hosts"
-sudo wget http://rlwpx.free.fr/WPFF/hblc.7z -P /etc/piholeparser/compressedconvert/
+sudo wget http://rlwpx.free.fr/WPFF/hblc.7z -P $COMPRESSEDDIR
 echo ""
 printf "$yellow"  "Extracting"
 echo ""
-sudo 7za e /etc/piholeparser/compressedconvert/hblc.7z -o/etc/piholeparser/compressedconvert/
+sudo 7za e "$COMPRESSEDDIR"hblc.7z -o"$COMPRESSEDDIR"
 echo ""
 printf "$yellow"  "Renaming"
 echo ""
-sudo mv /etc/piholeparser/compressedconvert/Hosts.blc /etc/piholeparser/compressedconvert/AirellesPhishingHosts.txt
+sudo mv "$COMPRESSEDDIR"Hosts.blc "$COMPRESSEDDIR"AirellesPhishingHosts.txt
 echo ""
 printf "$magenta" "___________________________________________________________"
+
+## Airelle UpCheck End
 else
 echo ""
 printf "$red"     "Airelle's lists are unavailable right now"
@@ -166,15 +136,15 @@ printf "$blue"    "___________________________________________________________"
 echo ""
 printf "$green"   "Downloading Blackweb List"
 echo ""
-sudo wget https://github.com/maravento/blackweb/raw/master/blackweb.tar.gz -P /etc/piholeparser/compressedconvert/
+sudo wget https://github.com/maravento/blackweb/raw/master/blackweb.tar.gz -P $COMPRESSEDDIR
 echo ""
 printf "$yellow"  "Extracting"
 echo ""
-sudo tar -xvf /etc/piholeparser/compressedconvert/blackweb.tar.gz -C /etc/piholeparser/compressedconvert/
+sudo tar -xvf "$COMPRESSEDDIR"blackweb.tar.gz -C $COMPRESSEDDIR
 echo ""
 printf "$yellow"  "Renaming"
 echo ""
-sudo mv /etc/piholeparser/compressedconvert/blackweb.txt /etc/piholeparser/compressedconvert/Blackweb.txt
+sudo mv "$COMPRESSEDDIR"blackweb.txt "$COMPRESSEDDIR"Blackweb.txt
 echo ""
 printf "$magenta" "___________________________________________________________"
 else
