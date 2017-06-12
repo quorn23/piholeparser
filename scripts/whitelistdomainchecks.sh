e7be44b40fed24a5548899f3f3f0ee9ba8344dea
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-12 11:42:36 -0400

    Create whitelistdomainchecks.sh

--- scripts/whitelistdomainchecks.sh
+++ scripts/whitelistdomainchecks.sh
@@ -4,8 +4,8 @@
 ## Version
 source /etc/piholeparser.var
 
-## Colors
-source /etc/piholeparser/scripts/colors.var
+## Variables
+source /etc/piholeparser/scriptvars/variables.var
 
 if 
 ls /etc/piholeparser/whitelisted/*.domains &> /dev/null; 
