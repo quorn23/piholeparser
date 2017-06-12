6e06fcc7b93012fe2b2b4aaf56a127094be4b00b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-12 14:48:08 -0400

    Create dependencycheck.sh

--- scripts/dependencycheck.sh
+++ scripts/dependencycheck.sh
@@ -3,9 +3,6 @@
 ## It checks if it is installed, and if not,
 ## it installs the program
 
-## Version
-source /etc/piholeparser.var
-
 ## Variables
 source /etc/piholeparser/scriptvars/variables.var
 
@@ -32,46 +29,6 @@ else
 printf "$yellow"  "Installing gawk"
 sudo apt-get install -y gawk
 fi
-
-#if
-#which python >/dev/null;
-#then
-#echo ""
-#printf "$yellow"  "python is installed"
-#else
-#printf "$yellow"  "Installing python"
-#sudo apt-get install -y python
-#fi
-
-#if
-#which python-pip >/dev/null;
-#then
-#echo ""
-#printf "$yellow"  "python-pip is installed"
-#else
-#printf "$yellow"  "Installing python-pip"
-#sudo apt-get install -y python-pip
-#fi
-
-#if
-#pip list --format=legacy | grep -F requests >/dev/null;
-#then
-#echo ""
-#printf "$yellow"  "pip module requests is installed"
-#else
-#printf "$yellow"  "Installing pip module requests"
-#sudo pip install requests
-#fi
-
-#if
-#pip list --format=legacy | grep -F beautifulsoup4 >/dev/null;
-#then
-#echo ""
-#printf "$yellow"  "pip module beautifulsoup4 is installed"
-#else
-#printf "$yellow"  "Installing pip module beautifulsoup4"
-#sudo pip install beautifulsoup4
-#fi
 
 printf "$magenta" "___________________________________________________________"
 echo ""
