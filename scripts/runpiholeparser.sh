5b1ceecac29449f92cc905c4eb7752f050cfd5d9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-13 10:44:05 -0400

    Update runpiholeparser.sh

--- scripts/runpiholeparser.sh
+++ scripts/runpiholeparser.sh
@@ -17,7 +17,7 @@ sudo bash /etc/piholeparser/scripts/whitelistdomainchecks.sh
 sudo bash /etc/piholeparser/scripts/7ziplists.sh
 
 ## Lists compressed in tar
-#sudo bash /etc/piholeparser/scripts/tarlists.sh
+sudo bash /etc/piholeparser/scripts/tarlists.sh
 
 ## Parse Individual Lists that barely need it
 sudo bash /etc/piholeparser/scripts/lightparser.sh
