1ef32cd3f04bdcff97b7028bcc919130fe2534b9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-13 15:11:22 -0400

    Create createbiglist.sh

--- scripts/createbiglist.sh
+++ scripts/createbiglist.sh
@@ -29,19 +29,22 @@ sudo rm $TEMPAPL
 
 ## Github has a 100mb limit and empty files are useless
 BFILESIZE=$(stat -c%s $BIGAPL)
+timestamp=$(echo `date`)
 if
 [ "$BFILESIZE" -ge "$GITHUBLIMIT" ]
 then
 echo ""
 printf "$red"     "Parsed File Too Large For Github. Deleting."
+sudo echo "Allparsedlist list was too large to host on github. $BFILESIZE bytes $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
 sudo rm $BIGAPL
 sudo echo "File exceeded Githubs 100mb limitation" | sudo tee --append $BIGAPL
 elif
-[ "$BFILESIZE" -ge 0 ]
+[ "$BFILESIZE" -eq 0 ]
 then
 echo ""
 printf "$red"     "File Empty"
 sudo echo "File Size equaled zero." | sudo tee --append $BIGAPL
+sudo echo "Allparsedlist list was an empty file $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
 else
 echo ""
 printf "$yellow"  "Big List Created Successfully."
@@ -57,13 +60,14 @@ echo ""
 ## Big Source edit##
 ####################
 
-sudo cp $BIGAPL $BIGAPLE
-
 echo ""
 printf "$blue"    "___________________________________________________________"
+
 echo ""
-printf "$green"   ""
+printf "$green"   "Attempting Creation of Big List Edited."
 echo ""
+
+sudo cp $BIGAPL $BIGAPLE
 
 ## Start File Loop
 for source in `cat $BDC $DBBW $WHITELIST`;
@@ -76,6 +80,29 @@ sudo mv $TEMPAPLE $BIGAPLE
 
 ## end of loops
 done
+
+## Github has a 100mb limit and empty files are useless
+BEFILESIZE=$(stat -c%s $BIGAPLE)
+timestamp=$(echo `date`)
+if
+[ "$BEFILESIZE" -ge "$GITHUBLIMIT" ]
+then
+echo ""
+printf "$red"     "Parsed File Too Large For Github. Deleting."
+sudo echo "Allparsedlistedited list was too large to host on github. $BEFILESIZE bytes $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+sudo rm $BIGAPLE
+sudo echo "File exceeded Githubs 100mb limitation" | sudo tee --append $BIGAPLE
+elif
+[ "$BEFILESIZE" -eq 0 ]
+then
+echo ""
+printf "$red"     "File Empty"
+sudo echo "File Size equaled zero." | sudo tee --append $BIGAPLE
+sudo echo "Allparsedlistedited list was an empty file $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+else
+echo ""
+printf "$yellow"  "Big List (edited) Created Successfully."
+fi
 
 ####################
 ## Big Source     ##
@@ -94,10 +121,10 @@ sudo sed '/^$/d' $BIGAPLSOURCE > $BIGAPLSOURCE2
 sudo rm $BIGAPLSOURCE
 sudo mv $BIGAPLSOURCE2 $BIGAPLSOURCE
 
-echo -e "\t`wc -l $BIGAPLSOURCE | cut -d " " -f 1` lists processed by the script."
-
-printf "$magenta" "___________________________________________________________"
-echo ""
+timestamp=$(echo `date`)
+HOWMANYLISTS=$(echo -e "\t`wc -l $BIGAPLSOURCE | cut -d " " -f 1` lists processed by the script.")
+sudo echo "$HOWMANYLISTS"
+sudo echo "$HOWMANYLISTS $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
 
 printf "$magenta" "___________________________________________________________"
 echo ""
