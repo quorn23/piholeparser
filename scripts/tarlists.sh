e16649b4783d0c92f7efb9401655095a1f211146
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-13 10:05:45 -0400

    Create tarlists.sh

--- scripts/tarlists.sh
+++ scripts/tarlists.sh
@@ -8,17 +8,12 @@ source /etc/piholeparser/scriptvars/variables.var
 for f in $TARLISTS
 do
 
-## Variables
-FNAME=`echo $f | cut -f 1 -d '.'` ## Used for better filenaming
-FNAMEDONE="$FNAME".txt
-TEMPFILE="$FNAME".temp.7z ## Temp File
-
 ## Process sources within file.lst
 for source in `cat $f`;
 do
 
-## Variables
-UPCHECK=`echo $source | awk -F/ '{print $3}'` ## used to filter domain name
+## Set Variables (again, I guess)
+source /etc/piholeparser/scriptvars/variables.var
 
 printf "$blue"    "___________________________________________________________"
 echo ""
@@ -32,14 +27,10 @@ then
 SOURCEIPFETCH=`ping -c 1 $UPCHECK | gawk -F'[()]' '/PING/{print $2}'`
 SOURCEIP=`echo $SOURCEIPFETCH`
 printf "$yellow"    "Fetching List from $UPCHECK located at the IP of $SOURCEIP and extracting."
-sudo wget -q -O $TEMPFILE $source
-sudo tar -xvf $TEMPFILE
-#sudo tar -xvf $TEMPFILE -C $COMPRESSEDDIR > "$FNAMEDONE"
-sudo rm $TEMPFILE
-#echo ""
-#echo -e "\t`wc -l $FNAMEDONE | cut -d " " -f 1` lines downloaded"
-#ORIGFILESIZE=$(stat -c%s "$FNAMEDONE")
-#printf "$yellow"  "Size of $FNAMEDONE = $ORIGFILESIZE bytes."
+sudo wget -q -O $TARTEMPFILE $source
+TARFILEX=$(tar -xavf $TARTEMPFILE)
+sudo mv $TARFILEX $FNAMEDONE
+sudo rm $TARTEMPFILE
 else 
 printf "$red"    "$FNAME list unavailable right now"
 fi 
@@ -47,6 +38,12 @@ fi
 echo ""
 printf "$magenta" "___________________________________________________________"
 
-## End looping
+## End Source loop
 done
+
+echo -e "\t`wc -l $FNAMEDONE | cut -d " " -f 1` lines downloaded"
+ORIGFILESIZE=$(stat -c%s "$FNAMEDONE")
+printf "$yellow"  "Size of $FNAMEDONE = $ORIGFILESIZE bytes."
+
+## End File Loop
 done
