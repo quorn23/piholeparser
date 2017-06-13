43ba4a944f5bb4a07b32c162582ee60d9b886863
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-13 16:04:37 -0400

    Create tarlists.sh

--- scripts/tarlists.sh
+++ scripts/tarlists.sh
@@ -49,6 +49,7 @@ done
 
 sudo cat $TARDONE > $FNAMEDONE
 
+echo ""
 echo -e "\t`wc -l $FNAMEDONE | cut -d " " -f 1` lines downloaded"
 ORIGFILESIZE=$(stat -c%s "$FNAMEDONE")
 printf "$yellow"  "Size of $FNAMEDONE = $ORIGFILESIZE bytes."
