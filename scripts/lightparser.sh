451b996bf16adf563db11f435434814039280fc3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-14 07:56:07 -0400

    Create lightparser.sh

--- scripts/lightparser.sh
+++ scripts/lightparser.sh
@@ -218,6 +218,24 @@ sudo rm $PRE
 echo -e "\t`wc -l $POST | cut -d " " -f 1` lines after $PARSECOMMENT"
 sudo mv $POST $PRE
 
+## turn spaces into lines
+echo ""
+PARSECOMMENT="replacing spaces with lines"
+printf "$yellow"  "$PARSECOMMENT ..."
+sudo sed 's/\s\+/\n/g' $PRE > $POST
+sudo rm $PRE
+echo -e "\t`wc -l $POST | cut -d " " -f 1` lines after $PARSECOMMENT"
+sudo mv $POST $PRE
+
+## Remove Empty Lines
+echo ""
+PARSECOMMENT="removing empty lines"
+printf "$yellow"  "$PARSECOMMENT ..."
+sudo sed '/^$/d' $PRE > $POST
+sudo rm $PRE
+echo -e "\t`wc -l $POST | cut -d " " -f 1` lines after $PARSECOMMENT"
+sudo mv $POST $PRE
+
 ## Remove IP addresses
 echo ""
 PARSECOMMENT="removing IP addresses"
