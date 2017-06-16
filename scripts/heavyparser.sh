4311971257ea927bbe693435edb9b48d42db2172
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-15 20:56:42 -0400

    Update heavyparser.sh

--- scripts/heavyparser.sh
+++ scripts/heavyparser.sh
@@ -16,7 +16,7 @@ timestamp=$(echo `date`)
 sudo echo "## LightParsing $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
 
 ## Start File Loop
-for f in $HEAVYPARSE
+for f in $HEAVYPARSELISTS
 do
 
 echo ""
