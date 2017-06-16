a8e27164197564d011bd2a6b13b6d06bbb3e8657
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-16 00:03:51 -0400

    Update whitelistdomains.sh

--- scripts/whitelistdomains.sh
+++ scripts/whitelistdomains.sh
@@ -34,6 +34,7 @@ for source in `cat $f`;
 do
 
 ## add to whitelist file
+UPCHECK=`echo $source | awk -F/ '{print $3}'`
 sudo echo "$UPCHECK" | sudo tee --append $TEMPFILE &>/dev/null
 
 ## end of loops
