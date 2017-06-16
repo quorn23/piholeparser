73c0581153fb5923875c2f8c20bc4ff1b3ef0e08
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-16 12:11:44 -0400

    Create createbigsource.sh

---
+++ scripts/createbigsource.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## This trims off the "white" domains
+
+## Variables
+source /etc/piholeparser/scriptvars/variables.var
+
+####################
+## Big Source     ##
+####################
+
+## add all sources
+timestamp=$(echo `date`)
+sudo cat $EVERYLISTFILEWILDCARD | sort > $TEMPFILE
+HOWMANYLISTS=$(echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` lists processed by the script.")
+sudo echo "$HOWMANYLISTS"
+sudo echo "* $HOWMANYLISTS $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+sudo sed '/^$/d' $TEMPFILE > $FILETEMP
+sudo mv $FILETEMP $BIGAPLSOURCE
