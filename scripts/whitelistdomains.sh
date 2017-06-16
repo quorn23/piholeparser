13c3502986636a37c069b42bb4dd3e4c28c4f2fc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-16 00:23:06 -0400

    Update whitelistdomains.sh

--- scripts/whitelistdomains.sh
+++ scripts/whitelistdomains.sh
@@ -28,8 +28,7 @@ sudo echo "* $WHATITIS not there, not removing. $timestamp" | sudo tee --append 
 fi
 
 ## Start File Loop
-#for f in $EVERYLISTFILEWILDCARD
-for f in $ALLOFTHEDOMAINS
+for f in $EVERYLISTFILEWILDCARD
 do
 for source in `cat $f`;
 do
