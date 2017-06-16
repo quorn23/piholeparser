7608c43f8bd408b2e88fcd2d0e8a79c03874ba3e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-16 10:16:21 -0400

    Update whitelistdomains.sh

--- scripts/whitelistdomains.sh
+++ scripts/whitelistdomains.sh
@@ -16,10 +16,12 @@ echo ""
 if
 ls $CHECKME &> /dev/null;
 then
+printf "$red"  "Removing $WHATITIS"
 sudo rm $CHECKME
 sudo touch $CHECKME
 sudo echo "* $WHATITIS removed $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
 else
+printf "$cyan"  "$WHATITIS not there. Not Removing."
 sudo touch $CHECKME
 sudo echo "* $WHATITIS not there, not removing. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
 fi
@@ -57,7 +59,7 @@ timestamp=$(echo `date`)
 printf "$yellow"  "Processing $WHITESORTDEDUPE."
 sudo cat -s $WHATLISTTOSORT | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILE
 HOWMANYLINES=$(echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` Lines In File")
-sudo echo "$HOWMANYLINES $timestamp"
+sudo echo "$HOWMANYLINES"
 sudo rm $WHATLISTTOSORT
 sudo mv $TEMPFILE $WHATLISTTOSORT
 sudo echo "* Processed "$WHITESORTDEDUPE". $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
@@ -70,7 +72,7 @@ timestamp=$(echo `date`)
 printf "$yellow"  "Processing $WHITESORTDEDUPE."
 sudo cat -s $WHATLISTTOSORT | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILE
 HOWMANYLINES=$(echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` Lines In File")
-sudo echo "$HOWMANYLINES $timestamp"
+sudo echo "$HOWMANYLINES"
 sudo rm $WHATLISTTOSORT
 sudo mv $TEMPFILE $WHATLISTTOSORT
 sudo echo "* Processed "$WHITESORTDEDUPE". $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
@@ -83,7 +85,7 @@ timestamp=$(echo `date`)
 printf "$yellow"  "Processing $WHITESORTDEDUPE."
 sudo cat -s $WHATLISTTOSORT | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILE
 HOWMANYLINES=$(echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` Lines In File")
-sudo echo "$HOWMANYLINES $timestamp"
+sudo echo "$HOWMANYLINES"
 sudo rm $WHATLISTTOSORT
 sudo mv $TEMPFILE $WHATLISTTOSORT
 sudo echo "* Processed "$WHITESORTDEDUPE". $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
@@ -96,7 +98,7 @@ timestamp=$(echo `date`)
 printf "$yellow"  "Processed $WHITESORTDEDUPE"
 sudo cat -s $WHATLISTTOSORT | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILE
 HOWMANYLINES=$(echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` Lines In File")
-sudo echo "$HOWMANYLINES $timestamp"
+sudo echo "$HOWMANYLINES"
 sudo mv $TEMPFILE $WHITELISTTEMP
 sudo echo "* "$WHITESORTDEDUPE". $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
 sudo echo "* $HOWMANYLINES $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
