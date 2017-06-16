85fac90d56c89be3ab4e7ed51556c027c093006e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-16 12:48:48 -0400

    Update tarlists.sh

--- scripts/tarlists.sh
+++ scripts/tarlists.sh
@@ -36,7 +36,7 @@ TARFILEX=$(tar -xavf $COMPRESSEDTEMPTAR -C $TEMPDIR)
 sudo cat $TARFILEX > $TEMPFILE
 echo -e "\t`wc -l $TEMPFILE  | cut -d " " -f 1` lines downloaded"
 FETCHFILESIZE=$(stat -c%s "$TEMPFILE")
-printf "$yellow"  "Size of $TARLISTDONE = $FECTHFILESIZE bytes."
+printf "$yellow"  "Size of $BASEFILENAME = $FECTHFILESIZE bytes."
 sudo mv $TEMPFILE $TARLISTDONE
 sudo rm $COMPRESSEDTEMPTAR
 else 
@@ -46,6 +46,7 @@ fi
 
 ## End Source loop
 done
-
+printf "$magenta" "___________________________________________________________"
+echo ""
 ## End File Loop
 done
