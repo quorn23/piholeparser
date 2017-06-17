1c225520c6d43bbdeab09ed86ad7d35d2385ec92
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-16 23:46:39 -0400

    Update deletetempfile.sh

--- scripts/deletetempfile.sh
+++ scripts/deletetempfile.sh
@@ -39,27 +39,3 @@ sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/de
 else
 sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
 fi
-
-WHATITIS="Temporary 7zip File"
-CHECKME=$COMPRESSEDTEMPSEVEN
-timestamp=$(echo `date`)
-if
-ls $CHECKME &> /dev/null;
-then
-sudo rm $CHECKME
-sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-else
-sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-fi
-
-WHATITIS="Temporary Tar File"
-CHECKME=$COMPRESSEDTEMPTAR
-timestamp=$(echo `date`)
-if
-ls $CHECKME &> /dev/null;
-then
-sudo rm $CHECKME
-sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-else
-sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-fi
