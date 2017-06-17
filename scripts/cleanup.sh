7ca49583bf620db4c5a0d3382182dda8e61c0030
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-16 23:23:12 -0400

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,1 +1,137 @@
+#!/bin/bash
+## This script Cleans up directories 
+## mainly to avoid pushing excess files to Github
 
+## Variables
+source /etc/piholeparser/scriptvars/variables.var
+
+printf "$blue"    "___________________________________________________________"
+echo ""
+printf "$green"   "Cleaning Up."
+timestamp=$(echo `date`)
+sudo echo "## Cleanup $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+
+## Clean list directories
+
+WHATITIS="Heavy Parsing Folder txt files"
+CHECKME=/etc/piholeparser/lists/heavyparsing/*.txt
+timestamp=$(echo `date`)
+if
+ls $CHECKME &> /dev/null;
+then
+sudo rm $CHECKME
+sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+else
+sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+fi
+
+WHATITIS="Light Parsing Folder txt files"
+CHECKME=/etc/piholeparser/lists/lightparsing/*.txt
+timestamp=$(echo `date`)
+if
+ls $CHECKME &> /dev/null;
+then
+sudo rm $CHECKME
+sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+else
+sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+fi
+
+WHATITIS="7zip Parsing Folder txt files"
+CHECKME=/etc/piholeparser/lists/7zip/*.txt
+timestamp=$(echo `date`)
+if
+ls $CHECKME &> /dev/null;
+then
+sudo rm $CHECKME
+sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+else
+sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+fi
+
+WHATITIS="Tar Parsing Folder txt files"
+CHECKME=/etc/piholeparser/lists/tar/*.txt
+timestamp=$(echo `date`)
+if
+ls $CHECKME &> /dev/null;
+then
+sudo rm $CHECKME
+sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+else
+sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+fi
+
+WHATITIS="7z Files"
+CHECKME=/etc/piholeparser/lists/7zip/*.7z
+timestamp=$(echo `date`)
+if
+ls $CHECKME &> /dev/null;
+then
+sudo rm $CHECKME
+sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+else
+sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+fi
+
+WHATITIS="Tar Files"
+CHECKME=/etc/piholeparser/lists/tar/*.tar.gz
+timestamp=$(echo `date`)
+if
+ls $CHECKME &> /dev/null;
+then
+sudo rm $CHECKME
+sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+else
+sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+fi
+
+WHATITIS="txt Files in the lists directory"
+CHECKME=/etc/piholeparser/lists/*.txt
+timestamp=$(echo `date`)
+if
+ls $CHECKME &> /dev/null;
+then
+sudo rm $CHECKME
+sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+else
+sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+fi
+
+WHATITIS="garbage txt Files in the mirrored directory"
+CHECKME=/etc/piholeparser/mirroredlists/*.orig.txt
+timestamp=$(echo `date`)
+if
+ls $CHECKME &> /dev/null;
+then
+sudo rm $CHECKME
+sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+else
+sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+fi
+
+## Clean parsed directory if using locally
+if 
+[ "$version" = "local" ]
+then
+sudo rm /etc/piholeparser/parsed/*.txt
+else
+:
+fi
+
+sudo echo "" | sudo tee --append $RECENTRUN &>/dev/null
+printf "$magenta" "___________________________________________________________"
+echo ""
+
+printf "$blue"    "___________________________________________________________"
+echo ""
+printf "$green"   "Updating Main Readme file."
+
+timestamp=$(echo `date`)
+sudo echo "## Updated Main README.md $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+
+sudo rm $MAINREADME
+sudo sed "s/LASTRUNVARIABLEGOESHERE/$timestamp/" $MAINREADMEDEFAULT > $MAINREADME
+
+sudo echo "" | sudo tee --append $RECENTRUN &>/dev/null
+printf "$magenta" "___________________________________________________________"
+echo ""
