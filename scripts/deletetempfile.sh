df91a47fdf49d2cfe808969810b81c31df179803
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-18 10:37:26 -0400

    Update deletetempfile.sh

--- scripts/deletetempfile.sh
+++ scripts/deletetempfile.sh
@@ -4,42 +4,6 @@
 ## Variables
 source /etc/piholeparser/scriptvars/staticvariables.var
 
-WHATITIS="Temporary File"
-CHECKME=$TEMPFILE
-timestamp=$(echo `date`)
-if
-ls $CHECKME &> /dev/null;
-then
-sudo rm $CHECKME
-sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-else
-sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-fi
-
-WHATITIS="Temporary File #2 "
-CHECKME=$FILETEMP
-timestamp=$(echo `date`)
-if
-ls $CHECKME &> /dev/null;
-then
-sudo rm $CHECKME
-sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-else
-sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-fi
-
-WHATITIS="Temporary Original File"
-CHECKME=$ORIGINALFILETEMP
-timestamp=$(echo `date`)
-if
-ls $CHECKME &> /dev/null;
-then
-sudo rm $CHECKME
-sudo echo "* $WHATITIS Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-else
-sudo echo "* $WHATITIS Not Removed. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-fi
-
 WHATITIS="Temporary txt Files"
 CHECKME=$TEMPCLEANUP
 timestamp=$(echo `date`)
