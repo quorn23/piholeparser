96580cb362482eaeded4a1bccac816c601b48fd8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-18 10:10:13 -0400

    Update lightparser.sh

--- scripts/lightparser.sh
+++ scripts/lightparser.sh
@@ -46,38 +46,38 @@ if
 then
 printf "$yellow"    "Fetching List from $UPCHECK located at the IP of "$SOURCEIP"."
 echo ""
-sudo wget -q -O $TEMPFILE $source
-sudo cat $TEMPFILE >> $ORIGINALFILETEMP
+sudo wget -q -O $BTEMPFILE $source
+sudo cat $BTEMPFILE >> $BORIGINALFILETEMP
 sudo rm $TEMPFILE
 elif
 [[ $source == file* ]]
 then
 printf "$yellow"    "Fetching List From Local File."
 echo ""
-sudo curl --silent -L $source >> $TEMPFILE
-sudo cat $TEMPFILE >> $ORIGINALFILETEMP
-sudo rm $TEMPFILE
+sudo curl --silent -L $source >> $BTEMPFILE
+sudo cat $BTEMPFILE >> $BORIGINALFILETEMP
+sudo rm $BTEMPFILE
 else
 printf "$yellow"    "Fetching List From Git Repo Mirror."
 sudo echo "* $BASEFILENAME list unavailable to download. Attempted to use Mirror. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
 echo ""
-sudo wget -q -O $TEMPFILE $MIRROREDFILEDL
-sudo cat $TEMPFILE >> $ORIGINALFILETEMP
-sudo rm $TEMPFILE
+sudo wget -q -O $BTEMPFILE $MIRROREDFILEDL
+sudo cat $BTEMPFILE >> $BORIGINALFILETEMP
+sudo rm $BTEMPFILE
 fi
 
 ## Source completion
 done
 
 ## Original File Size
-FETCHFILESIZE=$(stat -c%s "$ORIGINALFILETEMP")
+FETCHFILESIZE=$(stat -c%s "$BORIGINALFILETEMP")
 if 
 [ "$FETCHFILESIZE" -eq 0 ]
 then
 timestamp=$(echo `date`)
 sudo echo "* $BASEFILENAME list was an empty file upon download. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
 else
-echo -e "\t`wc -l $ORIGINALFILETEMP | cut -d " " -f 1` lines downloaded"
+echo -e "\t`wc -l $BORIGINALFILETEMP | cut -d " " -f 1` lines downloaded"
 printf "$yellow"  "Size of $BASEFILENAME = $FETCHFILESIZE bytes."
 echo ""
 fi
@@ -91,11 +91,11 @@ printf "$green"   "Attempting Creation of Mirror File."
 echo ""
 
 ## Copy original, one for mirror, one for next step
-sudo cp $ORIGINALFILETEMP $FILETEMP
-sudo cp $ORIGINALFILETEMP $TEMPFILE
+sudo cp $BORIGINALFILETEMP $BFILETEMP
+sudo cp $BORIGINALFILETEMP $BTEMPFILE
 
 ## Github has a 100mb limit, and empty files are useless
-FETCHFILESIZE=$(stat -c%s "$TEMPFILE")
+FETCHFILESIZE=$(stat -c%s "$BTEMPFILE")
 timestamp=$(echo `date`)
 if 
 [ "$FETCHFILESIZE" -ge "$GITHUBLIMIT" ]
@@ -105,7 +105,7 @@ printf "$red"     "Size of $BASEFILENAME = $FETCHFILESIZE bytes."
 printf "$red"     "Mirror File Too Large For Github. Deleting."
 echo ""
 sudo echo "* $BASEFILENAME list was $FETCHFILESIZE bytes, and too large to mirror on github. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-sudo rm $TEMPFILE
+sudo rm $BTEMPFILE
 elif
 [ "$FETCHFILESIZE" -eq 0 ]
 then
@@ -113,7 +113,7 @@ echo ""
 printf "$red"     "Size of $BASEFILENAME = $FETCHFILESIZE bytes. Deleting."
 echo ""
 sudo echo "* $BASEFILENAME list was an empty file. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-sudo rm $TEMPFILE
+sudo rm $BTEMPFILE
 else
 echo ""
 printf "$yellow"     "Size of $BASEFILENAME = $FETCHFILESIZE bytes."
@@ -129,17 +129,17 @@ fi
 ## Domain Requirements,, a period and a letter
 PARSECOMMENT="Checking for FQDN Requirements."
 printf "$yellow"  "$PARSECOMMENT ..."
-sed '/[a-z]/!d; /[.]/!d' < $FILETEMP > $TEMPFILE
-echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
-sudo mv $TEMPFILE $FILETEMP
+sed '/[a-z]/!d; /[.]/!d' < $BFILETEMP > $BTEMPFILE
+echo -e "\t`wc -l $BTEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
+sudo mv $BTEMPFILE $BFILETEMP
 echo ""
 
 ## Comments #'s and !'s, also empty lines
 PARSECOMMENT="Removing Commented Lines."
 printf "$yellow"  "$PARSECOMMENT ..."
-sed '/^\s*#/d; s/[#]/\'$'\n/g; /[#]/d; /[!]/d; /^$/d' < $FILETEMP > $TEMPFILE
-echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
-sudo mv $TEMPFILE $FILETEMP
+sed '/^\s*#/d; s/[#]/\'$'\n/g; /[#]/d; /[!]/d; /^$/d' < $BFILETEMP > $BTEMPFILE
+echo -e "\t`wc -l $BTEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
+sudo mv $BTEMPFILE $BFILETEMP
 echo ""
 
 ## Invalid Characters
@@ -147,17 +147,17 @@ echo ""
 ## apparently you can have an emoji domain name?
 PARSECOMMENT="Removing Invalid FQDN characters."
 printf "$yellow"  "$PARSECOMMENT ..."
-sed '/[,]/d; s/"/'\''/g; /\"\//d; /[+]/d; /[\]/d; /[/]/d; /[<]/d; /[>]/d; /[?]/d; /[*]/d; /[#]/d; /[!]/d; /[@]/d; /[~]/d; /[`]/d; /[=]/d; /[:]/d; /[;]/d; /[%]/d; /[&]/d; /[(]/d; /[)]/d; /[$]/d; /\[\//d; /\]\//d; /[{]/d; /[}]/d' < $FILETEMP > $TEMPFILE
-echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
-sudo mv $TEMPFILE $FILETEMP
+sed '/[,]/d; s/"/'\''/g; /\"\//d; /[+]/d; /[\]/d; /[/]/d; /[<]/d; /[>]/d; /[?]/d; /[*]/d; /[#]/d; /[!]/d; /[@]/d; /[~]/d; /[`]/d; /[=]/d; /[:]/d; /[;]/d; /[%]/d; /[&]/d; /[(]/d; /[)]/d; /[$]/d; /\[\//d; /\]\//d; /[{]/d; /[}]/d' < $BFILETEMP > $BTEMPFILE
+echo -e "\t`wc -l $BTEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
+sudo mv $BTEMPFILE $BFILETEMP
 echo ""
 
 ## Periods at begining and end of lines
 PARSECOMMENT="Removing Lines With a Period at the Start or End."
 printf "$yellow"  "$PARSECOMMENT ..."
-sudo sed '/^[.],/d; /^[.]/d; /[.]$/d' < $FILETEMP > $TEMPFILE
-echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
-sudo mv $TEMPFILE $FILETEMP
+sudo sed '/^[.],/d; /^[.]/d; /[.]$/d' < $BFILETEMP > $BTEMPFILE
+echo -e "\t`wc -l $BTEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
+sudo mv $BTEMPFILE $BFILETEMP
 echo ""
 
 ## Replace Spaces and, then Remove Empty Lines
@@ -187,29 +187,29 @@ echo ""
 ## Pipes and Carrots
 PARSECOMMENT="Removing Pipes and Carrots."
 printf "$yellow"  "$PARSECOMMENT ..."
-sudo cat -s $FILETEMP | sed 's/^||//' | cut -d'^' -f-1 > $TEMPFILE
-echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
-sudo mv $TEMPFILE $FILETEMP
+sudo cat -s $BFILETEMP | sed 's/^||//' | cut -d'^' -f-1 > $BTEMPFILE
+echo -e "\t`wc -l $BTEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
+sudo mv $BTEMPFILE $BFILETEMP
 echo ""
 
 ## Domain Requirements,, a period and a letter
 PARSECOMMENT="Checking for FQDN Requirements."
 printf "$yellow"  "$PARSECOMMENT ..."
-sed '/[a-z]/!d; /[.]/!d' < $FILETEMP > $TEMPFILE
-echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
-sudo mv $TEMPFILE $FILETEMP
+sed '/[a-z]/!d; /[.]/!d' < $BFILETEMP > $BTEMPFILE
+echo -e "\t`wc -l $BTEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
+sudo mv $BTEMPFILE $BFILETEMP
 echo ""
 
 ## Duplicate Removal
 PARSECOMMENT="Removing Duplicate Lines."
 printf "$yellow"  "$PARSECOMMENT ..."
-sudo cat -s $FILETEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILE
-echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
-sudo mv $TEMPFILE $FILETEMP
+sudo cat -s $BFILETEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $BTEMPFILE
+echo -e "\t`wc -l $BTEMPFILE | cut -d " " -f 1` lines after $PARSECOMMENT"
+sudo mv $BTEMPFILE $BFILETEMP
 echo ""
 
 ## Prepare for next step
-sudo mv $FILETEMP $TEMPFILE
+sudo mv $BFILETEMP $BTEMPFILE
 
 ####################
 ## Complete Lists ##
@@ -220,7 +220,7 @@ printf "$green"   "Attempting Creation of Parsed List."
 echo ""
 
 ## Github has a 100mb limit, and empty files are useless
-FETCHFILESIZE=$(stat -c%s "$TEMPFILE")
+FETCHFILESIZE=$(stat -c%s "$BTEMPFILE")
 timestamp=$(echo `date`)
 if 
 [ "$FETCHFILESIZE" -ge "$GITHUBLIMIT" ]
@@ -230,7 +230,7 @@ printf "$red"     "Size of $BASEFILENAME = $FETCHFILESIZE bytes."
 printf "$red"     "Mirror File Too Large For Github. Deleting."
 echo ""
 sudo echo "* $BASEFILENAME list was $FETCHFILESIZE bytes, and too large to mirror on github. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-sudo rm $TEMPFILE
+sudo rm $BTEMPFILE
 elif
 [ "$FETCHFILESIZE" -eq 0 ]
 then
@@ -238,13 +238,13 @@ echo ""
 printf "$red"     "Size of $BASEFILENAME = $FETCHFILESIZE bytes. Deleting."
 echo ""
 sudo echo "* $BASEFILENAME list was an empty file. $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
-sudo rm $TEMPFILE
+sudo rm $BTEMPFILE
 else
 echo ""
 printf "$yellow"     "Size of $BASEFILENAME = $FETCHFILESIZE bytes."
 printf "$yellow"  "Copying to the parsed Directory."
 echo ""
-sudo mv $TEMPFILE $PARSEDFILE
+sudo mv $BTEMPFILE $PARSEDFILE
 fi
 
 printf "$magenta" "___________________________________________________________"
