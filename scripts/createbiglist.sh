ca36fb1eacb087b549bc80d2b2813feb1264523a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-19 08:23:36 -0400

    Update createbiglist.sh

--- scripts/createbiglist.sh
+++ scripts/createbiglist.sh
@@ -45,3 +45,37 @@ echo ""
 sudo mv $TEMPFILE $BIGAPL
 printf "$yellow"  "Big List Created Successfully."
 fi
+
+####################
+## Big Source edit##
+####################
+
+## should remove faster than a sed loop
+sudo gawk 'NR==FNR{a[$0];next} !($0 in a)' $WHITELISTTEMP $BIGAPL > $TEMPFILE
+sudo rm $WHITELISTTEMP
+
+## Github has a 100mb limit and empty files are useless
+FETCHFILESIZE=$(stat -c%s $TEMPFILE)
+timestamp=$(echo `date`)
+if
+[ "$FETCHFILESIZE" -ge "$GITHUBLIMIT" ]
+then
+echo ""
+printf "$red"     "Parsed File Too Large For Github. Deleting."
+sudo echo "* Allparsedlist list was too large to host on github. $FETCHFILESIZE bytes $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+sudo rm $FILETEMP
+sudo echo "File exceeded Githubs 100mb limitation" | sudo tee --append $TEMPFILE
+sudo mv $TEMPFILE $BIGAPLE
+elif
+[ "$FETCHFILESIZE" -eq 0 ]
+then
+echo ""
+printf "$red"     "File Empty"
+sudo echo "File Size equaled zero." | sudo tee --append $TEMPFILE
+sudo echo "* Allparsedlist list was an empty file $timestamp" | sudo tee --append $RECENTRUN &>/dev/null
+sudo mv $TEMPFILE $BIGAPLE
+else
+echo ""
+sudo mv $TEMPFILE $BIGAPLE
+printf "$yellow"  "Big List Edited Created Successfully."
+fi
