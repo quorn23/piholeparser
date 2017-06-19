86e8ca32b5eed498711b65251e43a05b3bc28f91
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-19 14:13:46 -0400

    Update deletetempfile.sh

--- scripts/deletetempfile.sh
+++ scripts/deletetempfile.sh
@@ -4,9 +4,7 @@
 ## Variables
 source /etc/piholeparser/scriptvars/staticvariables.var
 
-WHATITIS="Temporary txt Files"
 CHECKME=$TEMPCLEANUP
-timestamp=$(echo `date`)
 if
 ls $CHECKME &> /dev/null;
 then
@@ -15,9 +13,7 @@ else
 :
 fi
 
-WHATITIS="Temporary 7zip File"
 CHECKME=$COMPRESSEDTEMPSEVEN
-timestamp=$(echo `date`)
 if
 ls $CHECKME &> /dev/null;
 then
@@ -26,9 +22,7 @@ else
 :
 fi
 
-WHATITIS="Temporary Tar File"
 CHECKME=$COMPRESSEDTEMPTAR
-timestamp=$(echo `date`)
 if
 ls $CHECKME &> /dev/null;
 then
