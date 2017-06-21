387c85fd3e2d5efe91b9eaab4e9abaf8b4613a8a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-21 10:53:23 -0400

    Update querylists.sh

--- scripts/querylists.sh
+++ scripts/querylists.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 ## This should help me find what parsed list contains a domain
-## whiptail required
 
+## whiptail required
+WHATITIS=whiptail
+WHATPACKAGE=whiptail
+if
+which $WHATITIS >/dev/null;
+then
+:
+else
+printf "$yellow"  "Installing $WHATITIS"
+sudo apt-get install -y $WHATPACKAGE
+fi
+
+LISTLOCATION=/etc/piholeparserdev/parsed/*.txt
 DOMAINTOLOOKFOR=$(whiptail --inputbox "What Domain are you hunting for?" 10 80 "" 3>&1 1>&2 2>&3)
-for f in /etc/piholeparserdev/parsed/*.txt
+echo ""
+echo "Searching for $DOMAINTOLOOKFOR"
+echo ""
+for f in $LISTLOCATION
 do
 BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
 if 
 grep -q $DOMAINTOLOOKFOR "$f"
 then
-echo "$DOMAINTOLOOKFOR was found in $BASEFILENAME "
+echo "Found In "$BASEFILENAME". Matching Included:"
+echo "                    `grep $DOMAINTOLOOKFOR $f`"
+echo ""
 else
 :
 fi
 done
-echo " The Search For $DOMAINTOLOOKFOR completed. "
+echo ""
+echo ""
+echo "The Search For $DOMAINTOLOOKFOR completed. "
