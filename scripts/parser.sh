880c275cc97906440447f6f4b545b5fe25f84110
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-21 08:17:27 -0400

    Update parser.sh

--- scripts/parser.sh
+++ scripts/parser.sh
@@ -278,6 +278,41 @@ then
 FILESIZEZERO=true
 fi
 
+## HTTP and HTTPS
+PARSECOMMENT="Removing Lines with HTTP or HTTPS."
+if
+[[ -z $FILESIZEZERO ]]
+then
+printf "$cyan"  "$PARSECOMMENT"
+sed '/[https:]/d; /[HTTPS:]/d; /[http:]/d; /[HTTP:]/d' < $BFILETEMP > $BTEMPFILE
+FETCHFILESIZE=$(stat -c%s "$BTEMPFILE")
+HOWMANYLINES=$(echo -e "`wc -l $BTEMPFILE | cut -d " " -f 1`")
+ENDCOMMENT="$HOWMANYLINES Lines After $PARSECOMMENT"
+sudo mv $BTEMPFILE $BFILETEMP
+else
+:
+fi
+if
+[[ -n $ENDCOMMENT && $HOWMANYLINES -eq 0 ]]
+then
+printf "$red"  "$ENDCOMMENT $SKIPPINGTOENDOFPARSERLOOP"
+echo ""
+unset ENDCOMMENT
+unset HOWMANYLINES
+elif
+[[ -n $ENDCOMMENT && $HOWMANYLINES -gt 0 ]]
+then
+printf "$yellow"  "$ENDCOMMENT"
+echo ""
+unset ENDCOMMENT
+unset HOWMANYLINES
+fi
+if
+[[ "$FETCHFILESIZE" -eq 0 ]]
+then
+FILESIZEZERO=true
+fi
+
 #####################################################################
 ## Perl Parser
 ## We skip this if the file is in the "heavy" directory
