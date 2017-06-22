903d378c2f7d8ab1f2b9adbc23460e8f58cc0a76
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-22 11:52:22 -0400

    Update parser.sh

--- scripts/parser.sh
+++ scripts/parser.sh
@@ -21,6 +21,7 @@ HOWMANYLINES=$(echo -e "`wc -l $f | cut -d " " -f 1`")
 if
 [[ "$HOWMANYLINES" -gt 1 ]]
 then
+BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
 echo "* $BASEFILENAME Has $HOWMANYLINES sources. $timestamp" | tee --append $RECENTRUN &>/dev/null
 printf "$yellow"    "$BASEFILENAME Has $HOWMANYLINES Sources."
 else
