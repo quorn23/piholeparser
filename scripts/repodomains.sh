d9e2100365ffc89f445f683e5f7deef7263d458f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-23 13:15:20 -0400

    Update repodomains.sh

--- scripts/repodomains.sh
+++ scripts/repodomains.sh
@@ -28,6 +28,26 @@ touch $CHECKME
 echo "* $WHATITIS not there, not removing. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
 
+WHATITIS="Blacklist File"
+CHECKME=$LISTWHITELISTDOMAINS
+timestamp=$(echo `date`)
+printf "$yellow"  "Checking For $WHATITIS"
+echo ""
+if
+ls $CHECKME &> /dev/null;
+then
+printf "$red"  "Removing $WHATITIS"
+echo ""
+rm $CHECKME
+touch $CHECKME
+echo "* $WHATITIS removed $timestamp" | tee --append $RECENTRUN &>/dev/null
+else
+printf "$cyan"  "$WHATITIS not there. Not Removing."
+echo ""
+touch $CHECKME
+echo "* $WHATITIS not there, not removing. $timestamp" | tee --append $RECENTRUN &>/dev/null
+fi
+
 ####################
 ## Whitelist .lst ##
 ####################
@@ -53,6 +73,7 @@ echo "$UPCHECK" | tee --append $LISTWHITELISTDOMAINS &>/dev/null
 done
 done
 fi
+
 ###########################
 ## Whitelist sort dedupe ##
 ###########################
@@ -89,6 +110,44 @@ HOWMANYLINES=$(echo -e "\t`wc -l $WHITELISTTEMP | cut -d " " -f 1` Lines In File
 echo "* "$WHITESORTDEDUPE". $timestamp" | tee --append $RECENTRUN &>/dev/null
 echo "* $HOWMANYLINES $timestamp" | tee --append $RECENTRUN &>/dev/null
 echo ""
+
+###########################
+## Blacklist sort dedupe ##
+###########################
+
+## Start File Loop
+for f in $BLACKLISTDOMAINSALL
+do
+
+## Variables
+source /etc/piholeparser/scriptvars/dynamicvariables.var
+
+WHATLISTTOSORT=$f
+BLACKSORTDEDUPE="$BASEFILENAME Domains."
+timestamp=$(echo `date`)
+printf "$yellow"  "Processing $BLACKSORTDEDUPE."
+cat -s $WHATLISTTOSORT | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $BBLACKTEMP
+HOWMANYLINES=$(echo -e "\t`wc -l $BBLACKTEMP | cut -d " " -f 1` Lines In File")
+echo "$HOWMANYLINES"
+rm $WHATLISTTOSORT
+mv $BBLACKTEMP $WHATLISTTOSORT
+echo "* Processed "$BLACKSORTDEDUPE". $timestamp" | tee --append $RECENTRUN &>/dev/null
+echo "* $HOWMANYLINES $timestamp" | tee --append $RECENTRUN &>/dev/null
+echo ""
+
+## end of loop
+done
+
+BLACKSORTDEDUPE="Merging the Blacklists for Later."
+WHATLISTSMERGE="$BLACKLISTDOMAINSALL"
+timestamp=$(echo `date`)
+printf "$yellow"  "Processed $BLACKSORTDEDUPE"
+cat -s $WHATLISTSMERGE | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $BLACKLISTTEMP
+HOWMANYLINES=$(echo -e "\t`wc -l $BLACKLISTTEMP | cut -d " " -f 1` Lines In File")
+echo "* "$BLACKSORTDEDUPE". $timestamp" | tee --append $RECENTRUN &>/dev/null
+echo "* $HOWMANYLINES $timestamp" | tee --append $RECENTRUN &>/dev/null
+echo ""
+
 
 ####################
 ## pihole -w      ##
