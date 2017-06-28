de278e468e0f95e815e317432fb92f8866335e6d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-28 16:54:50 -0400

    Update housekeeping.sh

--- scripts/housekeeping.sh
+++ scripts/housekeeping.sh
@@ -219,6 +219,7 @@ fi
 cat $EVERYLISTFILEWILDCARD | sort > $TEMPFILE
 HOWMANYSOURCELISTS=$(echo -e "\t`wc -l $TEMPFILE | cut -d " " -f 1`")
 HOWMANYSOURCE="$HOWMANYSOURCELISTS lists to be processed by the script."
+echo "HOWMANYSOURCELISTS='"$HOWMANYSOURCELISTS"'" | tee --append $TEMPVARS &>/dev/null
 echo "HOWMANYSOURCE='"$HOWMANYSOURCE"'" | tee --append $TEMPVARS &>/dev/null
 echo "$HOWMANYSOURCE"
 echo "* $HOWMANYSOURCE $timestamp" | tee --append $RECENTRUN &>/dev/null
