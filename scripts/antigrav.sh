9e946461fa7de032b1d75d4db96253ec94da4d72
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-28 09:06:20 -0400

    Update antigrav.sh

--- scripts/antigrav.sh
+++ scripts/antigrav.sh
@@ -39,6 +39,6 @@ sed "s/^$HOSTIP\s\+[ \t]*//" < $GRAVITY > $TEMPFILE
 
 ## WhatDiff
 gawk 'NR==FNR{a[$0];next} !($0 in a)' $BIGAPLE $TEMPFILE > $ANTIGRAV
-HOWMANYLINES=$(echo -e "`wc -l $ANTIGRAV | cut -d " " -f 1`")
-#printf "$yellow"  "Antigrav File contains $HOWMANYLINES Domains taht are not used by gravity."
+#HOWMANYLINES=$(echo -e "`wc -l $ANTIGRAV | cut -d " " -f 1`")
+printf "$yellow"  "Antigrav File contains $HOWMANYLINES Domains that are not used by gravity."
 rm $TEMPFILE
