f49590ed394ab47d65ae5d0151131cd750f01f71
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-28 09:04:32 -0400

    Update antigrav.sh

--- scripts/antigrav.sh
+++ scripts/antigrav.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 ## This should show what lines are not making it through gravity.sh
 
+## Vars
+source /etc/piholeparser/scripts/scriptvars/staticvariables.var
+CURRENTUSER="$(whoami)"
+GRAVITY=/etc/pihole/gravity.list
+GRAVITYSH=/etc/.pihole/gravity.sh
+ANTIGRAV="/home/"$CURRENTUSER"/antigrav.list
+
 ## whiptail required
 WHATITIS=whiptail
 WHATPACKAGE=whiptail
@@ -15,23 +22,23 @@ fi
 
 ## Remove old antigrav
 if
-ls $CHECKME &> /dev/null;
+ls $ANTIGRAV &> /dev/null;
 then
-rm $CHECKME
+rm $ANTIGRAV
 else
 :
 fi
 
-## Vars
-source /etc/piholeparser/scripts/scriptvars/staticvariables.var
-CURRENTUSER="$(whoami)"
-GRAVITY=/etc/pihole/gravity.list
-ANTIGRAV="/home/"$CURRENTUSER"/antigrav.list
-HOSTIP=$(whiptail --inputbox "What IP Needs to be removed?" 10 80 "192.168.1.99" 3>&1 1>&2 2>&3)
+## Update Gravity
+# pihole -g
+bash $GRAVITYSH
 
 ## Trim IP from HOSTS format
+HOSTIP=$(whiptail --inputbox "What IP Needs to be removed?" 10 80 "192.168.1.99" 3>&1 1>&2 2>&3)
 sed "s/^$HOSTIP\s\+[ \t]*//" < $GRAVITY > $TEMPFILE
 
 ## WhatDiff
-sudo gawk 'NR==FNR{a[$0];next} !($0 in a)' $BIGAPLE $TEMPFILE > $ANTIGRAV
-sudo rm $TEMPFILE
+gawk 'NR==FNR{a[$0];next} !($0 in a)' $BIGAPLE $TEMPFILE > $ANTIGRAV
+HOWMANYLINES=$(echo -e "`wc -l $ANTIGRAV | cut -d " " -f 1`")
+printf "$yellow"  "Antigrav File contains $HOWMANYLINES Domains taht are not used by gravity."
+rm $TEMPFILE
