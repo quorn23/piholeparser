4e0341a82b0efa6052c660a9948bd1fac11d3aed
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-28 13:16:27 -0400

    Update runpiholeparser.sh

--- scripts/runpiholeparser.sh
+++ scripts/runpiholeparser.sh
@@ -62,13 +62,17 @@ LISTBASENAMETXT="$BASEFILENAME".txt
 echo "$LISTBASENAMETXT" | tee --append $FILETEMP &>/dev/null
 done
 ls $PARSEDDIR > $TEMPFILE
-cat $TEMPFILE | sed '/README.md.txt/d' > $TEMPOFILE
+cat $TEMPFILE | sed '/README.md/d' > $TEMPOFILE
 gawk 'NR==FNR{a[$0];next} !($0 in a)' $FILETEMP $TEMPOFILE > $TEMPOFILEB
 for source in `cat $TEMPOFILEB`;
 do
 REMPARSEDFILE="$PARSEDDIR""$source"
+if
+[[ $source == *.txt ]]
+then
 rm $REMPARSEDFILE
 printf "$red"    "The $source .lst No Longer Exists. Parsed File Deleted."
+fi
 done
 bash $DELETETEMPFILE
 echo ""
