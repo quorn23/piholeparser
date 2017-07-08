08b3067c11b0ae5afa984735e5e2c5dba406f343
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-08 11:58:00 -0400

    Update tempvars.sh

--- scripts/miniscripts/tempvars.sh
+++ scripts/miniscripts/tempvars.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ## This Recreates The Temporary Variables
 
+## Variables
+source /etc/piholeparser/scripts/scriptvars/staticvariables.var
+
 CHECKME=$TEMPVARS
 if
 ls $CHECKME &> /dev/null;
@@ -8,7 +11,4 @@ then
 rm $CHECKME
 fi
 
-
 echo "## Vars that we don't keep" | tee --append $TEMPVARS &>/dev/null
-
-source $TEMPVARS
