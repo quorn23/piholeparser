928de2ef1f1981914ac0e5393ab896968182cb71
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-08 19:45:49 -0400

    Update domaintlds.sh

--- scripts/miniscripts/domaintlds.sh
+++ scripts/miniscripts/domaintlds.sh
@@ -39,7 +39,7 @@ rm $TEMPFILEN
 done
 
 cat $TEMPFILEM | sed '/[/]/d; /\#\+/d; s/\s\+$//; /^$/d; /[[:blank:]]/d; /[.]/d' > $TEMPFILEL
-cat $TEMPFILEL | sed 's/.*/\L\1/g' > $TEMPFILEK
+cat $TEMPFILEL | sed 's/\([A-Z]\)/\L\1/g' > $TEMPFILEK
 cat -s $TEMPFILEK | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $VALIDDOMAINTLD
 rm $TEMPFILEL
 rm $TEMPFILEK
