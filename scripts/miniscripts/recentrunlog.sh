62af44bc5bf2253e3e39b8d74ddbb7b5b9de9ee9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-08 12:03:41 -0400

    Create recentrunlog.sh

---
+++ scripts/miniscripts/recentrunlog.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## This Recreates Recent Run Log
+
+## Variables
+source /etc/piholeparser/scripts/scriptvars/staticvariables.var
+
+if 
+ls $RECENTRUN &> /dev/null; 
+then
+rm $RECENTRUN
+echo "## $SCRIPTTEXT $timestamp" | tee --append $RECENTRUN &>/dev/null
+echo "* Recent Run Log Removed and Recreated." | tee --append $RECENTRUN &>/dev/null
+else
+echo "## $SCRIPTTEXT $timestamp" | tee --append $RECENTRUN &>/dev/null
+echo "* Recent Run Log Created." | tee --append $RECENTRUN &>/dev/null
+fi
+
+echo "* $STARTTIME" | tee --append $RECENTRUN &>/dev/null
