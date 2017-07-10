410079f9fc2cb5f60cdb453f993a98dbe76e772f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-10 13:24:51 -0400

    Update oldparseddelete.sh

--- scripts/miniscripts/oldparseddelete.sh
+++ scripts/miniscripts/oldparseddelete.sh
@@ -1,29 +1,56 @@
 #!/bin/bash
 ## This Removes Parsed Files That We No Longer Need
 
-## Variables
+## Static Variables
 source /etc/piholeparser/scripts/scriptvars/staticvariables.var
 
+printf "$cyan"    "Making List of All .lst Files."
 for f in $EVERYLISTFILEWILDCARD
 do
+
+## Dynamic Variables
 source /etc/piholeparser/scripts/scriptvars/dynamicvariables.var
-TEMPOFILE="$TEMPDIR"TEMPOFILE.txt
-TEMPOFILEB="$TEMPDIR"TEMPOFILEB.txt
-LISTBASENAMETXT="$BASEFILENAME".txt
+
+## Make List Of Every .lst file
 echo "$LISTBASENAMETXT" | tee --append $FILETEMP &>/dev/null
+
 done
+echo ""
 
+printf "$cyan"    "Making List Of Parsed Files."
+
+## Write contents of parsed directory to a file
 ls $PARSEDDIR > $TEMPFILE
-cat $TEMPFILE | sed '/README.md/d' > $TEMPOFILE
-gawk 'NR==FNR{a[$0];next} !($0 in a)' $FILETEMP $TEMPOFILE > $TEMPOFILEB
 
-for source in `cat $TEMPOFILEB`;
+## Ignore readme.md
+cat $TEMPFILE | sed '/README.md/d' > $TEMPFILEN
+echo ""
+
+printf "$cyan"    "Comparing Lists."
+
+## Compare
+gawk 'NR==FNR{a[$0];next} !($0 in a)' $FILETEMP $TEMPFILEN > $TEMPFILEM
+rm $TEMPFILE
+rm $FILETEMP
+rm $TEMPFILEN
+echo ""
+
+for source in `cat $TEMPFILEM`;
 do
+
 REMPARSEDFILE="$PARSEDDIR""$source"
+
 if
 [[ $source == *.txt ]]
 then
 rm $REMPARSEDFILE
-printf "$red"    "The $source .lst No Longer Exists. Parsed File Deleted."
+echo "* $source" | tee --append $TEMPFILEL &>/dev/null
+echo "* The $source .lst No Longer Exists. Parsed File Deleted." | tee --append $RECENTRUN &>/dev/null
 fi
+
 done
+
+HOWMANYPARSEDDELETED=$(echo -e "`wc -l $TEMPFILEL | cut -d " " -f 1`")
+printf "$red"    "$HOWMANYPARSEDDELETED Lists Deleted."
+rm $TEMPFILEM
+rm $TEMPFILEL
