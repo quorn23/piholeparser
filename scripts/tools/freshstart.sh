1234f19f453eaf2868cafe34e933d10b3051fc38
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-12 10:53:52 -0400

    Update freshstart.sh

--- scripts/tools/freshstart.sh
+++ scripts/tools/freshstart.sh
@@ -3,23 +3,66 @@
 ## Variables
 source /etc/piholeparser/scripts/scriptvars/staticvariables.var
 
-## Clean Mirror Folder
-rm $MIRROREDLISTSALL
+#########################
+## Clean Mirror Folder ##
+#########################
+echo "Cleaning Mirror Directory"
+CHECKME=$MIRROREDLISTSALL
+if
+ls $CHECKME &> /dev/null;
+then
+rm $CHECKME
+fi
+echo ""
 
-## Clean Parsed Folder
-rm $PARSEDLISTSALL
+#########################
+## Clean Parsed Folder ##
+#########################
+echo "Cleaning Parsed Directory"
+CHECKME=$PARSEDLISTSALL
+if
+ls $CHECKME &> /dev/null;
+then
+rm $CHECKME
+fi
+echo ""
 
-## Revert Killed Lists
+#########################
+## Revert Killed Lists ##
+#########################
+echo "Reverting Killed Lists"
 for f in $KILLTHELISTALL
 do
-BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
-mv $f "$MAINLISTSDIR""$BASEFILENAME".lst
+source /etc/piholeparser/scripts/scriptvars/dynamicvariables.var
+mv $f $BMAINPARSELIST
 done
+echo ""
 
-## Push Changes up to Github
+#####################
+## Clean TLD Lists ##
+#####################
+echo "Cleaning TLD Lists"
+CHECKME=$CLEANTLDS
+if
+ls $CHECKME &> /dev/null;
+then
+rm $CHECKME
+fi
+
+CHECKME=$VALIDDOMAINTLDBKUP
+if
+ls $CHECKME &> /dev/null;
+then
+rm $CHECKME
+fi
+echo ""
+
+###############################
+## Push Changes up to Github ##
+###############################
 timestamp=$(echo `date`)
 WHYYOUDODIS=$(whiptail --inputbox "Why are you doing a manual push?" 10 80 " $timestamp" 3>&1 1>&2 2>&3)
-printf "$green"   "Pushing Lists to Github"
+echo "Pushing Lists to Github"
 git -C $REPODIR pull
 git -C $REPODIR remote set-url origin $GITWHERETOPUSH
 git -C $REPODIR add .
