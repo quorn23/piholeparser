5381403349fdb2fede38d550814df0112edfd730
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-12 18:01:04 -0400

    Create 01-Creating-Main-Recent-Run-Log.sh

---
+++ scripts/housekeepingscripts/01-Creating-Main-Recent-Run-Log.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+## This Recreates Recent Run Log
+
+## Variables
+source /etc/piholeparser/scripts/scriptvars/staticvariables.var
+
+
+SCRIPTTEXT="Creating Main Recent Run Log."
+timestamp=$(echo `date`)
+
+if 
+ls $RECENTRUN &> /dev/null; 
+then
+rm $RECENTRUN
+printf "$red" "Old Log Purged."
+fi
+
+echo "### $SCRIPTTEXT $timestamp" | tee --append $RECENTRUN &>/dev/null
+echo "* Recent Run Log Recreated." | tee --append $RECENTRUN &>/dev/null
+
+if 
+ls $RECENTRUN &> /dev/null; 
+then
+printf "$yellow" "Recent Run Log Recreated."
+fi
