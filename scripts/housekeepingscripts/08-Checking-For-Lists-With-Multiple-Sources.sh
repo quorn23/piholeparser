99c664ca23b69178889f1d5a7e83a94f6a0d8ce9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-17 14:40:13 -0400

    Create 08-Checking-For-Lists-With-Multiple-Sources.sh

---
+++ scripts/housekeepingscripts/08-Checking-For-Lists-With-Multiple-Sources.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+## This will help log it better
+##########
+############
+########## Fix me soon
+
+## Variables
+script_dir=$(dirname $0)
+source "$script_dir"/../scriptvars/staticvariables.var
+
+## Process Every .lst file within the List Directories
+for f in $EVERYLISTFILEWILDCARD
+do
+
+BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
+
+## Amount of sources greater than one?
+timestamp=$(echo `date`)
+HOWMANYLINES=$(echo -e "`wc -l $f | cut -d " " -f 1`")
+if
+[[ "$HOWMANYLINES" -gt 1 ]]
+then
+echo "* $BASEFILENAME Has $HOWMANYLINES sources. $timestamp" | tee --append $RECENTRUN &>/dev/null
+#printf "$yellow"    "$BASEFILENAME Has $HOWMANYLINES Sources."
+elif
+[[ "$HOWMANYLINES" -le 1 ]]
+then
+echo ""
+#printf "$yellow"    "$BASEFILENAME Has Only One Source."
+fi
+
+done
