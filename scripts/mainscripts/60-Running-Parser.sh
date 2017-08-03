7b3bf30977b80fb623e5f46eb8cea09bdd2c2061
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-02 20:34:05 -0400

    Update 60-Running-Parser.sh

--- scripts/mainscripts/60-Running-Parser.sh
+++ scripts/mainscripts/60-Running-Parser.sh
@@ -567,16 +567,14 @@ for source in `cat $VALIDDOMAINTLD`;
 do
 HOWMANYTIMESTLD=$(echo -e "`grep -o [.]$source\$ $BFILETEMP | wc -l`")
 WHATLINENUMBER=$(echo -e "`grep -n $source $VALIDDOMAINTLD | cut -d : -f 1`")
-#TLDPERCENTAGEMATH=$(awk "BEGIN { pc=100*${WHATLINENUMBER}/${HOWMANYVALIDTLD}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
 TLDPERCENTAGEMATH=$(echo -e "$WHATLINENUMBER*100/$HOWMANYVALIDTLD" | bc)
-TLDPERCENTAGE=$(printf "%03d\n" $TLDPERCENTAGEMATH)
 if
 [[ "$HOWMANYTIMESTLD" != 0 ]]
 then
 cat $BFILETEMP | grep -e [.]$source\$ >> $BTEMPFILE
 touch $BTEMPFILE
 fi
-echo -ne "Percent Done Is $TLDPERCENTAGE \r"
+echo -ne "Percent Done Is $TLDPERCENTAGEMATH \r"
 done
 touch $BTEMPFILE
 gawk 'NR==FNR{a[$0];next} !($0 in a)' $BTEMPFILE $BFILETEMP >> $TRYNACATCHFIlES
