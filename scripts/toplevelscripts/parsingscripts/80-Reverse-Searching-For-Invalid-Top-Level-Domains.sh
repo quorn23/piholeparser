7ef9a33d6946ec4b086ba67ab71e2f52afefe638
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 10:09:27 -0400

    Update 80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh

--- scripts/toplevelscripts/parsingscripts/80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh
+++ scripts/toplevelscripts/parsingscripts/80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-## 
+## Invalid TLD's
 
 ## Variables
 script_dir=$(dirname $0)
 source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
+
+printf "$yellow"  "This Process Normally Takes Longer Than The Others."
+HOWMANYVALIDTLD=$(echo -e "`wc -l $VALIDDOMAINTLD | cut -d " " -f 1`")
+for source in `cat $VALIDDOMAINTLD`;
+do
+HOWMANYTIMESTLD=$(echo -e "`grep -o [.]$source\$ $BFILETEMP | wc -l`")
+WHATLINENUMBER=$(echo -e "`grep -n $source $VALIDDOMAINTLD | cut -d : -f 1`")
+TLDPERCENTAGEMATH=$(awk "BEGIN { pc=100*${WHATLINENUMBER}/${HOWMANYVALIDTLD}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
+if
+[[ "$HOWMANYTIMESTLD" != 0 ]]
+then
+cat $BFILETEMP | grep -e [.]$source\$ >> $BTEMPFILE
+touch $BTEMPFILE
+fi
+echo -ne "Percent Done Is $TLDPERCENTAGEMATH \r"
+done
+touch $BTEMPFILE
+gawk 'NR==FNR{a[$0];next} !($0 in a)' $BTEMPFILE $BFILETEMP >> $TRYNACATCHFIlES
