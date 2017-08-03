d3527811545f375aabf18296a3a01139d58394ac
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 13:29:00 -0400

    Update 80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh

--- scripts/toplevelscripts/parsingscripts/80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh
+++ scripts/toplevelscripts/parsingscripts/80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh
@@ -7,7 +7,7 @@ source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
 
-printf "$yellow"  "This Process Normally Takes Longer Than The Others."
+printf "$yellow"  "This Process Normally Takes Longer Than The Others. Percentage Should Be Below."
 HOWMANYVALIDTLD=$(echo -e "`wc -l $VALIDDOMAINTLD | cut -d " " -f 1`")
 
 for source in `cat $VALIDDOMAINTLD`;
@@ -15,7 +15,6 @@ do
 
 WHATLINENUMBER=$(echo -e "`grep -n $source $VALIDDOMAINTLD | cut -d : -f 1`")
 TLDPERCENTAGEMATH=$(awk "BEGIN { pc=100*${WHATLINENUMBER}/${HOWMANYVALIDTLD}; i=int(pc); print (pc-i<0.5)?i:i+1}" )
-TLDPERCENTAGE=$(echo $TLDPERCENTAGEMATH)
 
 HOWMANYTIMESTLD=$(echo -e "`grep -o [.]$source\$ $BFILETEMP | wc -l`")
 if
@@ -25,7 +24,8 @@ cat $BFILETEMP | grep -e [.]$source\$ >> $BTEMPFILE
 touch $BTEMPFILE
 fi
 
-echo -ne "Percent Done Is "$TLDPERCENTAGE". \r"
+#echo -ne "$TLDPERCENTAGEMATH \r"
+echo -ne "$TLDPERCENTAGEMATH"
 
 done
 
