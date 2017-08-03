87250d5a43e552a8f131e4d529b276c66a6aff69
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 10:02:21 -0400

    Update 20-Removing-Lines-With-Comments.sh

--- scripts/toplevelscripts/parsingscripts/20-Removing-Lines-With-Comments.sh
+++ scripts/toplevelscripts/parsingscripts/20-Removing-Lines-With-Comments.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-## 
+## Comments #'s and !'s .'s
 
 ## Variables
 script_dir=$(dirname $0)
 source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
+
+cat $BFILETEMP | sed '/\#\+/d; /\!\+/d; /^[.]/d' | grep -v '\^.' > $BTEMPFILE
