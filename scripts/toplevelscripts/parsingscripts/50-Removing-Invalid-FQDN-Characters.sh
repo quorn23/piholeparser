5282dde58d1f27ada36d49909618b1f7abae9c80
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 11:13:13 -0400

    Update 50-Removing-Invalid-FQDN-Characters.sh

--- scripts/toplevelscripts/parsingscripts/50-Removing-Invalid-FQDN-Characters.sh
+++ scripts/toplevelscripts/parsingscripts/50-Removing-Invalid-FQDN-Characters.sh
@@ -5,7 +5,7 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../../../scriptvars/staticvariables.var
+source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
 
