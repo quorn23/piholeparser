60cddae6473af434db03b63f312907ff95e37769
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 11:13:41 -0400

    Update 90-Removing-Duplicate-Lines.sh

--- scripts/toplevelscripts/parsingscripts/90-Removing-Duplicate-Lines.sh
+++ scripts/toplevelscripts/parsingscripts/90-Removing-Duplicate-Lines.sh
@@ -3,7 +3,7 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../../../scriptvars/staticvariables.var
+source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
 
