1df4bffd3e252ade811aa5d6ac0617d4df024c5e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 10:12:40 -0400

    Update 90-Removing-Duplicate-Lines.sh

--- scripts/toplevelscripts/parsingscripts/90-Removing-Duplicate-Lines.sh
+++ scripts/toplevelscripts/parsingscripts/90-Removing-Duplicate-Lines.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-## 
+## Duplicate Removal
 
 ## Variables
 script_dir=$(dirname $0)
 source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
+
+cat -s $BFILETEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $BTEMPFILE
