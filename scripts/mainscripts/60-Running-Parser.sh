69b16ac8331059a75dacdab7863f29600eb16150
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 08:41:02 -0400

    Update 60-Running-Parser.sh

--- scripts/mainscripts/60-Running-Parser.sh
+++ scripts/mainscripts/60-Running-Parser.sh
@@ -567,8 +567,7 @@ for source in `cat $VALIDDOMAINTLD`;
 do
 HOWMANYTIMESTLD=$(echo -e "`grep -o [.]$source\$ $BFILETEMP | wc -l`")
 WHATLINENUMBER=$(echo -e "`grep -n $source $VALIDDOMAINTLD | cut -d : -f 1`")
-#TLDPERCENTAGEMATH=$(awk "BEGIN { pc=100*${WHATLINENUMBER}/${HOWMANYVALIDTLD}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
-TLDPERCENTAGEMATH=$(echo -e `"scale=2; ${WHATLINENUMBER}*100/${HOWMANYVALIDTLD}" | bc` &>/dev/null)
+TLDPERCENTAGEMATH=$(awk "BEGIN { pc=100*${WHATLINENUMBER}/${HOWMANYVALIDTLD}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
 if
 [[ "$HOWMANYTIMESTLD" != 0 ]]
 then
