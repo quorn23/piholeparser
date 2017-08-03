9353fc05f376c0aecce80309bea384ed07b514ca
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 10:04:24 -0400

    Update 50-Removing-Invalid-FQDN-Characters.sh

--- scripts/toplevelscripts/parsingscripts/50-Removing-Invalid-FQDN-Characters.sh
+++ scripts/toplevelscripts/parsingscripts/50-Removing-Invalid-FQDN-Characters.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-## 
+## Invalid Characters
+## FQDN's  can only have . _ and -
+## apparently you can have an emoji domain name?
 
 ## Variables
 script_dir=$(dirname $0)
 source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
+
+cat $BFILETEMP | sed '/,/d; s/"/'\''/g; /\"\//d; /[+]/d; /[\]/d; /[/]/d; /[<]/d; /[>]/d; /[?]/d; /[*]/d; /[@]/d; /~/d; /[`]/d; /[=]/d; /[:]/d; /[;]/d; /[%]/d; /[&]/d; /[(]/d; /[)]/d; /[$]/d; /\[\//d; /\]\//d; /[{]/d; /[}]/d; /[][]/d; /\^\//d; s/^||//; /[|]/d' > $BTEMPFILE
