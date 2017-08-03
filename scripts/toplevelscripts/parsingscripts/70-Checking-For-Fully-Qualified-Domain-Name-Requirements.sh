cf196f5fdf49fdcc6b5475db15fcce463d196d18
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 10:06:21 -0400

    Update 70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh

--- scripts/toplevelscripts/parsingscripts/70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh
+++ scripts/toplevelscripts/parsingscripts/70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-## 
+## Domain Requirements,, a period and a letter
 
 ## Variables
 script_dir=$(dirname $0)
 source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
+
+cat $BFILETEMP | sed '/[a-z]/!d; /[.]/!d; /[a-z]$/!d; /^[.]/d' | grep -v '\^.' > $BTEMPFILE
