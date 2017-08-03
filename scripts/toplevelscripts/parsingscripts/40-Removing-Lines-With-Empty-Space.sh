20252e22b4b20882fdeca889337f3fc5432920b1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 10:03:36 -0400

    Update 40-Removing-Lines-With-Empty-Space.sh

--- scripts/toplevelscripts/parsingscripts/40-Removing-Lines-With-Empty-Space.sh
+++ scripts/toplevelscripts/parsingscripts/40-Removing-Lines-With-Empty-Space.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-## 
+## Empty Space
 
 ## Variables
 script_dir=$(dirname $0)
 source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
+
+cat $BFILETEMP | sed 's/\s\+$//; /^$/d; /[[:blank:]]/d' > $BTEMPFILE
