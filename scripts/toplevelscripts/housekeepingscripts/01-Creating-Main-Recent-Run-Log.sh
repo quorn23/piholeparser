185e2c337f930665e2712a8d4241ae7baba93a7b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-03 08:50:21 -0400

    Create 01-Creating-Main-Recent-Run-Log.sh

---
+++ scripts/toplevelscripts/housekeepingscripts/01-Creating-Main-Recent-Run-Log.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+## This Recreates Recent Run Log
+
+## Variables
+script_dir=$(dirname $0)
+source "$script_dir"/../scriptvars/staticvariables.var
+
+SCRIPTTEXT="Creating Main Recent Run Log."
+timestamp=$(echo `date`)
+
+if 
+ls $RECENTRUN &> /dev/null; 
+then
+rm $RECENTRUN
+printf "$red" "Old Log Purged."
+fi
+
+echo "## Running Housekeeping Tasks $timestamp" | tee --append $RECENTRUN &>/dev/null
+echo "### $SCRIPTTEXT $timestamp" | tee --append $RECENTRUN &>/dev/null
+echo "* Recent Run Log Recreated." | tee --append $RECENTRUN &>/dev/null
+
+if 
+ls $RECENTRUN &> /dev/null; 
+then
+printf "$yellow" "Recent Run Log Recreated."
+fi
