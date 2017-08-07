05962c523de13134efe32a730e8da07184a49650
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-07 16:03:23 -0400

    Create 35-Trimming-HOSTS-Format-IP-Addresses.sh

---
+++ scripts/toplevelscripts/parsingscripts/35-Trimming-HOSTS-Format-IP-Addresses.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## Content Filtering and IP addresses
+
+## Variables
+script_dir=$(dirname $0)
+source "$script_dir"/../../scriptvars/staticvariables.var
+source $TEMPVARS
+source $DYNOVARS
+
+cat $BFILETEMP | sed 's/^PRIMARY\s\+[ \t]*//; s/^localhost\s\+[ \t]*//; s/blockeddomain.hosts\s\+[ \t]*//; s/^0.0.0.0\s\+[ \t]*//; s/^127.0.0.1\s\+[ \t]*//; s/^::1\s\+[ \t]*//' > $BTEMPFILE
