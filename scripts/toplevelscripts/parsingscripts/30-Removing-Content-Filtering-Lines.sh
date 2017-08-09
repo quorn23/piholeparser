5a5bc2a5df83345b1bc3de41a78499943d00d898
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-08 20:36:40 -0400

    Update 30-Removing-Content-Filtering-Lines.sh

--- scripts/toplevelscripts/parsingscripts/30-Removing-Content-Filtering-Lines.sh
+++ scripts/toplevelscripts/parsingscripts/30-Removing-Content-Filtering-Lines.sh
@@ -7,4 +7,4 @@ source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
 
-cat $TEMPFILEL | sed '/^https\?:\/\//d; /^http\?:\/\//d; /third-party$/d; /popup$/d' > $BTEMPFILE
+cat $TEMPFILEL | sed '/^https\?:\/\//d; /^http\?:\/\//d; /third-party$/d; /popup$/d' > $TEMPFILEM
