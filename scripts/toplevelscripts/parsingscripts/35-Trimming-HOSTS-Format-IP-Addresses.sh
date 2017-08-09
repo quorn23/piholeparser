20d623356218ec769bca97b1c81d9e66b63da0e0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-08 20:37:44 -0400

    Update 35-Trimming-HOSTS-Format-IP-Addresses.sh

--- scripts/toplevelscripts/parsingscripts/35-Trimming-HOSTS-Format-IP-Addresses.sh
+++ scripts/toplevelscripts/parsingscripts/35-Trimming-HOSTS-Format-IP-Addresses.sh
@@ -7,4 +7,4 @@ source "$script_dir"/../../scriptvars/staticvariables.var
 source $TEMPVARS
 source $DYNOVARS
 
-cat $TEMPFILEL | sed 's/^PRIMARY\s\+[ \t]*//; s/^localhost\s\+[ \t]*//; s/blockeddomain.hosts\s\+[ \t]*//; s/^0.0.0.0\s\+[ \t]*//; s/^127.0.0.1\s\+[ \t]*//; s/^::1\s\+[ \t]*//' > $BTEMPFILE
+cat $TEMPFILEL | sed 's/^PRIMARY\s\+[ \t]*//; s/^localhost\s\+[ \t]*//; s/blockeddomain.hosts\s\+[ \t]*//; s/^0.0.0.0\s\+[ \t]*//; s/^127.0.0.1\s\+[ \t]*//; s/^::1\s\+[ \t]*//' > $TEMPFILEM
