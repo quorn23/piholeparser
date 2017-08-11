000a6d400f64b54acd7459c2482c9c432f0d3193
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 10:31:01 -0400

    Update 20-Calculating-Total-Runtime-Of-Script.sh

--- scripts/toplevelscripts/cleanupscripts/20-Calculating-Total-Runtime-Of-Script.sh
+++ scripts/toplevelscripts/cleanupscripts/20-Calculating-Total-Runtime-Of-Script.sh
@@ -11,5 +11,10 @@ ENDTIMESTAMP=$(date +"%s")
 DIFFTIMESEC=`expr $ENDTIMESTAMP - $STARTTIMESTAMP`
 DIFFTIME=`expr $DIFFTIMESEC / 60`
 TOTALRUNTIME="Script Took $DIFFTIME minutes To Filter $HOWMANYSOURCELISTS Lists."
+
 printf "$yellow"   "$TOTALRUNTIME"
+
 echo "* $TOTALRUNTIME" | tee --append $RECENTRUN &>/dev/null
+
+echo "ENDTIME='"$ENDTIME"'" | tee --append $TEMPVARS &>/dev/null
+echo "TOTALRUNTIME='"$TOTALRUNTIME"'" | tee --append $TEMPVARS &>/dev/null
