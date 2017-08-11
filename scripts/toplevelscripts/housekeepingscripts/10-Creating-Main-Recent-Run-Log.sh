dcb53c5f1a85aaf8fad17a0191ad6933e79b51f7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 15:23:00 -0400

    Update 10-Creating-Main-Recent-Run-Log.sh

--- scripts/toplevelscripts/housekeepingscripts/10-Creating-Main-Recent-Run-Log.sh
+++ scripts/toplevelscripts/housekeepingscripts/10-Creating-Main-Recent-Run-Log.sh
@@ -3,7 +3,16 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../../scriptvars/staticvariables.var
+SCRIPTVARSDIR="$script_dir"/../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
 
 SCRIPTTEXT="Creating Main Recent Run Log."
 timestamp=$(echo `date`)
