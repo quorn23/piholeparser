d23390fddd556bb970345b9c875726c5e67229b7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:44:36 -0400

    Update 90-Removing-Duplicate-Lines.sh

--- scripts/toplevelscripts/parsingscripts/90-Removing-Duplicate-Lines.sh
+++ scripts/toplevelscripts/parsingscripts/90-Removing-Duplicate-Lines.sh
@@ -3,8 +3,30 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../../scriptvars/staticvariables.var
+STATICVARS="$script_dir"/../../scriptvars/staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPVARS ]]
+then
 source $TEMPVARS
+else
+echo "Temp Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $DYNOVARS ]]
+then
 source $DYNOVARS
+else
+echo "Temp Vars File Missing, Exiting."
+exit
+fi
 
 cat -s $TEMPFILEL | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILEM
