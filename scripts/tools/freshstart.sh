4ec9f2d1b3c5ec49b1a9771c47cbfe55d1c1eaba
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:36:06 -0400

    Update freshstart.sh

--- scripts/tools/freshstart.sh
+++ scripts/tools/freshstart.sh
@@ -2,7 +2,15 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../scriptvars/staticvariables.var
+STATICVARS="$script_dir"/../scriptvars/staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
 
 ## whiptail required
 WHATITIS=whiptail
@@ -54,7 +62,16 @@ then
 for f in $KILLTHELISTALL
 do
 BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
+# Dynamic Variables
+BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
+if
+[[ -f $DYNOVARS ]]
+then
 source $DYNOVARS
+else
+echo "Dynamic Vars File Missing, Exiting."
+exit
+fi
 mv $f "$MAINLISTDIR""$BASEFILENAME".lst
 done
 fi
