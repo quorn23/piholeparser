d002278bed3b7d8ac67cda6c812346e5964fbf9b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:51:07 -0400

    Update 80-Checking-For-Lists-With-Multiple-Sources.sh

--- scripts/toplevelscripts/housekeepingscripts/80-Checking-For-Lists-With-Multiple-Sources.sh
+++ scripts/toplevelscripts/housekeepingscripts/80-Checking-For-Lists-With-Multiple-Sources.sh
@@ -6,7 +6,15 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../../scriptvars/staticvariables.var
+STATICVARS="$script_dir"/../../scriptvars/staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
 
 ## Process Every .lst file within the List Directories
 for f in $EVERYLISTFILEWILDCARD
