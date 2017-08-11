deefef9fe22369a6ddd5a88b768dd2aa098d2b48
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:50:30 -0400

    Update 40-Identifying-Lists-Without-https.sh

--- scripts/toplevelscripts/housekeepingscripts/40-Identifying-Lists-Without-https.sh
+++ scripts/toplevelscripts/housekeepingscripts/40-Identifying-Lists-Without-https.sh
@@ -12,15 +12,6 @@ else
 echo "Static Vars File Missing, Exiting."
 exit
 fi
-if
-[[ -f $TEMPVARS ]]
-then
-source $TEMPVARS
-else
-echo "Temp Vars File Missing, Exiting."
-exit
-fi
-
 if 
 ls $NOHTTPSLISTS &> /dev/null; 
 then
