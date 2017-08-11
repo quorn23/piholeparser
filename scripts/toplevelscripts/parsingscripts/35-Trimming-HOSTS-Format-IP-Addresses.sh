46ba944ef52c88b9c5d2492abd780caa4d2579d4
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:54:26 -0400

    Update 35-Trimming-HOSTS-Format-IP-Addresses.sh

--- scripts/toplevelscripts/parsingscripts/35-Trimming-HOSTS-Format-IP-Addresses.sh
+++ scripts/toplevelscripts/parsingscripts/35-Trimming-HOSTS-Format-IP-Addresses.sh
@@ -12,21 +12,5 @@ else
 echo "Static Vars File Missing, Exiting."
 exit
 fi
-if
-[[ -f $TEMPVARS ]]
-then
-source $TEMPVARS
-else
-echo "Temp Vars File Missing, Exiting."
-exit
-fi
-if
-[[ -f $DYNOVARS ]]
-then
-source $DYNOVARS
-else
-echo "Temp Vars File Missing, Exiting."
-exit
-fi
 
 cat $TEMPFILEL | sed 's/^PRIMARY\s\+[ \t]*//; s/^localhost\s\+[ \t]*//; s/blockeddomain.hosts\s\+[ \t]*//; s/^0.0.0.0\s\+[ \t]*//; s/^127.0.0.1\s\+[ \t]*//; s/^::1\s\+[ \t]*//' > $TEMPFILEM
