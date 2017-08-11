63ac1897b1cb53705567315e5ceac787d74b7c42
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:44:48 -0400

    Update 10-Calculating-Average-Parsing-Time.sh

--- scripts/toplevelscripts/endtaskscripts/10-Calculating-Average-Parsing-Time.sh
+++ scripts/toplevelscripts/endtaskscripts/10-Calculating-Average-Parsing-Time.sh
@@ -3,7 +3,31 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../../scriptvars/staticvariables.var
+STATICVARS="$script_dir"/../../scriptvars/staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPVARS ]]
+then
+source $TEMPVARS
+else
+echo "Temp Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $DYNOVARS ]]
+then
+source $DYNOVARS
+else
+echo "Temp Vars File Missing, Exiting."
+exit
+fi
 
 AVERAGEPARSETIMESEC=`awk '{ total += $1; count++ } END { print total/count }' $PARSEAVERAGEFILE`
 AVERAGEPARSETIME=`expr $AVERAGEPARSETIMESEC / 60`
