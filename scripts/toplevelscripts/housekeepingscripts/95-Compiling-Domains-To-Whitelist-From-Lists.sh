26b03ae2c30b2505b87fec8fa59cb332e8573816
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:51:28 -0400

    Update 95-Compiling-Domains-To-Whitelist-From-Lists.sh

--- scripts/toplevelscripts/housekeepingscripts/95-Compiling-Domains-To-Whitelist-From-Lists.sh
+++ scripts/toplevelscripts/housekeepingscripts/95-Compiling-Domains-To-Whitelist-From-Lists.sh
@@ -3,7 +3,15 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../../scriptvars/staticvariables.var
+STATICVARS="$script_dir"/../../scriptvars/staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
 
 ## Quick File Check
 WHATITIS="Whitelist File"
