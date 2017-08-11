a383752fd24e93b4a634aafa5d63a85a8f9d8cb7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 09:57:16 -0400

    Update and rename 96-Processing-Blacklists.sh to 97-Processing-Blacklists.sh

---
+++ scripts/toplevelscripts/housekeepingscripts/97-Processing-Blacklists.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+## This sets up blacklisting
+
+## Variables
+script_dir=$(dirname $0)
+source "$script_dir"/../../scriptvars/staticvariables.var
+
+## Sort And Dedupe Lists
+for f in $BLACKLISTDOMAINSALL
+do
+BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
+source $DYNOVARS
+timestamp=$(echo `date`)
+cat -s $f | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $BBLACKTEMP
+rm $f
+mv $BBLACKTEMP $f
+done
+
+## Merge Lists
+BLACKSORTDEDUPE="Merging the Blacklists for Later."
+WHATLISTSMERGE="$BLACKLISTDOMAINSALL"
+timestamp=$(echo `date`)
+printf "$yellow"  "Processed $BLACKSORTDEDUPE"
+cat -s $WHATLISTSMERGE | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $BLACKLISTTEMP
+HOWMANYLINES=$(echo -e "`wc -l $BLACKLISTTEMP | cut -d " " -f 1` Lines In File")
+printf "$yellow"  "$HOWMANYLINES"
+
+echo "* "$BLACKSORTDEDUPE"." | tee --append $RECENTRUN &>/dev/null
+echo "* $HOWMANYLINES" | tee --append $RECENTRUN &>/dev/null
+echo "" | tee --append $RECENTRUN &>/dev/null
