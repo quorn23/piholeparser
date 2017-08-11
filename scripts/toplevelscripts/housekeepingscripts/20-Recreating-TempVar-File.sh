d89fb9c5119d66642524e8f8cfc1178d7bc2fd2d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:47:56 -0400

    Update 20-Recreating-TempVar-File.sh

--- scripts/toplevelscripts/housekeepingscripts/20-Recreating-TempVar-File.sh
+++ scripts/toplevelscripts/housekeepingscripts/20-Recreating-TempVar-File.sh
@@ -3,7 +3,23 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../../scriptvars/staticvariables.var
+STATICVARS="$script_dir"/../../scriptvars/staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPVARS ]]
+then
+source $TEMPVARS
+else
+echo "Temp Vars File Missing, Exiting."
+exit
+fi
 
 CHECKME=$TEMPVARS
 if
