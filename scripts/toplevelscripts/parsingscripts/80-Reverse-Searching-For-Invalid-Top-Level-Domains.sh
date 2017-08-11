1a618541cc8df7264cf6ea16046c094ecbb50df1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:44:27 -0400

    Update 80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh

--- scripts/toplevelscripts/parsingscripts/80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh
+++ scripts/toplevelscripts/parsingscripts/80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh
@@ -3,9 +3,32 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../../scriptvars/staticvariables.var
+STATICVARS="$script_dir"/../../scriptvars/staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPVARS ]]
+then
 source $TEMPVARS
+else
+echo "Temp Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $DYNOVARS ]]
+then
 source $DYNOVARS
+else
+echo "Temp Vars File Missing, Exiting."
+exit
+fi
+
 HOWMANYVALIDTLD=$(echo -e "`wc -l $VALIDDOMAINTLDBKUP | cut -d " " -f 1`")
 
 printf "$yellow"  "This Process Normally Takes Longer Than The Others."
