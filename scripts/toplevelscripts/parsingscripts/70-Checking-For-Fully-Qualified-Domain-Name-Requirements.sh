cd9273a233ce0e2a37a37271bcf46420a34633a1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:44:13 -0400

    Update 70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh

--- scripts/toplevelscripts/parsingscripts/70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh
+++ scripts/toplevelscripts/parsingscripts/70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh
@@ -3,8 +3,30 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../../scriptvars/staticvariables.var
+STATICVARS="$script_dir"/../../scriptvars/staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPVARS ]]
+then
 source $TEMPVARS
+else
+echo "Temp Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $DYNOVARS ]]
+then
 source $DYNOVARS
+else
+echo "Temp Vars File Missing, Exiting."
+exit
+fi
 
 cat $TEMPFILEL | sed '/[a-z]/!d; /[.]/!d; /[a-z]$/!d; /^[.]/d' | grep -v '\^.' > $TEMPFILEM
