6f7ab29e915024a547465083c6bbe7133f2ef4ef
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:50:21 -0400

    Update 30-Setting-Start-Time-Variable.sh

--- scripts/toplevelscripts/housekeepingscripts/30-Setting-Start-Time-Variable.sh
+++ scripts/toplevelscripts/housekeepingscripts/30-Setting-Start-Time-Variable.sh
@@ -12,15 +12,6 @@ else
 echo "Static Vars File Missing, Exiting."
 exit
 fi
-if
-[[ -f $TEMPVARS ]]
-then
-source $TEMPVARS
-else
-echo "Temp Vars File Missing, Exiting."
-exit
-fi
-
 
 timestamp=$(echo `date`)
 STARTTIME="Script Started At $timestamp"
