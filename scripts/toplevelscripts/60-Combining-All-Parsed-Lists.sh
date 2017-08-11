2a7db19d4826947f306b3b988cc4fbbaf8647859
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:38:42 -0400

    Update 60-Combining-All-Parsed-Lists.sh

--- scripts/toplevelscripts/60-Combining-All-Parsed-Lists.sh
+++ scripts/toplevelscripts/60-Combining-All-Parsed-Lists.sh
@@ -4,7 +4,15 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../scriptvars/staticvariables.var
+STATICVARS="$script_dir"/../scriptvars/staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
 
 WHATITIS="All Parsed List"
 CHECKME=$BIGAPL
