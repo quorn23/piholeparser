069fb6e34f24a77281582f074dda0e3ceabcf8f6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:55:33 -0400

    Update 90-Removing-Duplicate-Lines.sh

--- scripts/toplevelscripts/parsingscripts/90-Removing-Duplicate-Lines.sh
+++ scripts/toplevelscripts/parsingscripts/90-Removing-Duplicate-Lines.sh
@@ -12,21 +12,5 @@ else
 echo "Static Vars File Missing, Exiting."
 exit
 fi
-if
-[[ -f $TEMPVARS ]]
-then
-source $TEMPVARS
-else
-echo "Temp Vars File Missing, Exiting."
-exit
-fi
-if
-[[ -f $DYNOVARS ]]
-then
-source $DYNOVARS
-else
-echo "Temp Vars File Missing, Exiting."
-exit
-fi
 
 cat -s $TEMPFILEL | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILEM
