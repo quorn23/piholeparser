c63027089c625fdff7429ed0d7333a41af34765e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 10:25:38 -0400

    Update 10-Calculating-Average-Parsing-Time.sh

--- scripts/toplevelscripts/cleanupscripts/10-Calculating-Average-Parsing-Time.sh
+++ scripts/toplevelscripts/cleanupscripts/10-Calculating-Average-Parsing-Time.sh
@@ -7,6 +7,16 @@ source "$script_dir"/../../scriptvars/staticvariables.var
 
 AVERAGEPARSETIMESEC=`awk '{ total += $1; count++ } END { print total/count }' $PARSEAVERAGEFILE`
 AVERAGEPARSETIME=`expr $AVERAGEPARSETIMESEC / 60`
+
 printf "$yellow"   "Average Parsing Time Was $AVERAGEPARSETIME Minutes."
+
+echo "AVERAGEPARSETIME='"$AVERAGEPARSETIME"'" | tee --append $TEMPVARS &>/dev/null
+
 echo "* Average Parsing Time Was $AVERAGEPARSETIME Minutes." | tee --append $RECENTRUN &>/dev/null
-rm $PARSEAVERAGEFILE
+
+CHECKME=$PARSEAVERAGEFILE
+if
+ls $CHECKME &> /dev/null;
+then
+rm $CHECKME
+fi
