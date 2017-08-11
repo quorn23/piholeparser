6f458d222dce7d502eb03a314c9854554cc47cfa
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-11 14:55:04 -0400

    Update 70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh

--- scripts/toplevelscripts/parsingscripts/70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh
+++ scripts/toplevelscripts/parsingscripts/70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh
@@ -12,21 +12,5 @@ else
 echo "Static Vars File Missing, Exiting."
 exit
 fi
-if
-[[ -f $TEMPVARS ]]
-then
-source $TEMPVARS
-else
-echo "Temp Vars File Missing, Exiting."
-exit
-fi
-if
-[[ -f $DYNOVARS ]]
-then
-source $DYNOVARS
-else
-echo "Temp Vars File Missing, Exiting."
-exit
-fi
 
 cat $TEMPFILEL | sed '/[a-z]/!d; /[.]/!d; /[a-z]$/!d; /^[.]/d' | grep -v '\^.' > $TEMPFILEM
