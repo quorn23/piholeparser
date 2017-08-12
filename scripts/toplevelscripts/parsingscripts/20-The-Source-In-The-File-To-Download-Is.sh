d47e3ed5f3cd881ccd55ebfc0ce96deeb7b0ef09
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-12 15:05:15 -0400

    Create 20-The-Source-In-The-File-To-Download-Is.sh

---
+++ scripts/toplevelscripts/parsingscripts/20-The-Source-In-The-File-To-Download-Is.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+## 
+
+## Variables
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPPARSEVARS ]]
+then
+source $TEMPPARSEVARS
+else
+echo "Temp Parsing Vars File Missing, Exiting."
+exit
+fi
+
+## Process Every source within the .lst from above
+for source in `cat $FILEBEINGPROCESSED`;
+do
+
+printf "$yellow"    "$source"
+echo ""
+
+done
