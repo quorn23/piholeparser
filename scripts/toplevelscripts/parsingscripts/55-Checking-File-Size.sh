e8776b4e8ebe89243e36d543a18f661c74ed5fa7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-12 18:04:12 -0400

    Create 55-Checking-File-Size.sh

---
+++ scripts/toplevelscripts/parsingscripts/55-Checking-File-Size.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+## 
+
+## Variables
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPPARSEVARS ]]
+then
+source $TEMPPARSEVARS
+else
+echo "Temp Parsing Vars File Missing, Exiting."
+exit
+fi
+
+## set filesizezero variable if empty
+if
+[[ -z $FULLSKIPPARSING ]]
+then
+FETCHFILESIZE=$(stat -c%s "$BORIGINALFILETEMP")
+FETCHFILESIZEMB=`expr $FETCHFILESIZE / 1024 / 1024`
+timestamp=$(echo `date`)
+fi
+if 
+[[ -z $FULLSKIPPARSING && "$FETCHFILESIZE" -eq 0 ]]
+then
+FILESIZEZERO=true
+echo "FILESIZEZERO="$FILESIZEZERO"" | tee --append $TEMPPARSEVARS &>/dev/null
+timestamp=$(echo `date`)
+printf "$red"     "$BASEFILENAME List Was An Empty File After Download."
+echo "* $BASEFILENAME List Was An Empty File After Download. $timestamp" | tee --append $RECENTRUN &>/dev/null
+touch $BORIGINALFILETEMP
+elif
+[[ -z $FULLSKIPPARSING && "$FETCHFILESIZE" -gt 0 ]]
+then
+ORIGFILESIZENOTZERO=true
+HOWMANYLINES=$(echo -e "`wc -l $BORIGINALFILETEMP | cut -d " " -f 1`")
+ENDCOMMENT="$HOWMANYLINES Lines After Download."
+printf "$yellow"  "Size of $BASEFILENAME = $FETCHFILESIZEMB MB."
+printf "$yellow"  "$ENDCOMMENT"
+echo ""
+fi
+
+## Duplicate the downloaded file for the next steps
+touch $BORIGINALFILETEMP
+if
+[[ -f $BORIGINALFILETEMP ]]
+then
+cp $BORIGINALFILETEMP $BTEMPFILE
+cp $BORIGINALFILETEMP $BFILETEMP
+rm $BORIGINALFILETEMP
+fi
