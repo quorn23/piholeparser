721ed255c28a6dc74bcef003fe9a06d8a53e448b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-12 18:30:48 -0400

    Update and rename PLACEHOLDER to 99-Clearing-Temp-Vars.sh

---
+++ scripts/toplevelscripts/parsingscripts/99-Clearing-Temp-Vars.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+## 
+
+## Variables
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPPARSEVARS ]]
+then
+source $TEMPPARSEVARS
+else
+echo "Temp Parsing Vars File Missing, Exiting."
+exit
+fi
+
+if
+[[ -f $TEMPPARSEVARS ]]
+then
+rm $TEMPPARSEVARS
+fi
