38896b229906810093fb85ca48a670aee525a60c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-14 08:12:29 -0400

    Update 70-Combining-All-Parsed-Lists.sh

--- scripts/toplevelscripts/70-Combining-All-Parsed-Lists.sh
+++ scripts/toplevelscripts/70-Combining-All-Parsed-Lists.sh
@@ -4,7 +4,16 @@
 
 ## Variables
 script_dir=$(dirname $0)
-source "$script_dir"/../scriptvars/staticvariables.var
+SCRIPTVARSDIR="$script_dir"/../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
 
 ## Cheap error handling
 touch $BLACKLISTTEMP
