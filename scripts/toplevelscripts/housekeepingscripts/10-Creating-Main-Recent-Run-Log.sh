95035123a18333461f903b3ba998c2f7eb253c30
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-21 14:08:26 -0400

    Update 10-Creating-Main-Recent-Run-Log.sh

--- scripts/toplevelscripts/housekeepingscripts/10-Creating-Main-Recent-Run-Log.sh
+++ scripts/toplevelscripts/housekeepingscripts/10-Creating-Main-Recent-Run-Log.sh
@@ -18,18 +18,25 @@ SCRIPTTEXT="Creating Main Recent Run Log."
 timestamp=$(echo `date`)
 
 if 
-ls $RECENTRUN &> /dev/null; 
+[[ -f $RECENTRUN ]]
 then
 rm $RECENTRUN
 printf "$red" "Old Log Purged."
+RECENTPURGED=true
 fi
 
 echo "## Running Housekeeping Tasks $timestamp" | tee --append $RECENTRUN &>/dev/null
 echo "### $SCRIPTTEXT $timestamp" | tee --append $RECENTRUN &>/dev/null
+
+if
+[[ -n $RECENTPURGED ]]
+then
+echo "* Old Recent Run Log Purged." | tee --append $RECENTRUN &>/dev/null
+fi
 echo "* Recent Run Log Recreated." | tee --append $RECENTRUN &>/dev/null
 
 if 
-ls $RECENTRUN &> /dev/null; 
+[[ -f $RECENTRUN ]] 
 then
 printf "$yellow" "Recent Run Log Recreated."
 fi
