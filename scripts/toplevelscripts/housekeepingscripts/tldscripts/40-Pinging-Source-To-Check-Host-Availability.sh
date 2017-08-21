84c387c4173c4d36271a7b77480737a0aa441445
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-21 15:16:21 -0400

    Create 40-Pinging-Source-To-Check-Host-Availability.sh

---
+++ scripts/toplevelscripts/housekeepingscripts/tldscripts/40-Pinging-Source-To-Check-Host-Availability.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+##
+
+## Variables
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPPARSEVARS ]]
+then
+source $TEMPPARSEVARS
+else
+echo "Temp Parsing Vars File Missing, Exiting."
+exit
+fi
+
+## If $SOURCEDOMAIN is set, Ping it
+if
+[[ -n $SOURCEDOMAIN ]]
+then
+SOURCEIPFETCH=`ping -c 1 $SOURCEDOMAIN | gawk -F'[()]' '/PING/{print $2}'`
+SOURCEIP=`echo $SOURCEIPFETCH`
+echo "SOURCEIP="$SOURCEIP"" | tee --append $TEMPPARSEVARS &>/dev/null
+fi
+if
+[[ -n $SOURCEIP ]]
+then
+printf "$green"    "Ping Test Was A Success!"
+elif
+[[ -z $SOURCEIP ]]
+then
+printf "$red"    "Ping Test Failed."
+PINGTESTFAILED=true
+echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
+fi
