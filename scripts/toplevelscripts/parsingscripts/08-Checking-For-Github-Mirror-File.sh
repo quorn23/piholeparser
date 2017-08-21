445378c91e2e45e4909d1db28eb7ba930b55ae49
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-21 13:24:19 -0400

    Create 08-Checking-For-Github-Mirror-File.sh

---
+++ scripts/toplevelscripts/parsingscripts/08-Checking-For-Github-Mirror-File.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+## 
+
+## Variables
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPPARSEVARS ]]
+then
+source $TEMPPARSEVARS
+else
+echo "Temp Parsing Vars File Missing, Exiting."
+exit
+fi
+
+if
+[[ `wget -S --spider $source  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
+then
+printf "$green"  "Github Mirror Currently Available."
+fi
