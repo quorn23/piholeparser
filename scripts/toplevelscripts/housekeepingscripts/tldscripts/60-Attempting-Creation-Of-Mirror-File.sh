d53d5db14e8831d0caf2c74b1226e521429692f0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-21 16:05:16 -0400

    Update 60-Attempting-Creation-Of-Mirror-File.sh

--- scripts/toplevelscripts/housekeepingscripts/tldscripts/60-Attempting-Creation-Of-Mirror-File.sh
+++ scripts/toplevelscripts/housekeepingscripts/tldscripts/60-Attempting-Creation-Of-Mirror-File.sh
@@ -36,28 +36,19 @@ printf "$green"  "Old Mirror File Removed"
 rm $CURRENTTLDLIST
 fi
 
-## Github has a 100mb limit, and empty files are useless
-if
-[[ -z $FULLSKIPPARSING ]]
-then
-FETCHFILESIZE=$(stat -c%s "$BTEMPFILE")
-FETCHFILESIZEMB=`expr $FETCHFILESIZE / 1024 / 1024`
-timestamp=$(echo `date`)
-fi
 if 
-[[ -z $FULLSKIPPARSING && -n $FILESIZEZERO ]]
-then
-printf "$red"     "Not Creating Mirror File. Nothing To Create!"
-rm $BTEMPFILE
-elif
-[[ -z $FULLSKIPPARSING && -z $FILESIZEZERO && "$FETCHFILESIZEMB" -ge "$GITHUBLIMITMB" ]]
-then
-printf "$red"     "Mirror File Too Large For Github. Deleting."
-echo "* $BASEFILENAME list was $FETCHFILESIZEMB MB, and too large to mirror on github. $timestamp" | tee --append $RECENTRUN &>/dev/null
-rm $BTEMPFILE
-elif
-[[ -z $FULLSKIPPARSING && -z $FILESIZEZERO && "$FETCHFILESIZEMB" -lt "$GITHUBLIMITMB" ]]
+[[ -z $FULLSKIPPARSING && -z $FILESIZEZERO ]]
 then
 printf "$green"  "Creating Mirror Of Unparsed File."
 mv $BTEMPFILE $CURRENTTLDLIST
+elif 
+[[ -z $FULLSKIPPARSING && -n $FILESIZEZERO ]]
+then
+rm $BTEMPFILE
+fi
+
+if
+[[ -f $CURRENTTLDLIST ]]
+then
+cat $CURRENTTLDLIST >> $VALIDDOMAINTLD
 fi
