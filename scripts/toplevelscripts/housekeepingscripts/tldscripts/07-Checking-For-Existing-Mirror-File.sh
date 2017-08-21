d123ebb563f9b366ae4431da6c7aa8fde79e2394
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-21 15:10:40 -0400

    Create 07-Checking-For-Existing-Mirror-File.sh

---
+++ scripts/toplevelscripts/housekeepingscripts/tldscripts/07-Checking-For-Existing-Mirror-File.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+##
+
+## Variables
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPPARSEVARS ]]
+then
+source $TEMPPARSEVARS
+else
+echo "Temp Parsing Vars File Missing, Exiting."
+exit
+fi
+
+if
+[[ -f $CURRENTTLDLIST ]]
+then
+printf "$green"  "Mirror File Currently Available."
+else
+printf "$red"  "Mirror File Currently Unavailable."
+fi
