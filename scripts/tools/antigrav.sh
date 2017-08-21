5e55b6922e1c830c1496e5a7db26c89502ad04c3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-21 19:17:48 -0400

    Update antigrav.sh

--- scripts/tools/antigrav.sh
+++ scripts/tools/antigrav.sh
@@ -15,47 +15,53 @@ exit
 fi
 
 ## Pihole vars
-source /etc/pihole/setupVars.conf
-TRIMMEDIP=${IPV4_ADDRESS%/*}
-CURRENTUSER="$(whoami)"
-GRAVITY=/etc/pihole/gravity.list
-GRAVITYSH=/etc/.pihole/gravity.sh
+PIHOLEDIR=/etc/pihole/
+PIHOLEVARSCONF="$PIHOLEDIR"setupVars.conf
+PIHOLEGRAVITY="$PIHOLEDIR"gravity.list
+PIHOLEGRAVITYSH=/etc/.pihole/gravity.sh
 ANTIGRAV="TEMPDIR"antigrav.list.txt
 
-## whiptail required
-WHATITIS=whiptail
-WHATPACKAGE=whiptail
 if
-which $WHATITIS >/dev/null;
+[[ -f $PIHOLEVARSCONF ]]
+then
+source $PIHOLEVARSCONF
+else
+echo "$PIHOLEVARSCONF missing"
+exit
+fi
+TRIMMEDIP=${IPV4_ADDRESS%/*}
+CURRENTUSER="$(whoami)"
+
+## whiptail required
+if
+which whiptail >/dev/null;
 then
 :
 else
-printf "$yellow"  "Installing $WHATITIS"
-apt-get install -y $WHATPACKAGE
+printf "$yellow"  "Installing whiptail"
+apt-get install -y whiptail
 fi
 
 ## Remove old antigrav
 if
-ls $ANTIGRAV &> /dev/null;
+[[ -f $ANTIGRAV ]]
 then
 rm $ANTIGRAV
-else
-:
 fi
 
 ## Update Gravity
 # pihole -g
-{ if
+if
 (whiptail --title "AntiGrav" --yes-button "No" --no-button "Yes" --yesno "Do you want to run Gravity Now?" 10 80) 
 then
 echo "not running gravity"
 else
-bash $GRAVITYSH
-fi }
+bash $PIHOLEGRAVITYSH
+fi
 
 ## Trim IP from HOSTS format
 HOSTIP=$(whiptail --inputbox "What IP Needs to be removed?" 10 80 "$TRIMMEDIP" 3>&1 1>&2 2>&3)
-sed "s/^$HOSTIP\s\+[ \t]*//" < $GRAVITY > $TEMPFILE
+sed "s/^$HOSTIP\s\+[ \t]*//" < $PIHOLEGRAVITY > $TEMPFILE
 
 ## WhatDiff
 gawk 'NR==FNR{a[$0];next} !($0 in a)' $BIGAPLE $TEMPFILE > $FILETEMP
@@ -64,4 +70,3 @@ cat $FILETEMP | sed 's/\s\+$//; /^$/d; /[[:blank:]]/d' > $ANTIGRAV
 rm $FILETEMP
 HOWMANYLINES=$(echo -e "`wc -l $ANTIGRAV | cut -d " " -f 1`")
 printf "$yellow"  "Antigrav File contains $HOWMANYLINES Domains that are not used by gravity."
-
