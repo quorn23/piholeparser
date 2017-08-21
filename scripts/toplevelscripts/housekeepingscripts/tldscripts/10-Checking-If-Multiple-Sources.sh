d69f54049b366ea26b9d8c149a78a7bd555edf5b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-21 15:12:15 -0400

    Create 10-Checking-If-Multiple-Sources.sh

---
+++ scripts/toplevelscripts/housekeepingscripts/tldscripts/10-Checking-If-Multiple-Sources.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+##
+
+## Variables
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPPARSEVARS ]]
+then
+source $TEMPPARSEVARS
+else
+echo "Temp Parsing Vars File Missing, Exiting."
+exit
+fi
+
+## Amount of sources greater than one?
+HOWMANYLINES=$(echo -e "`wc -l $FILEBEINGPROCESSED | cut -d " " -f 1`")
+if
+[[ "$HOWMANYLINES" -gt 1 ]]
+then
+printf "$yellow"    "$BASEFILENAME Has $HOWMANYLINES Sources."
+fi
