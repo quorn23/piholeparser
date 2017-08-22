ece8f65211f81f57ff4b82e8375188b3f4d5c825
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-22 15:54:41 -0400

    Update 40-Pinging-Source-To-Check-Host-Availability.sh

--- scripts/toplevelscripts/housekeepingscripts/tldscripts/40-Pinging-Source-To-Check-Host-Availability.sh
+++ scripts/toplevelscripts/housekeepingscripts/tldscripts/40-Pinging-Source-To-Check-Host-Availability.sh
@@ -30,6 +30,7 @@ SOURCEIPFETCH=`ping -c 1 $SOURCEDOMAIN | gawk -F'[()]' '/PING/{print $2}'`
 SOURCEIP=`echo $SOURCEIPFETCH`
 echo "SOURCEIP="$SOURCEIP"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
+
 if
 [[ -n $SOURCEIP ]]
 then
@@ -38,6 +39,20 @@ elif
 [[ -z $SOURCEIP ]]
 then
 printf "$red"    "Ping Test Failed."
+PINGTESTFAILEDA=true
+echo "PINGTESTFAILEDA="$PINGTESTFAILEDA"" | tee --append $TEMPPARSEVARS &>/dev/null
+fi
+
+if
+[[ -n $PINGTESTFAILEDA  ]]
+then
+{ if
+[[ `wget -S --spider $source  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
+then
+printf "$green"  "Header Check Successful."
+else
+printf "$red"  "Header Check Unsuccessful."
 PINGTESTFAILED=true
 echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
+fi }
 fi
