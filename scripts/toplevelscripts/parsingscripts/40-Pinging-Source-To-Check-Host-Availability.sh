89d838ac06d929c10bc27b4d594955d6fdace5cb
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-22 14:45:25 -0400

    Update 40-Pinging-Source-To-Check-Host-Availability.sh

--- scripts/toplevelscripts/parsingscripts/40-Pinging-Source-To-Check-Host-Availability.sh
+++ scripts/toplevelscripts/parsingscripts/40-Pinging-Source-To-Check-Host-Availability.sh
@@ -39,6 +39,16 @@ elif
 [[ -z $SOURCEIP ]]
 then
 printf "$red"    "Ping Test Failed."
+PINGTESTFAILEDA=true
+echo "PINGTESTFAILEDA="$PINGTESTFAILEDA"" | tee --append $TEMPPARSEVARS &>/dev/null
+fi
+
+if
+[[ -n $PINGTESTFAILEDA && `wget -S --spider $MIRROREDFILEDL  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
+then
+printf "$green"  "Header Check Successful."
+else
+printf "$red"  "Header Check Unsuccessful."
 PINGTESTFAILED=true
 echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
