bdda7d2e3b343abf2ba81f72d74799a6c956b93b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-23 14:47:56 -0400

    Update 60-Converting-All-Text-To-Lower-Case.sh

--- scripts/toplevelscripts/parsingscripts/actualparsing/60-Converting-All-Text-To-Lower-Case.sh
+++ scripts/toplevelscripts/parsingscripts/actualparsing/60-Converting-All-Text-To-Lower-Case.sh
@@ -22,4 +22,14 @@ echo "Temp Parsing Vars File Missing, Exiting."
 exit
 fi
 
+if
+[[ -f $BFILETEMP ]]
+then
 cat $BFILETEMP | sed 's/\([A-Z]\)/\L\1/g' > $BTEMPFILE
+fi
+
+if
+[[ -f $BFILETEMP ]]
+then
+rm $BFILETEMP
+fi
