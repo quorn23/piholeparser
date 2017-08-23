29bd0f1fbce2a0e6d34fb3ec8a5a2347e51bbef7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-23 08:06:46 -0400

    Update 06-Checking-If-Multiple-Sources.sh

--- scripts/toplevelscripts/parsingscripts/06-Checking-If-Multiple-Sources.sh
+++ scripts/toplevelscripts/parsingscripts/06-Checking-If-Multiple-Sources.sh
@@ -25,7 +25,15 @@ fi
 ## Amount of sources greater than one?
 HOWMANYLINES=$(echo -e "`wc -l $FILEBEINGPROCESSED | cut -d " " -f 1`")
 if
+[[ "$HOWMANYLINES" -le 1 ]]
+then
+printf "$yellow"    "$BASEFILENAME Has $HOWMANYLINES Sources."
+AMOUNTOFSOURCES=$HOWMANYLINES
+echo "AMOUNTOFSOURCES="$AMOUNTOFSOURCES"" | tee --append $TEMPPARSEVARS &>/dev/null
+elif
 [[ "$HOWMANYLINES" -gt 1 ]]
 then
 printf "$yellow"    "$BASEFILENAME Has $HOWMANYLINES Sources."
+AMOUNTOFSOURCES=$HOWMANYLINES
+echo "AMOUNTOFSOURCES="$AMOUNTOFSOURCES"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
