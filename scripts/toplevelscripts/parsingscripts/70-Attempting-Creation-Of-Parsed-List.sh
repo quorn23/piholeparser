61ade20f4f7e00f00344347a87abcca06915e5d9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-23 14:21:57 -0400

    Update 70-Attempting-Creation-Of-Parsed-List.sh

--- scripts/toplevelscripts/parsingscripts/70-Attempting-Creation-Of-Parsed-List.sh
+++ scripts/toplevelscripts/parsingscripts/70-Attempting-Creation-Of-Parsed-List.sh
@@ -22,64 +22,40 @@ echo "Temp Parsing Vars File Missing, Exiting."
 exit
 fi
 
-## if we skipped parsing due to file not changing
-if
-[[ -n $FULLSKIPPARSING && -f $PARSEDFILE ]]
-then
-HOWMANYLINES=$(echo -e "`wc -l $PARSEDFILE | cut -d " " -f 1`")
-printf "$green"  "Old Parsed File Retained."
-printf "$yellow"  "$HOWMANYLINES Lines In File."
-fi
-
 ## Delete Parsed file if current parsing method empties it
 if 
-[[ -z $FULLSKIPPARSING && -n $FILESIZEZERO && -n $ORIGFILESIZENOTZERO && -f $PARSEDFILE ]]
+[[ -f $KILLTHELIST && -f $PARSEDFILE ]]
 then
 printf "$red"  "Current Parsing Method Emptied File. Old File Removed."
 rm $PARSEDFILE
+elif
+[[ ! -f $KILLTHELIST && -f $PARSEDFILE ]]
+then
+printf "$green"  "Old Parsed File Removed."
+rm $PARSEDFILE
 fi
 
+## Remove mirror if we killed the list
 if 
-[[ -z $FULLSKIPPARSING && -n $FILESIZEZERO && -n $ORIGFILESIZENOTZERO && -f $MIRROREDFILE ]]
+[[ -f $KILLTHELIST && -f $MIRROREDFILE ]]
 then
-printf "$red"  "Current Parsing Method Emptied File. Old Mirror File Removed."
+printf "$red"  "Current Parsing Method Emptied File. Mirror File Removed."
 rm $MIRROREDFILE
 fi
 
-
-## let's get rid of the deadweight
-if 
-[[ -z $FULLSKIPPARSING && -n $FILESIZEZERO && -n $ORIGFILESIZENOTZERO ]]
-then
-printf "$red"  "Current Parsing Method Emptied File. It will be skipped in the future."
-echo "* $BASEFILENAME List Was Killed By The Parsing Process. It will be skipped in the future. $timestamp" | tee --append $RECENTRUN &>/dev/null
-mv $FILEBEINGPROCESSED $KILLTHELIST
-fi
-
 ## Github has a 100mb limit, and empty files are useless
-if
-[[ -z $FULLSKIPPARSING ]]
-then
-FETCHFILESIZE=$(stat -c%s "$BTEMPFILE")
-FETCHFILESIZEMB=`expr $FETCHFILESIZE / 1024 / 1024`
-timestamp=$(echo `date`)
-fi
 if 
-[[ -z $FULLSKIPPARSING && -n $FILESIZEZERO ]]
+[[ "$PARSEDFILESIZEBYTES" -eq 0 ]]
 then
-printf "$red"     "Not Creating Parsed File. Nothing To Create!"
-rm $BTEMPFILE
+printf "$red"     "Not Creating Mirror File. Nothing To Create!"
 elif
-[[ -z $FULLSKIPPARSING && -z $FILESIZEZERO && "$FETCHFILESIZEMB" -ge "$GITHUBLIMITMB" ]]
+[[ "$PARSEDFILESIZEMB" -ge "$GITHUBLIMITMB" ]]
 then
-printf "$red"     "Parsed File Too Large For Github. Deleting."
-echo "* $BASEFILENAME list was $FETCHFILESIZEMB MB, and too large to push to github. $timestamp" | tee --append $RECENTRUN &>/dev/null
-rm $BTEMPFILE
+printf "$red"     "Mirror File Too Large For Github."
+echo "* $BASEFILENAME list was $FETCHFILESIZEMB MB, and too large to mirror on github. $timestamp" | tee --append $RECENTRUN &>/dev/null
 elif
-[[ -z $FULLSKIPPARSING && -z $FILESIZEZERO && "$FETCHFILESIZEMB" -lt "$GITHUBLIMITMB" ]]
+[[ "$FETCHFILESIZEMB" -lt "$GITHUBLIMITMB" && -f $BPARSEDFILETEMP ]]
 then
-printf "$yellow"     "Size of $BASEFILENAME = $FETCHFILESIZEMB MB."
-printf "$green"  "Parsed File Completed Succesfully."
-echo "* $BASEFILENAME list was Updated. $timestamp" | tee --append $RECENTRUN &>/dev/null
-mv $BTEMPFILE $PARSEDFILE
+printf "$green"  "Creating Mirror Of Unparsed File."
+cp $BPARSEDFILETEMP $PARSEDFILE
 fi
