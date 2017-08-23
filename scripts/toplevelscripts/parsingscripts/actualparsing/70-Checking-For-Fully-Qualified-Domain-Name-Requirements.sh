18d3331b9f754a70c729882092ed5f5078c8445b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-23 14:48:02 -0400

    Update 70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh

--- scripts/toplevelscripts/parsingscripts/actualparsing/70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh
+++ scripts/toplevelscripts/parsingscripts/actualparsing/70-Checking-For-Fully-Qualified-Domain-Name-Requirements.sh
@@ -22,4 +22,14 @@ echo "Temp Parsing Vars File Missing, Exiting."
 exit
 fi
 
+if
+[[ -f $BFILETEMP ]]
+then
 cat $BFILETEMP | sed '/[a-z]/!d; /[.]/!d; /[a-z]$/!d; /^[.]/d' | grep -v '\^.' > $BTEMPFILE
+fi
+
+if
+[[ -f $BFILETEMP ]]
+then
+rm $BFILETEMP
+fi
