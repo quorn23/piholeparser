46adb1ceed6248aa19b871dfa9b2f63ce42db498
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-24 10:38:06 -0400

    Update 10-Calculating-Average-Parsing-Time.sh

--- scripts/toplevelscripts/endtaskscripts/10-Calculating-Average-Parsing-Time.sh
+++ scripts/toplevelscripts/endtaskscripts/10-Calculating-Average-Parsing-Time.sh
@@ -2,6 +2,7 @@
 ## average parsing time
 
 ## Variables
+SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
 script_dir=$(dirname $0)
 SCRIPTVARSDIR="$script_dir"/../../scriptvars/
 STATICVARS="$SCRIPTVARSDIR"staticvariables.var
@@ -13,6 +14,8 @@ else
 echo "Static Vars File Missing, Exiting."
 exit
 fi
+
+RECENTRUN="$ENDTASKSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".log
 
 if
 [[ -f $PARSEAVERAGEFILE ]]
