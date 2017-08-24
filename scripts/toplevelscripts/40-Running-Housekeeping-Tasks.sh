2bb6dc538663462bb49f8ca3e46af689d663e5d3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-24 10:33:30 -0400

    Update 40-Running-Housekeeping-Tasks.sh

--- scripts/toplevelscripts/40-Running-Housekeeping-Tasks.sh
+++ scripts/toplevelscripts/40-Running-Housekeeping-Tasks.sh
@@ -15,6 +15,8 @@ echo "Static Vars File Missing, Exiting."
 exit
 fi
 
+RECENTRUN="$SPECIFICLOGSDIR""$SCRIPTBASEFILENAME".log
+
 ## Start File Loop
 ## For .sh files In The housekeepingscripts Directory
 for f in $ALLHOUSEKEEPINGSCRIPTS
