b7063333d32b09c0664aebbc25e5abfa8fd8a658
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-24 15:52:25 -0400

    Update 80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh

--- scripts/toplevelscripts/parsingscripts/actualparsing/80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh
+++ scripts/toplevelscripts/parsingscripts/actualparsing/80-Reverse-Searching-For-Invalid-Top-Level-Domains.sh
@@ -35,7 +35,9 @@ do
 #WHATLINENUMBER=$(echo -e "`grep -n $source $VALIDDOMAINTLD | cut -d : -f 1`")
 WHATLINENUMBER=$(echo "`grep -n $source $VALIDDOMAINTLD | cut -d : -f 1`")
 #TLDPERCENTAGEMATH=$(awk "BEGIN { pc=100*${WHATLINENUMBER}/${HOWMANYVALIDTLD}; i=int(pc); print (pc-i<0.5)?i:i+1}" )
-TLDPERCENTAGEMATH=$(echo `awk "BEGIN { pc=100*${WHATLINENUMBER}/${HOWMANYVALIDTLD}; i=int(pc); print (pc-i<0.5)?i:i+1}"`)
+#TLDPERCENTAGEMATH=$(echo `awk "BEGIN { pc=100*${WHATLINENUMBER}/${HOWMANYVALIDTLD}; i=int(pc); print (pc-i<0.5)?i:i+1}"`)
+TLDPERCENTAGEMATHA=`expr $WHATLINENUMBER / $HOWMANYVALIDTLD `
+TLDPERCENTAGEMATH=`expr $TLDPERCENTAGEMATHA * 100 `
 
 HOWMANYTIMESTLD=$(echo -e "`grep -o [.]$source\$ $BFILETEMP | wc -l`")
 if
