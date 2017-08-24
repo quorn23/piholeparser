c8c2e8bfbdef6b08abb1b820d7321472dee02f15
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-24 10:38:35 -0400

    Update 44-Removing-Temporary-Blacklist.sh

--- scripts/toplevelscripts/endtaskscripts/44-Removing-Temporary-Blacklist.sh
+++ scripts/toplevelscripts/endtaskscripts/44-Removing-Temporary-Blacklist.sh
@@ -2,6 +2,7 @@
 ## Remove TempBlacks
 
 ## Variables
+SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
 script_dir=$(dirname $0)
 SCRIPTVARSDIR="$script_dir"/../../scriptvars/
 STATICVARS="$SCRIPTVARSDIR"staticvariables.var
@@ -13,6 +14,8 @@ else
 echo "Static Vars File Missing, Exiting."
 exit
 fi
+
+RECENTRUN="$ENDTASKSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".log
 
 CHECKME=$BLACKLISTTEMP
 if
