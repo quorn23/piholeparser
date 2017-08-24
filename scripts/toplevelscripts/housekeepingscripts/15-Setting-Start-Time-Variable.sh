bd590475e36ed03587950272720dc86afb895a5e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-24 10:44:44 -0400

    Update 15-Setting-Start-Time-Variable.sh

--- scripts/toplevelscripts/housekeepingscripts/15-Setting-Start-Time-Variable.sh
+++ scripts/toplevelscripts/housekeepingscripts/15-Setting-Start-Time-Variable.sh
@@ -2,6 +2,7 @@
 ## Sets The Beginning Script Time
 
 ## Variables
+SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
 script_dir=$(dirname $0)
 SCRIPTVARSDIR="$script_dir"/../../scriptvars/
 STATICVARS="$SCRIPTVARSDIR"staticvariables.var
@@ -13,6 +14,8 @@ else
 echo "Static Vars File Missing, Exiting."
 exit
 fi
+
+RECENTRUN="$HOUSEKEEPINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".log
 
 timestamp=$(echo `date`)
 STARTTIME="Script Started At $timestamp"
