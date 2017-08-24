d850eac788d495203e54b005489bd23695b30296
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-24 10:26:16 -0400

    Update 30-Checking-Dependencies.sh

--- scripts/toplevelscripts/30-Checking-Dependencies.sh
+++ scripts/toplevelscripts/30-Checking-Dependencies.sh
@@ -4,6 +4,7 @@
 ## it installs the program
 
 ## Variables
+SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
 script_dir=$(dirname $0)
 SCRIPTVARSDIR="$script_dir"/../scriptvars/
 STATICVARS="$SCRIPTVARSDIR"staticvariables.var
