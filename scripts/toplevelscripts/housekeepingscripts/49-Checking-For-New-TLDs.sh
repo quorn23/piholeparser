3d90a01f760bc0cc5f711c1f52d840ea06b02179
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-24 09:59:27 -0400

    Create 49-Checking-For-New-TLDs.sh

---
+++ scripts/toplevelscripts/housekeepingscripts/49-Checking-For-New-TLDs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+## This checks for new tld's
+
+## Variables
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+
+## Anything New?
+if
+[[ -f $VALIDDOMAINTLDBKUP && -f $VALIDDOMAINTLD ]]
+then
+gawk 'NR==FNR{a[$0];next} !($0 in a)' $VALIDDOMAINTLDBKUP $VALIDDOMAINTLD > $TLDCOMPARED
+HOWMANYTLDNEW=$(echo -e "`wc -l $TLDCOMPARED | cut -d " " -f 1`")
+fi
+
+if
+[[ "$HOWMANYTLDNEW" != 0 ]]
+then
+printf "$yellow"    "$HOWMANYTLDNEW New TLD's."
+echo "* $HOWMANYTLDNEW New TLD's" | tee --append $RECENTRUN &>/dev/null
+else
+printf "$yellow"    "No New TLD's"
+fi
+
+## Backup TLD list if not there
+if
+[[ -f $VALIDDOMAINTLDBKUP ]]
+then
+rm $VALIDDOMAINTLDBKUP
+fi
+cp $VALIDDOMAINTLD $VALIDDOMAINTLDBKUP
