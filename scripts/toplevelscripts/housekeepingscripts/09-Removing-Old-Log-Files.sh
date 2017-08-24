89acbba451a98d07283321c21646d7a945b4ec6a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-24 10:00:26 -0400

    Create 09-Removing-Old-Log-Files.sh

---
+++ scripts/toplevelscripts/housekeepingscripts/09-Removing-Old-Log-Files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+## This Recreates The Valid TLD file
+
+## Variables
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+
+
+CHECKME=$CLEANLOGS
+if
+ls $CHECKME &> /dev/null;
+then
+rm $CHECKME
+fi
+echo ""
