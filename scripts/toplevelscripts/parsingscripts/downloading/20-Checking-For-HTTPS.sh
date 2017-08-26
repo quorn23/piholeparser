f6f16949d6301a1f7d2ac243a39cc2c30b8a8a74
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-26 16:01:18 -0400

    Create 20-Checking-For-HTTPS.sh

---
+++ scripts/toplevelscripts/parsingscripts/downloading/20-Checking-For-HTTPS.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+## This  just displays the source on screen
+
+## Variables
+SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPPARSEVARS ]]
+then
+source $TEMPPARSEVARS
+else
+echo "Temp Parsing Vars File Missing, Exiting."
+exit
+fi
+
+## Is source not using https
+if
+[[ $source != https* ]]
+then
+printf "$red"    "$BASEFILENAME List Does NOT Use https."
+else
+printf "$green"    "$BASEFILENAME List Does Use https."
+fi
