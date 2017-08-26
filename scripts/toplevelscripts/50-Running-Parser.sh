a444989b023e0816eebcd023d75846e48884ce23
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-26 14:56:12 -0400

    Update 50-Running-Parser.sh

--- scripts/toplevelscripts/50-Running-Parser.sh
+++ scripts/toplevelscripts/50-Running-Parser.sh
@@ -15,7 +15,7 @@ echo "Static Vars File Missing, Exiting."
 exit
 fi
 
-RECENTRUN="$SPECIFICLOGSDIR""$SCRIPTBASEFILENAME".log
+RECENTRUN="$TOPLEVELLOGSDIR""$SCRIPTBASEFILENAME".log
 
 ## Process Every .lst file within the List Directories
 for f in $EVERYLISTFILEWILDCARD
