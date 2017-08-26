f7b6ba58633cb60271e8daafbaa96fb3eabf6c88
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-26 15:50:39 -0400

    Update 55-Checking-File-Size.sh

--- scripts/toplevelscripts/parsingscripts/55-Checking-File-Size.sh
+++ scripts/toplevelscripts/parsingscripts/55-Checking-File-Size.sh
@@ -23,8 +23,6 @@ echo "Temp Parsing Vars File Missing, Exiting."
 exit
 fi
 
-RECENTRUN="$PARSINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".log
-
 ## set filesizezero variable if empty
 if
 [[ -f $BORIGINALFILETEMP ]]
