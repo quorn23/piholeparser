8459c475679c1ecdc08339a2086e01e14fe76aa3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-26 15:49:20 -0400

    Update 10-Checking-For-Github-Mirror-File.sh

--- scripts/toplevelscripts/parsingscripts/10-Checking-For-Github-Mirror-File.sh
+++ scripts/toplevelscripts/parsingscripts/10-Checking-For-Github-Mirror-File.sh
@@ -23,8 +23,6 @@ echo "Temp Parsing Vars File Missing, Exiting."
 exit
 fi
 
-RECENTRUN="$PARSINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".log
-
 if
 [[ `wget -S --spider $MIRROREDFILEDL  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
 then
