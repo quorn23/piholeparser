10d03b4b360df8e5319bdd65cf870116578b85bf
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-26 15:43:32 -0400

    Update 45-Checking-If-List-Updated-Online.sh

--- scripts/toplevelscripts/housekeepingscripts/tldscripts/45-Checking-If-List-Updated-Online.sh
+++ scripts/toplevelscripts/housekeepingscripts/tldscripts/45-Checking-If-List-Updated-Online.sh
@@ -23,8 +23,6 @@ echo "Temp Parsing Vars File Missing, Exiting."
 exit
 fi
 
-RECENTRUN="$TLDSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".log
-
 ## Check if file is modified since last download
 if 
 [[ -f $CURRENTTLDLIST && -z $PINGTESTFAILED ]]
