99e4b30d6505c8e70fef92ee5a2963fd3fea5282
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-28 16:57:46 -0400

    Update and rename 75-Removing-Temporary-Variables.sh to 75-Removing-Temporary-Script-Files.sh

--- scripts/toplevelscripts/endtaskscripts/75-Removing-Temporary-Script-Files.sh
+++ scripts/toplevelscripts/endtaskscripts/75-Removing-Temporary-Script-Files.sh
@@ -31,3 +31,25 @@ ls $CHECKME &> /dev/null;
 then
 rm $CHECKME
 fi
+
+
+CHECKME=$TEMPCLEANUP
+if
+ls $CHECKME &> /dev/null;
+then
+rm $CHECKME
+fi
+
+CHECKME=$TEMPCLEANUPB
+if
+ls $CHECKME &> /dev/null;
+then
+rm $CHECKME
+fi
+
+CHECKME=$TEMPCLEANUPC
+if
+ls $CHECKME &> /dev/null;
+then
+rm $CHECKME
+fi
