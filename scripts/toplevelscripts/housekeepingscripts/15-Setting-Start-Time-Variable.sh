5b8dac941d51ff7cce87333fb8ecc1c374aacf17
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-28 09:14:40 -0400

    Update 15-Setting-Start-Time-Variable.sh

--- scripts/toplevelscripts/housekeepingscripts/15-Setting-Start-Time-Variable.sh
+++ scripts/toplevelscripts/housekeepingscripts/15-Setting-Start-Time-Variable.sh
@@ -27,6 +27,6 @@ STARTTIMESTAMP=$(date +"%s")
 echo "STARTIMEVAR='"$STARTIMEVAR"'" | tee --append $TEMPVARS &>/dev/null
 echo "STARTTIMESTAMP=$STARTTIMESTAMP" | tee --append $TEMPVARS &>/dev/null
 
-printf "$yellow" "Script Started At "$STARTTIME"."
+printf "$yellow" "Script Started At $STARTTIME"
 
 echo "Start Time Set To $timestamp" | tee --append $RECENTRUN &>/dev/null
