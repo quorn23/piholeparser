b23ccee2ee9ca044a7c8a971da5da530e05526c6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-28 15:41:28 -0400

    Update 65-Calculating-TLD-Percentages.sh

--- scripts/toplevelscripts/endtaskscripts/65-Calculating-TLD-Percentages.sh
+++ scripts/toplevelscripts/endtaskscripts/65-Calculating-TLD-Percentages.sh
@@ -39,12 +39,17 @@ if
 [[ "$HOWMANYTIMESTLD" != 0 ]]
 then
 TLDPERCENTAGERESULT=$(echo `awk "BEGIN { pc=100*${HOWMANYTIMESTLD}/${BIGAPLHOWMANY}; i=int(pc); print (pc-i<0.5)?i:i+1}"`)
+{ if
+[[ "$TLDPERCENTAGERESULT" != 0 ]]
+then
 echo "$TLDPERCENTAGERESULT Percent ."$source"" | tee --append $TEMPFILEN &>/dev/null
+fi }
 fi
 
 echo -ne "$TLDPERCENTAGEMATH \r"
+
 done
 
-echo "________________________________________________"" | tee --append $RECENTRUN &>/dev/null
+echo "________________________________________________" | tee --append $RECENTRUN &>/dev/null
 cat -s $TEMPFILEN | sort -n > $TEMPFILEM
 tac $TEMPFILEM >> $RECENTRUN
