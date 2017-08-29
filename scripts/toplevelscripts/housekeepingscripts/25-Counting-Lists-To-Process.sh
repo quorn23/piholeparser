0fe320cc08f0066b84ac4c90e4ebd1686a10c21a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-28 20:03:03 -0400

    Update 25-Counting-Lists-To-Process.sh

--- scripts/toplevelscripts/housekeepingscripts/25-Counting-Lists-To-Process.sh
+++ scripts/toplevelscripts/housekeepingscripts/25-Counting-Lists-To-Process.sh
@@ -22,15 +22,28 @@ if
 then
 rm $BIGAPLSOURCE
 printf "$red"    "Purging Old Source List."
-echo "Old Source List Purged." | tee --append $RECENTRUN &>/dev/null
+echo "* Old Multisource List Purged." | tee --append $RECENTRUN &>/dev/null
 fi
 
 ## Collect all the sources into one file
 printf "$cyan"    "Merging Sources."
-cat $EVERYLISTFILEWILDCARD | sort > $TEMPFILE
+CHECKME=$EVERYLISTFILEWILDCARD
+if
+ls $CHECKME &> /dev/null;
+then
+cat $EVERYLISTFILEWILDCARD | sort | sed '/^$/d' >> $BIGAPLSOURCE
+else
+touch $BIGAPLSOURCE
+fi
 
 ## Math Time
-HOWMANYSOURCELISTS=$(echo -e "`wc -l $TEMPFILE | cut -d " " -f 1`")
+if
+[[ -f $BIGAPLSOURCE ]]
+then
+HOWMANYSOURCELISTS=$(echo -e "`wc -l $BIGAPLSOURCE | cut -d " " -f 1`")
+else
+HOWMANYSOURCELISTS="unknown amount"
+fi
 HOWMANYSOURCE="$HOWMANYSOURCELISTS Lists Processed By The Script."
 
 ## Save to Tempvars
@@ -38,10 +51,7 @@ echo "HOWMANYSOURCELISTS='"$HOWMANYSOURCELISTS"'" | tee --append $TEMPVARS &>/de
 echo "HOWMANYSOURCE='"$HOWMANYSOURCE"'" | tee --append $TEMPVARS &>/dev/null
 
 ## Log Activity
-echo "* $HOWMANYSOURCE" | tee --append $RECENTRUN &>/dev/null
+echo "* $HOWMANYSOURCE Lists To Be Processed." | tee --append $RECENTRUN &>/dev/null
 
 ## Terminal Display
-printf "$yellow"    "$HOWMANYSOURCE"
-
-## Remove Empty Lines
-cat $TEMPFILE | sed '/^$/d' > $BIGAPLSOURCE
+printf "$yellow"    "$HOWMANYSOURCE Lists To Be Processed."
