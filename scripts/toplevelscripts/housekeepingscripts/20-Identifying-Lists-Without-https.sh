3c2877400b564259bccbe8928fc0ab79df7ab0be
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-29 10:11:31 -0400

    Update 20-Identifying-Lists-Without-https.sh

--- scripts/toplevelscripts/housekeepingscripts/20-Identifying-Lists-Without-https.sh
+++ scripts/toplevelscripts/housekeepingscripts/20-Identifying-Lists-Without-https.sh
@@ -81,3 +81,68 @@ HOWMANYLISTSWITHOUTHTTPS="No"
 fi
 
 echo "HOWMANYLISTSWITHOUTHTTPS='"$HOWMANYLISTSWITHOUTHTTPS"'" | tee --append $TEMPVARS &>/dev/null
+
+##################
+##   TLD        ##
+##################
+
+SCRIPTTEXT="Checking For Old https-Less File."
+printf "$cyan"    "$SCRIPTTEXT"
+echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
+if 
+[[ -f $NOHTTPSLISTS ]]
+then
+rm $NOHTTPSLISTS
+printf "$red"   "Old https-less List Removed"
+echo "* Old https-less List Purged." | tee --append $RECENTRUN &>/dev/null
+else
+echo "* Old https-less List Does Not Exist." | tee --append $RECENTRUN &>/dev/null
+fi
+echo ""
+
+echo "" | tee --append $RECENTRUN &>/dev/null
+echo "___________________________________________________________________" | tee --append $RECENTRUN &>/dev/null
+
+SCRIPTTEXT="TLD Lists That Do NOT use https."
+printf "$cyan"    "$SCRIPTTEXT"
+echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
+
+## Start File Loop
+## For Every .lst File
+for f in $VALIDDOMAINTLDLINKS
+do
+
+BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
+
+for source in `cat $f`;
+do
+
+if
+[[ $source != https* ]]
+then
+printf "$red"    "$BASEFILENAME"
+echo "* $BASEFILENAME" | tee --append $NOHTTPSLISTS &>/dev/null
+fi
+
+## End Source Loop
+done
+
+## End File Loop
+done
+echo ""
+
+SCRIPTTEXT="Checking List."
+printf "$cyan"    "$SCRIPTTEXT"
+if 
+[[ -f $NOHTTPSLISTS ]]
+then
+printf "$yellow"   "https-less List Recreated."
+HOWMANYLISTSWITHOUTHTTPS=$(echo -e "`wc -l $NOHTTPSLISTS | cut -d " " -f 1`")
+printf "$red"    "$HOWMANYLISTSWITHOUTHTTPS Lists Do NOT Use HTTPS. See Log For Details."
+cat $NOHTTPSLISTS >> $RECENTRUN
+rm $NOHTTPSLISTS
+else
+printf "$green"   "All Lists Use https."
+echo "All TLD Lists Use https." | tee --append $RECENTRUN &>/dev/null
+HOWMANYLISTSWITHOUTHTTPS="No"
+fi
