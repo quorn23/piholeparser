b40d8fd24be70e86b02ed649e442b33675cad08c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-29 09:49:38 -0400

    Update 40-Removing-Old-TLD-File.sh

--- scripts/toplevelscripts/housekeepingscripts/40-Removing-Old-TLD-File.sh
+++ scripts/toplevelscripts/housekeepingscripts/40-Removing-Old-TLD-File.sh
@@ -17,11 +17,16 @@ fi
 
 RECENTRUN="$HOUSEKEEPINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
 
+SCRIPTTEXT="Checking For Old TLD File."
+printf "$cyan"    "$SCRIPTTEXT"
+echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
 if
 [[ -f $VALIDDOMAINTLD ]]
 then
+printf "$cyan"    "Old TLD List Removed."
 rm $VALIDDOMAINTLD
 echo "* Old TLD List Removed." | tee --append $RECENTRUN &>/dev/null
 else
+printf "$cyan"    "Old TLD List Not Present."
 echo "* Old TLD List Not Present." | tee --append $RECENTRUN &>/dev/null
 fi
