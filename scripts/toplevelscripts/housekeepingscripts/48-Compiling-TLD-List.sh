affec4ef987be391fdbbfe71d68a146ccd8c0528
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-28 20:03:41 -0400

    Update 48-Compiling-TLD-List.sh

--- scripts/toplevelscripts/housekeepingscripts/48-Compiling-TLD-List.sh
+++ scripts/toplevelscripts/housekeepingscripts/48-Compiling-TLD-List.sh
@@ -17,29 +17,45 @@ fi
 
 RECENTRUN="$HOUSEKEEPINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
 
+CHECKME=$ALLTLDMLISTSDIR
+if
+ls $CHECKME &> /dev/null;
+then
+cat $ALLTLDMLISTSDIR >> $VALIDDOMAINTLD
+else
+touch $VALIDDOMAINTLD
+fi
+
 if
 [[ -f $TLDCOMPARED ]]
 then
 rm $TLDCOMPARED
+echo "Old TLD Comparison Removed." | sudo tee --append $RECENTRUN &>/dev/null
+else
+echo "Old TLD Comparison Not Present." | sudo tee --append $RECENTRUN &>/dev/null
 fi
 
-## Cleanup File
+printf "$cyan"    "Formatting TLD List."
 if
 [[ -f $VALIDDOMAINTLD ]]
 then
-mv $VALIDDOMAINTLD $TEMPFILEJ
+cat $VALIDDOMAINTLD | sed '/[/]/d; /\#\+/d; s/\s\+$//; /^$/d; /[[:blank:]]/d; /[.]/d; s/\([A-Z]\)/\L\1/g' > $TEMPFILEF
+rm $VALIDDOMAINTLD
 else
-touch $TEMPFILEJ
+touch $TEMPFILEF
 fi
 
-printf "$cyan"    "Formatting And Removing Duplicatates From TLD List."
-
-cat $TEMPFILEJ | sed '/[/]/d; /\#\+/d; s/\s\+$//; /^$/d; /[[:blank:]]/d; /[.]/d; s/\([A-Z]\)/\L\1/g' > $TEMPFILEF
-rm $TEMPFILEJ
-
+printf "$cyan"    "Removing Duplicatates From TLD List."
+if
+[[ -f $TEMPFILEF ]]
+then
 cat -s $TEMPFILEF | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $VALIDDOMAINTLD
 rm $TEMPFILEF
+else
+touch $VALIDDOMAINTLD
+fi
 
 HOWMANYTLD=$(echo -e "`wc -l $VALIDDOMAINTLD | cut -d " " -f 1`")
+echo "HOWMANYTLDTOTAL='"$HOWMANYTLD"'" | tee --append $TEMPVARS &>/dev/null
 printf "$yellow"    "$HOWMANYTLD Valid TLD's Total."
-echo ""
+echo "$HOWMANYTLD Valid TLD's Total." | sudo tee --append $RECENTRUN &>/dev/null
