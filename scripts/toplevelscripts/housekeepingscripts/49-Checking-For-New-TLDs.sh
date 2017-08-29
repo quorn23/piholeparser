92c2e3c77543494310253c7c439871cd2f43cbb5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-29 10:02:19 -0400

    Update 49-Checking-For-New-TLDs.sh

--- scripts/toplevelscripts/housekeepingscripts/49-Checking-For-New-TLDs.sh
+++ scripts/toplevelscripts/housekeepingscripts/49-Checking-For-New-TLDs.sh
@@ -17,23 +17,35 @@ fi
 
 RECENTRUN="$HOUSEKEEPINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
 
+SCRIPTTEXT="Making Backup Copy of TLD List."
+printf "$cyan"    "$SCRIPTTEXT"
+echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
 if
 [[ ! -f $VALIDDOMAINTLDBKUP && -f $VALIDDOMAINTLD ]]
 then
 cp $VALIDDOMAINTLD $VALIDDOMAINTLDBKUP
+elif
+[[ -f $VALIDDOMAINTLDBKUP && -f $VALIDDOMAINTLD ]]
+then
+rm $VALIDDOMAINTLDBKUP
+cp $VALIDDOMAINTLD $VALIDDOMAINTLDBKUP
 fi
+echo ""
 
-## Anything New?
+SCRIPTTEXT="Checking For New TLDs."
+printf "$cyan"    "$SCRIPTTEXT"
+echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
 if
 [[ -f $VALIDDOMAINTLDBKUP && -f $VALIDDOMAINTLD ]]
 then
 gawk 'NR==FNR{a[$0];next} !($0 in a)' $VALIDDOMAINTLDBKUP $VALIDDOMAINTLD > $TLDCOMPARED
-fi
-
-if
+{ if
 [[ -f $TLDCOMPARED ]]
 then
 HOWMANYTLDNEW=$(echo -e "`wc -l $TLDCOMPARED | cut -d " " -f 1`")
+else
+HOWMANYTLDNEW="0"
+fi }
 fi
 
 if
@@ -47,15 +59,3 @@ HOWMANYTLDNEW="No"
 echo "* No New TLD's" | tee --append $RECENTRUN &>/dev/null
 fi
 echo "HOWMANYTLDNEW='"$HOWMANYTLDNEW"'" | tee --append $TEMPVARS &>/dev/null
-
-## Backup TLD list if not there
-if
-[[ -f $VALIDDOMAINTLDBKUP ]]
-then
-rm $VALIDDOMAINTLDBKUP
-fi
-if
-[[ -f $VALIDDOMAINTLD ]]
-then
-cp $VALIDDOMAINTLD $VALIDDOMAINTLDBKUP
-fi
