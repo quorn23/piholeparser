0c5870cc5d34a976b51061e104500d6cba6d3e7a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-31 19:55:25 -0400

    Create fixer.sh

---
+++ scripts/tools/fixer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## Variables
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+
+## whiptail required
+WHATITIS=whiptail
+WHATPACKAGE=whiptail
+if
+which $WHATITIS >/dev/null;
+then
+:
+else
+printf "$yellow"  "Installing $WHATITIS"
+apt-get install -y $WHATPACKAGE
+fi
+
+for f in $EVERYCCTLDOLD
+do
+BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
+BUNDEADPARSELIST="$COUNTRYCODESTLDDIR""$BASEFILENAME".lst
+mv $f $BUNDEADPARSELIST
+done
