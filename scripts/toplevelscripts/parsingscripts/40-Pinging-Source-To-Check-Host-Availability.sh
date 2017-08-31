264c319ebb786cc02649910dea250ab595080ca5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-31 12:06:14 -0400

    Update 40-Pinging-Source-To-Check-Host-Availability.sh

--- scripts/toplevelscripts/parsingscripts/40-Pinging-Source-To-Check-Host-Availability.sh
+++ scripts/toplevelscripts/parsingscripts/40-Pinging-Source-To-Check-Host-Availability.sh
@@ -25,64 +25,68 @@ fi
 
 ## If $SOURCEDOMAIN is set, Ping it
 if
-[[ -n $SOURCEDOMAIN ]]
+[[  -n $SOURCEDOMAIN && `ping -c 1 $SOURCEDOMAIN | gawk -F'[()]' '/PING/{print $2}'` ]]
 then
+printf "$green"    "Ping Test Was A Success!"
 SOURCEIPFETCH=`ping -c 1 $SOURCEDOMAIN | gawk -F'[()]' '/PING/{print $2}'`
 SOURCEIP=`echo $SOURCEIPFETCH`
 echo "SOURCEIP="$SOURCEIP"" | tee --append $TEMPPARSEVARS &>/dev/null
+else
+printf "$red"    "Ping Test Failed."
+SOURCEIP="unknown"
+echo "SOURCEIP="$SOURCEIP"" | tee --append $TEMPPARSEVARS &>/dev/null
+TESTPINGFAILED=true
 fi
+echo ""
 
+## curl Header
 if
 [[ `curl -s -H "$AGENTDOWNLOAD" -o /dev/null -I -w "%{http_code}" $source | grep '200'` ]]
 then
 printf "$green"  "Curl Header Check Successful."
-unset PINGTESTFAILED
 else
 printf "$red"  "Curl Header Check Unsuccessful."
-PINGTESTFAILED=true
-echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
+TESTCURLHEADERFAILED=true
 fi
+echo ""
 
+## wget header
 if
-[[ -n $SOURCEIP ]]
+[[ `wget -S --header="$WGETAGENTDOWNLOAD" --spider $source  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
 then
-printf "$green"    "Ping Test Was A Success!"
-elif
-[[ -z $SOURCEIP ]]
+printf "$green"  "Header Check Successful."
+else
+printf "$red"  "Header Check Unsuccessful."
+TESTWGETHEADERFAILED=true
+fi
+echo ""
+
+## host unavailable
+if
+[[ -n $TESTPINGFAILED && -n $TESTCURLHEADERFAILED && -n $TESTWGETHEADERFAILED ]]
 then
-printf "$red"    "Ping Test Failed."
 PINGTESTFAILED=true
 echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
 
-#if
-#[[ -n $PINGTESTFAILED ]]
-#then
-#{ if
-#[[ `wget -S --header="$WGETAGENTDOWNLOAD" --spider $source  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
-#then
-#printf "$green"  "Header Check Successful."
-#unset PINGTESTFAILED
-#else
-#printf "$red"  "Header Check Unsuccessful."
-#PINGTESTFAILED=true
-#echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
-#fi }
-#fi
-
+## Dead List?
+timestamp=$(echo `date`)
 if
 [[ -n $PINGTESTFAILED && $FILEBEINGPROCESSED != $BDEADPARSELIST ]]
 then
 printf "$red"  "List Marked As Dead."
+echo "* $BASEFILENAME List Marked As Dead. $timestamp" | tee --append $RECENTRUN &>/dev/null
 mv $FILEBEINGPROCESSED $BDEADPARSELIST
-fi
-
-if
+elif
+[[ -n $PINGTESTFAILED && $FILEBEINGPROCESSED == $BDEADPARSELIST ]]
+then
+printf "$red"  "List Already Marked As Dead."
+echo "* $BASEFILENAME List Already Marked As Dead. $timestamp" | tee --append $RECENTRUN &>/dev/null
+elif
 [[ -z $PINGTESTFAILED && $FILEBEINGPROCESSED == $BDEADPARSELIST ]]
 then
 printf "$green"  "List Was Marked As Dead, But Now Works."
 UNDEADLIST=true
 echo "UNDEADLIST="$UNDEADLIST"" | tee --append $TEMPPARSEVARS &>/dev/null
-timestamp=$(echo `date`)
 echo "* $BASEFILENAME List Unavailable To Download. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
