2a07c7ea6bc38ad5877032d89418b87451a07d44
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-01 12:09:12 -0400

    Create 44-Determining-Host-Availability.sh

---
+++ scripts/toplevelscripts/parsingscripts/44-Determining-Host-Availability.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+## is the file available?
+
+## Variables
+SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
+script_dir=$(dirname $0)
+SCRIPTVARSDIR="$script_dir"/../../scriptvars/
+STATICVARS="$SCRIPTVARSDIR"staticvariables.var
+if
+[[ -f $STATICVARS ]]
+then
+source $STATICVARS
+else
+echo "Static Vars File Missing, Exiting."
+exit
+fi
+if
+[[ -f $TEMPPARSEVARS ]]
+then
+source $TEMPPARSEVARS
+else
+echo "Temp Parsing Vars File Missing, Exiting."
+exit
+fi
+
+## host unavailable
+if
+[[ -n $TESTPINGFAILED && -n $TESTCURLHEADERFAILED && -n $TESTWGETHEADERFAILED ]]
+then
+PINGTESTFAILED=true
+echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
+fi
+
+## Dead List?
+timestamp=$(echo `date`)
+if
+[[ -n $PINGTESTFAILED && $FILEBEINGPROCESSED != $BDEADPARSELIST ]]
+then
+printf "$red"  "List Marked As Dead."
+echo "* $BASEFILENAME List Marked As Dead. $timestamp" | tee --append $RECENTRUN &>/dev/null
+mv $FILEBEINGPROCESSED $BDEADPARSELIST
+elif
+[[ -n $PINGTESTFAILED && $FILEBEINGPROCESSED == $BDEADPARSELIST ]]
+then
+printf "$red"  "List Already Marked As Dead."
+echo "* $BASEFILENAME List Already Marked As Dead. $timestamp" | tee --append $RECENTRUN &>/dev/null
+elif
+[[ -z $PINGTESTFAILED && $FILEBEINGPROCESSED == $BDEADPARSELIST ]]
+then
+printf "$green"  "List Was Marked As Dead, But Now Works."
+UNDEADLIST=true
+echo "UNDEADLIST="$UNDEADLIST"" | tee --append $TEMPPARSEVARS &>/dev/null
+echo "* $BASEFILENAME List Unavailable To Download. $timestamp" | tee --append $RECENTRUN &>/dev/null
+fi
