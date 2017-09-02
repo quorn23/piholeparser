7808de1b800a0962213734f0fb11b0d2b0eab3ae
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 11:50:27 -0400

    Update runscripts.sh

--- scripts/runscripts.sh
+++ scripts/runscripts.sh
@@ -2,26 +2,7 @@
 ## This is the central script that ties the others together
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-if
-[[ -f $SUBMAINVAR ]]
-then
-source $SUBMAINVAR
-else
-echo "Sub Main Vars File Missing, Exiting."
-exit
-fi
+source ./foldervars.var
 
 ## Run Logs
 if
