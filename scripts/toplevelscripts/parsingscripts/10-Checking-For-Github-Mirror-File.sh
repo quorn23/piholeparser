bf1923427455901f527774490104fce05d55c662
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 12:15:38 -0400

    Update 10-Checking-For-Github-Mirror-File.sh

--- scripts/toplevelscripts/parsingscripts/10-Checking-For-Github-Mirror-File.sh
+++ scripts/toplevelscripts/parsingscripts/10-Checking-For-Github-Mirror-File.sh
@@ -2,26 +2,7 @@
 ## Checks if github mirror present
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-if
-[[ -f $TEMPPARSEVARS ]]
-then
-source $TEMPPARSEVARS
-else
-echo "Temp Parsing Vars File Missing, Exiting."
-exit
-fi
+source ./foldervars.var
 
 if
 [[ `wget -S --spider $MIRROREDFILEDL  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
