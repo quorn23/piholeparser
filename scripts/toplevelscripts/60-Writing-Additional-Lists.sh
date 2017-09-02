2ba6946f0de68551fd0a6b5e20b2d2e6a87d9634
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 11:52:12 -0400

    Update 60-Writing-Additional-Lists.sh

--- scripts/toplevelscripts/60-Writing-Additional-Lists.sh
+++ scripts/toplevelscripts/60-Writing-Additional-Lists.sh
@@ -2,20 +2,7 @@
 ## This Sets up the writing of additional lists
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-
-RECENTRUN="$TOPLEVELLOGSDIR""$SCRIPTBASEFILENAME".md
+source ./foldervars.var
 
 ## Start File Loop
 ## For .sh files In The listgen Directory
