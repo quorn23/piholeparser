7fd343373142968341a1357578d428eb7a776fbc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 11:34:41 -0400

    Update deletetempfile.sh

--- scripts/toplevelscripts/commonscripts/deletetempfile.sh
+++ scripts/toplevelscripts/commonscripts/deletetempfile.sh
@@ -2,20 +2,7 @@
 ## This should be run alot, to make sure the temp folder doesn't get screwed up
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-
-RECENTRUN="$COMMONSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
+source ./foldervars.var
 
 CHECKME=$TEMPCLEANUP
 if
