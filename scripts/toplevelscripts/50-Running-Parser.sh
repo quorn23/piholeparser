fd92ffcfa6332918fa7f2937aacd33fd0480e1bc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 11:51:56 -0400

    Update 50-Running-Parser.sh

--- scripts/toplevelscripts/50-Running-Parser.sh
+++ scripts/toplevelscripts/50-Running-Parser.sh
@@ -2,29 +2,9 @@
 ## This is the Parsing Process
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-if
-[[ -f $TEMPVARS ]]
-then
-source $TEMPVARS
-else
-echo "Temp Vars File Missing, Exiting."
-exit
-fi
+source ./foldervars.var
 
-RECENTRUN="$TOPLEVELLOGSDIR""$SCRIPTBASEFILENAME".md
-
+## This is a way to skipp parsing
 if
 [[ -n $FULLSKIPPARSING ]]
 then
