1575ebf3c075316eaa79777fe7797a20a18ee68a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 12:11:17 -0400

    Update 48-Compiling-TLD-List.sh

--- scripts/toplevelscripts/housekeepingscripts/48-Compiling-TLD-List.sh
+++ scripts/toplevelscripts/housekeepingscripts/48-Compiling-TLD-List.sh
@@ -2,20 +2,7 @@
 ## This Recreates The Valid TLD file
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-
-RECENTRUN="$HOUSEKEEPINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
+source ./foldervars.var
 
 SCRIPTTEXT="Merging Individual TLD Lists."
 printf "$cyan"    "$SCRIPTTEXT"
