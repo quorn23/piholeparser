7883c3d34bcff35036ac70302c3dcc51e7da8300
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 12:10:49 -0400

    Update 30-Checking-For-Lists-With-Multiple-Sources.sh

--- scripts/toplevelscripts/housekeepingscripts/30-Checking-For-Lists-With-Multiple-Sources.sh
+++ scripts/toplevelscripts/housekeepingscripts/30-Checking-For-Lists-With-Multiple-Sources.sh
@@ -2,20 +2,7 @@
 ## This will log lists with more than one source
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-
-RECENTRUN="$HOUSEKEEPINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
+source ./foldervars.var
 
 ## Process Every .lst file within the List Directories
 for f in $EVERYBLISTFILEWILDCARD
