fe1eaa3c0977f09a3e7c6631daaa32851bc17c7d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 12:10:29 -0400

    Update 20-Identifying-Lists-Without-https.sh

--- scripts/toplevelscripts/housekeepingscripts/20-Identifying-Lists-Without-https.sh
+++ scripts/toplevelscripts/housekeepingscripts/20-Identifying-Lists-Without-https.sh
@@ -2,20 +2,8 @@
 ## This Generates a list of which blacklists don't use https
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
+source ./foldervars.var
 
-RECENTRUN="$HOUSEKEEPINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
 RECENTRUNWITHOUTHTTPSMD="$HOUSEKEEPINGSCRIPTSLOGDIRRAW""$SCRIPTBASEFILENAME".md
 echo "RECENTRUNWITHOUTHTTPSMD='"$RECENTRUNWITHOUTHTTPSMD"'" | tee --append $TEMPVARS &>/dev/null
 
