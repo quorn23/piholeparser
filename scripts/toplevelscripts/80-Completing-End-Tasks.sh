272c45a6e42fa5efd4830031de486f434fe7a733
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 11:52:26 -0400

    Update 80-Completing-End-Tasks.sh

--- scripts/toplevelscripts/80-Completing-End-Tasks.sh
+++ scripts/toplevelscripts/80-Completing-End-Tasks.sh
@@ -2,20 +2,7 @@
 ## This should create the fun info for the run log, and Readme.md
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-
-RECENTRUN="$TOPLEVELLOGSDIR""$SCRIPTBASEFILENAME".md
+source ./foldervars.var
 
 ## Start File Loop
 ## For .sh files In The cleanupscripts Directory
