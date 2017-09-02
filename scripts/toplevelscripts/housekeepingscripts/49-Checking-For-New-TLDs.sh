fc4e73a16a535eb09cf9c8828e2ef40f5cc33f55
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 12:11:26 -0400

    Update 49-Checking-For-New-TLDs.sh

--- scripts/toplevelscripts/housekeepingscripts/49-Checking-For-New-TLDs.sh
+++ scripts/toplevelscripts/housekeepingscripts/49-Checking-For-New-TLDs.sh
@@ -2,20 +2,7 @@
 ## This checks for new tld's
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-
-RECENTRUN="$HOUSEKEEPINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
+source ./foldervars.var
 
 SCRIPTTEXT="Making Backup Copy of TLD List."
 printf "$cyan"    "$SCRIPTTEXT"
