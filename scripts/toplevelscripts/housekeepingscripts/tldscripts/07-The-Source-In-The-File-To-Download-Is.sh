67016fc0ea3315c36b5bd5924c041ca7df86fc79
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 13:45:39 -0400

    Update 07-The-Source-In-The-File-To-Download-Is.sh

--- scripts/toplevelscripts/housekeepingscripts/tldscripts/07-The-Source-In-The-File-To-Download-Is.sh
+++ scripts/toplevelscripts/housekeepingscripts/tldscripts/07-The-Source-In-The-File-To-Download-Is.sh
@@ -2,25 +2,6 @@
 ##
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../../../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-if
-[[ -f $TEMPPARSEVARS ]]
-then
-source $TEMPPARSEVARS
-else
-echo "Temp Parsing Vars File Missing, Exiting."
-exit
-fi
+source ./foldervars.var
 
 printf "$yellow"    "$source"
