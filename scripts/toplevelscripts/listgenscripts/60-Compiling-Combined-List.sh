3a594ada41c6e458555e7f8164ff7e7d24fec30a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 12:13:13 -0400

    Update 60-Compiling-Combined-List.sh

--- scripts/toplevelscripts/listgenscripts/60-Compiling-Combined-List.sh
+++ scripts/toplevelscripts/listgenscripts/60-Compiling-Combined-List.sh
@@ -3,20 +3,7 @@
 ## and merges it all into one
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-
-RECENTRUN="$LISTGENSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
+source ./foldervars.var
 
 WHATITIS="All Parsed List"
 timestamp=$(echo `date`)
