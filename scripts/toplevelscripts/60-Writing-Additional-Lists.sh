83c227ed50c77b02f8de7a6ec210f5dc251447a3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 10:59:57 -0400

    Update 60-Writing-Additional-Lists.sh

--- scripts/toplevelscripts/60-Writing-Additional-Lists.sh
+++ scripts/toplevelscripts/60-Writing-Additional-Lists.sh
@@ -49,7 +49,7 @@ then
 rm $BREPOLOG
 fi
 echo "$MAINREPOFOLDERGITTAG" | tee --append $BREPOLOG &>/dev/null
-echo "$TAGTHEMAINREPOLOG" | tee --append $BREPOLOG &>/dev/null
+echo "$MAINRECENTRUNLOGMDGITTAG" | tee --append $BREPOLOG &>/dev/null
 echo "$TAGTHEUPONEREPOLOG" | tee --append $BREPOLOG &>/dev/null
 echo "____________________________________" | tee --append $BREPOLOG &>/dev/null
 echo "# $BASEFILENAME" | sudo tee --append $BREPOLOG &>/dev/null
