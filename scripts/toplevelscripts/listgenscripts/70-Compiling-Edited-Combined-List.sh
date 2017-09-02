fb8a90a622d793ee0f0abb00ca58f449964f2b98
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 12:13:04 -0400

    Update 70-Compiling-Edited-Combined-List.sh

--- scripts/toplevelscripts/listgenscripts/70-Compiling-Edited-Combined-List.sh
+++ scripts/toplevelscripts/listgenscripts/70-Compiling-Edited-Combined-List.sh
@@ -2,20 +2,7 @@
 ## This creates my custom biglist
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-
-RECENTRUN="$LISTGENSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
+source ./foldervars.var
 
 WHATITIS="All Parsed List (edited)"
 timestamp=$(echo `date`)
