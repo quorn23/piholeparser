3cbe1fa94e9d8d3fc08d45869788ff232404413d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 15:08:53 -0400

    Update 06-Checking-If-Multiple-Sources.sh

--- scripts/TopLevelScripts/TLDlistScripts/06-Checking-If-Multiple-Sources.sh
+++ scripts/TopLevelScripts/TLDlistScripts/06-Checking-If-Multiple-Sources.sh
@@ -2,7 +2,8 @@
 ##
 
 ## Variables
-source ./foldervars.var
+SCRIPTDIRA=$(dirname $0)
+source "$SCRIPTDIRA"/foldervars.var
 
 ## Amount of sources greater than one?
 HOWMANYLINES=$(echo -e "`wc -l $FILEBEINGPROCESSED | cut -d " " -f 1`")
