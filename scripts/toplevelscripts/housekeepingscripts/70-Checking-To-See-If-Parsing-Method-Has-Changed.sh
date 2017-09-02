b981cc3b671e3606fcc0e4e450126c249d86684a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 12:12:04 -0400

    Update 70-Checking-To-See-If-Parsing-Method-Has-Changed.sh

--- scripts/toplevelscripts/housekeepingscripts/70-Checking-To-See-If-Parsing-Method-Has-Changed.sh
+++ scripts/toplevelscripts/housekeepingscripts/70-Checking-To-See-If-Parsing-Method-Has-Changed.sh
@@ -2,20 +2,7 @@
 ## This Checks if parsing method has changed
 
 ## Variables
-SCRIPTBASEFILENAME=$(echo `basename $0 | cut -f 1 -d '.'`)
-script_dir=$(dirname $0)
-SCRIPTVARSDIR="$script_dir"/../../scriptvars/
-STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
-then
-source $STATICVARS
-else
-echo "Static Vars File Missing, Exiting."
-exit
-fi
-
-RECENTRUN="$HOUSEKEEPINGSCRIPTSLOGDIR""$SCRIPTBASEFILENAME".md
+source ./foldervars.var
 
 SCRIPTTEXT="Finding The most recently modified Parsing Script File."
 printf "$cyan"    "$SCRIPTTEXT"
