693c3280ec6595f60131463ae16d5532766fd3fb
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 21:08:39 -0400

    Update 30-Removing-Content-Filtering-Lines.sh

--- scripts/TopLevelScripts/BlacklistScripts/actualparsing/30-Removing-Content-Filtering-Lines.sh
+++ scripts/TopLevelScripts/BlacklistScripts/actualparsing/30-Removing-Content-Filtering-Lines.sh
@@ -26,7 +26,7 @@ fi
 if
 [[ -f $BFILETEMP ]]
 then
-cat $BFILETEMP | sed '/^https\?:\/\//d; /^http\?:\/\//d; s/third-party$//g; s/popup$//g; s/important$//g; s/subdocument$//g; s/websocket$//g; s/^||//; s/[\^]$//g' > $BTEMPFILE
+cat $BFILETEMP | sed '/^https\:\/\//d; /^http\:\/\//d; s/[\^]third-party$//g; s/[\^]popup$//g; s/[\^]important$//g; s/[\^]subdocument$//g; s/[\^]websocket$//g; s/^||//; s/[\^]$//g' > $BTEMPFILE
 fi
 
 if
