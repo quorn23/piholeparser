a5354adfc3e4bd4e6280a39ee17b011d244483bc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-02 21:08:34 -0400

    Update 35-Trimming-HOSTS-Format-IP-Addresses.sh

--- scripts/TopLevelScripts/BlacklistScripts/actualparsing/35-Trimming-HOSTS-Format-IP-Addresses.sh
+++ scripts/TopLevelScripts/BlacklistScripts/actualparsing/35-Trimming-HOSTS-Format-IP-Addresses.sh
@@ -26,7 +26,7 @@ fi
 if
 [[ -f $BFILETEMP ]]
 then
-cat $BFILETEMP | sed 's/^PRIMARY\s\+[ \t]*//; s/^localhost\s\+[ \t]*//; s/blockeddomain.hosts\s\+[ \t]*//; s/^0.0.0.0\s\+[ \t]*//; s/^127.0.0.1\s\+[ \t]*//; s/^::1\s\+[ \t]*//' > $BTEMPFILE
+cat $BFILETEMP | sed 's/^PRIMARY\s\+[ \t]*//g; s/^localhost\s\+[ \t]*//g; s/blockeddomain.hosts\s\+[ \t]*//g; s/^0.0.0.0\s\+[ \t]*//g; s/^127.0.0.1\s\+[ \t]*//g; s/^::1\s\+[ \t]*//g' > $BTEMPFILE
 fi
 
 if
