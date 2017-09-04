c7a3e763cb0c655c7bc93e509e45a759232e9dcf
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-04 17:19:26 -0400

    Update 55-Processing-Whitelists.sh

--- scripts/TopLevelScripts/10-InitialTasksScripts/55-Processing-Whitelists.sh
+++ scripts/TopLevelScripts/10-InitialTasksScripts/55-Processing-Whitelists.sh
@@ -11,34 +11,30 @@ echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
 echo ""
 for f in $WHITEDOMAINSALL
 do
+BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
+echo "#### $BASEFILENAME" | sudo tee --append $RECENTRUN &>/dev/null
+printf "$cyan"  "Processing $BASEFILENAME"
 
 timestamp=$(echo `date`)
-WBASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
-echo "#### $WBASEFILENAME" | sudo tee --append $RECENTRUN &>/dev/null
-printf "$cyan"  "Processing $WBASEFILENAME"
-
 cat -s $f | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILEA
 cat $TEMPFILEA >> $WHITELISTTEMP
-
 HOWMANYLINES=$(echo -e "`wc -l $TEMPFILEA | cut -d " " -f 1`")
 echo "$HOWMANYLINES In $BASEFILENAME" | sudo tee --append $RECENTRUN &>/dev/null
 printf "$yellow"  "$HOWMANYLINES In $BASEFILENAME"
-
 rm $f
 mv $TEMPFILEA $f
-
 echo ""
 done
 
-SCRIPTTEXT="Deduplicating Merged  List."
+SCRIPTTEXT="Deduplicating Merged List."
 printf "$cyan"    "$SCRIPTTEXT"
 echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
 if
 [[ -f $WHITELISTTEMP ]]
 then
-cat -s $WHITELISTTEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILET
+cat -s $WHITELISTTEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILER
 rm $WHITELISTTEMP
-mv $TEMPFILET $WHITELISTTEMP
+mv $TEMPFILER $WHITELISTTEMP
 else
 touch $WHITELISTTEMP
 fi
