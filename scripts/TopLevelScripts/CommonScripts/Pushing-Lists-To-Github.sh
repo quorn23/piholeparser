fccb0d18f0253b8537fb9920a617069e2afd691c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-03 21:58:47 -0400

    Update Pushing-Lists-To-Github.sh

--- scripts/TopLevelScripts/CommonScripts/Pushing-Lists-To-Github.sh
+++ scripts/TopLevelScripts/CommonScripts/Pushing-Lists-To-Github.sh
@@ -41,7 +41,7 @@ if
 then
 printf "$green"   "Pushing Lists to Github"
 timestamp=$(echo `date`)
-printf "$green"    "$(git -C $REPODIR remote.origin.url $GITWHERETOPUSH)"
+printf "$green"    "$(git -C $REPODIR remote set-url origin $GITWHERETOPUSH)"
 printf "$green"    "$(git -C $REPODIR add .)"
 printf "$green"    "$(git -C $REPODIR commit -m "Update lists $timestamp")"
 printf "$green"    "$(git -C $REPODIR push -u origin master)"
