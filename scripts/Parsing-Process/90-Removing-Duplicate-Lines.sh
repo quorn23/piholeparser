3a8cdf602f64f79db36403de2875aa0a89899dde
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-04 16:57:30 -0400

    Create 90-Removing-Duplicate-Lines.sh

---
+++ scripts/Parsing-Process/90-Removing-Duplicate-Lines.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## Duplicate Removal
+
+## Variables
+SCRIPTDIRA=$(dirname $0)
+source "$SCRIPTDIRA"/foldervars.var
+
+if
+[[ -f $BFILETEMP ]]
+then
+cat -s $BFILETEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $BTEMPFILE
+fi
+
+if
+[[ -f $BFILETEMP ]]
+then
+rm $BFILETEMP
+fi
