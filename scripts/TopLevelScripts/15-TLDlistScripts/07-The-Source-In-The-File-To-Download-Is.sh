1991701cabd23b4d6578741bc9381c8edf31a563
deathbybandaid <sam@deathbybandaid.net>
2017-09-04 20:08:42 -0400

    Change Made Offline Mon 4 Sep 20:08:40 EDT 2017

--- scripts/TopLevelScripts/15-TLDlistScripts/07-The-Source-In-The-File-To-Download-Is.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-##
-
-## Variables
-SCRIPTDIRA=$(dirname $0)
-source "$SCRIPTDIRA"/foldervars.var
-
-printf "$yellow"    "$source"
--- scripts/TopLevelScripts/30-BlacklistScripts/13-The-Source-In-The-File-To-Download-Is.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## This  just displays the source on screen
-
-## Variables
-SCRIPTDIRA=$(dirname $0)
-source "$SCRIPTDIRA"/foldervars.var
-
-printf "$yellow"    "$source"
