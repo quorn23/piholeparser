3df4432514729e40fdf2a12afcb25a9ee0e50453
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-04-28 17:30:14 -0400

    Create 09-Checking-For-Existing-Mirror-File.sh

---
+++ scripts/TopLevelScripts/26-Processing-External-Whitelists/09-Checking-For-Existing-Mirror-File.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## Checks If Mirror File Is There
+
+## Variables
+SCRIPTDIRA=$(dirname $0)
+source "$SCRIPTDIRA"/foldervars.var
+
+if
+[[ -f $MIRROREDFILE ]]
+then
+printf "$green"  "Mirror File Currently Available."
+else
+printf "$red"  "Mirror File Currently Unavailable."
+fi
