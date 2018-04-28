be2c5ce6123bb7dc0ad538ffa0aa127d9402d699
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-04-28 19:29:50 -0400

    Update Processing-Internal-Whitelists.sh

--- scripts/TopLevelScripts/25-Processing-Internal-Whitelists/Processing-Internal-Whitelists.sh
+++ scripts/TopLevelScripts/25-Processing-Internal-Whitelists/Processing-Internal-Whitelists.sh
@@ -14,17 +14,17 @@ SCRIPTTEXT="Checking For Script Whitelist File."
 printf "$cyan"    "$SCRIPTTEXT"
 echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
 if
-[[ -f $WHITESCRIPTDOMAINS ]]
+[[ -f $WHITELISTTEMP ]]
 then
 printf "$red"  "Removing Script Whitelist File."
 echo ""
-rm $WHITESCRIPTDOMAINS
-touch $WHITESCRIPTDOMAINS
+rm $WHITELISTTEMP
+touch $WHITELISTTEMP
 echo "* Whitelist File removed $timestamp" | tee --append $RECENTRUN &>/dev/null
 else
 printf "$cyan"  "Script Whitelist File not there. Not Removing."
 echo ""
-touch $WHITESCRIPTDOMAINS
+touch $WHITELISTTEMP
 echo "* Script Whitelist File not there, not removing. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
 echo ""
@@ -33,52 +33,6 @@ SCRIPTTEXT="Pulling Domains From Individual Lists."
 printf "$cyan"    "$SCRIPTTEXT"
 echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
 
-if
-ls $WHITEDOMAINSALL &> /dev/null;
-then
-
-for f in $WHITEDOMAINSALL
-do
-
-source=`cat $f`
-
-SOURCEDOMAIN=`echo $source | awk -F/ '{print $3}'`
-if
-[[ -n $SOURCEDOMAIN ]]
-then
-echo "$SOURCEDOMAIN" | tee --append $WHITESCRIPTDOMAINS &>/dev/null
-fi
-
-done
-HOWMANYLINES=$(echo -e "`wc -l $WHITESCRIPTDOMAINS | cut -d " " -f 1`")
-echo "$HOWMANYLINES After $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
-printf "$yellow"  "$HOWMANYLINES After $SCRIPTTEXT"
-echo ""
-else
-echo "No Whitelists Present."
-fi
-
-SCRIPTTEXT="Deduping Merge of Individual Whitelists."
-printf "$cyan"    "$SCRIPTTEXT"
-echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
-if
-[[ -f $WHITESCRIPTDOMAINS ]]
-then
-cat -s $WHITESCRIPTDOMAINS | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILEH
-rm $WHITESCRIPTDOMAINS
-mv $TEMPFILEH $WHITESCRIPTDOMAINS
-else
-touch $WHITESCRIPTDOMAINS
-fi
-HOWMANYLINES=$(echo -e "`wc -l $WHITESCRIPTDOMAINS | cut -d " " -f 1`")
-echo "$HOWMANYLINES After $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
-printf "$yellow"  "$HOWMANYLINES After $SCRIPTTEXT"
-
-
-SCRIPTTEXT="Sorting and Deduping Individual Whitelists."
-printf "$cyan"    "$SCRIPTTEXT"
-echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
-echo ""
 
 if
 ls $WHITEDOMAINSALL &> /dev/null;
@@ -86,25 +40,29 @@ then
 
 for f in $WHITEDOMAINSALL
 do
+
 BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
-echo "#### $BASEFILENAME" | sudo tee --append $RECENTRUN &>/dev/null
-printf "$cyan"  "Processing $BASEFILENAME"
 
-timestamp=$(echo `date`)
+## Dedupe and Sort
 cat -s $f | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILEA
-cat $TEMPFILEA >> $WHITELISTTEMP
+mv $TEMPFILEA $f
+
+## Add to Script.domains
+cat $f >> $WHITELISTTEMP
+
 HOWMANYLINES=$(echo -e "`wc -l $TEMPFILEA | cut -d " " -f 1`")
 echo "$HOWMANYLINES In $BASEFILENAME" | sudo tee --append $RECENTRUN &>/dev/null
 printf "$yellow"  "$HOWMANYLINES In $BASEFILENAME"
-rm $f
-mv $TEMPFILEA $f
-echo ""
+
 done
 
-else
-echo "No Individual Whitelists Present."
-fi
+## Total Whitelist
+echo ""
+HOWMANYLINES=$(echo -e "`wc -l $WHITELISTTEMP | cut -d " " -f 1`")
+echo "$HOWMANYLINES To Whitelist" | sudo tee --append $RECENTRUN &>/dev/null
+printf "$yellow"  "$HOWMANYLINES To Whitelist"
 
+## Dedupe merge
 SCRIPTTEXT="Deduplicating Merged List."
 printf "$cyan"    "$SCRIPTTEXT"
 echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
