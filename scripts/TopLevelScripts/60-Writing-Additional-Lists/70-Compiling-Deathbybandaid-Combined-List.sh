4bf5bdfbe7a781d93e1b89558c4f9598308fdd07
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-04-28 20:33:53 -0400

    Update 70-Compiling-Deathbybandaid-Combined-List.sh

--- scripts/TopLevelScripts/60-Writing-Additional-Lists/70-Compiling-Deathbybandaid-Combined-List.sh
+++ scripts/TopLevelScripts/60-Writing-Additional-Lists/70-Compiling-Deathbybandaid-Combined-List.sh
@@ -71,10 +71,7 @@ if
 then
 printf "$yellow"  "Removing whitelist Domains."
 gawk 'NR==FNR{a[$0];next} !($0 in a)' $WHITELISTTEMP $TEMPFILE >> $FILETEMP
-rm $TEMPFILE
-comm -23 $FILETEMP <(sort $WHITELISTTEMP) > $TEMPFILE
-rm $FILETEMP
-grep -Fvxf $WHITELISTTEMP $TEMPFILE >> $FILETEMP
+#grep -Fvxf $WHITELISTTEMP $TEMPFILE >> $FILETEMP
 rm $TEMPFILE
 echo -e "`wc -l $FILETEMP | cut -d " " -f 1` lines after whitelist"
 echo ""
