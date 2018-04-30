0bfd84486425d3555c7879299a9c56e6092ef5ee
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-04-30 07:55:05 -0400

    Update 70-Compiling-Deathbybandaid-Combined-List.sh

--- scripts/TopLevelScripts/60-Writing-Additional-Lists/70-Compiling-Deathbybandaid-Combined-List.sh
+++ scripts/TopLevelScripts/60-Writing-Additional-Lists/70-Compiling-Deathbybandaid-Combined-List.sh
@@ -39,46 +39,49 @@ if
 [[ -f $COMBINEDWHITELISTS  ]]
 then
 printf "$yellow"  "Removing whitelist Domains."
-gawk 'NR==FNR{a[$0];next} !($0 in a)' $COMBINEDWHITELISTS $COMBINEDBLACKLISTS >> $COMBINEDBLACKLISTSDBB
-#grep -Fvxf $COMBINEDWHITELISTS $COMBINEDBLACKLISTS >> $COMBINEDBLACKLISTSDBB
-#comm -23 $COMBINEDBLACKLISTS $COMBINEDWHITELISTS > $COMBINEDBLACKLISTSDBB
-echo -e "`wc -l $COMBINEDBLACKLISTSDBB | cut -d " " -f 1` lines after whitelist"
+#gawk 'NR==FNR{a[$0];next} !($0 in a)' $COMBINEDWHITELISTS $COMBINEDBLACKLISTS >> $FILETEMP
+#grep -Fvxf $COMBINEDWHITELISTS $COMBINEDBLACKLISTS >> $FILETEMP
+comm -23 $COMBINEDBLACKLISTS $COMBINEDWHITELISTS > $FILETEMP
+echo -e "`wc -l $FILETEMP | cut -d " " -f 1` lines after whitelist"
 echo ""
 fi
 
 if
-[[ -f $COMBINEDBLACKLISTSDBB ]]
+[[ -f $FILETEMP ]]
 then
-echo ""
-else
-touch $COMBINEDBLACKLISTSDBB
-fi
-
-EDITEDALLPARSEDSIZEBYTES=$(stat -c%s "$COMBINEDBLACKLISTSDBB")
+EDITEDALLPARSEDSIZEBYTES=$(stat -c%s "$FILETEMP")
 EDITEDALLPARSEDSIZEKB=`expr $EDITEDALLPARSEDSIZEBYTES / 1024`
 EDITEDALLPARSEDSIZEMB=`expr $EDITEDALLPARSEDSIZEBYTES / 1024 / 1024`
 echo "EDITEDALLPARSEDSIZEMB="$EDITEDALLPARSEDSIZEMB"" | tee --append $TEMPVARS &>/dev/null
+fi
 
 if
 [[ "$EDITEDALLPARSEDSIZEMB" -gt 0 && "$EDITEDALLPARSEDSIZEKB" -gt 0 && "$EDITEDALLPARSEDSIZEBYTES" -gt 0 ]]
 then
-printf "$yellow"  "Size of DBB List = $EDITEDALLPARSEDSIZEMB MB."
+printf "$yellow"  "Size of $BASEFILENAME = $EDITEDALLPARSEDSIZEMB MB."
 elif
 [[ "$EDITEDALLPARSEDSIZEMB" -eq 0 && "$EDITEDALLPARSEDSIZEKB" -gt 0 && "$EDITEDALLPARSEDSIZEBYTES" -gt 0 ]]
 then
-printf "$yellow"  "Size of DBB ListE = $EDITEDALLPARSEDSIZEKB KB."
+printf "$yellow"  "Size of $BASEFILENAME = $EDITEDALLPARSEDSIZEKB KB."
 elif
 [[ "$EDITEDALLPARSEDSIZEMB" -eq 0 && "$EDITEDALLPARSEDSIZEKB" -eq 0 && "$EDITEDALLPARSEDSIZEBYTES" -gt 0 ]]
 then
-printf "$yellow"  "Size of DBB List = $EDITEDALLPARSEDSIZEBYTES Bytes."
+printf "$yellow"  "Size of List = $EDITEDALLPARSEDSIZEBYTES Bytes."
 fi
 
 if
 [[ "$EDITEDALLPARSEDSIZEBYTES" -gt 0 ]]
 then
-EDITEDALLPARSEDHOWMANYLINES=$(echo -e "`wc -l $COMBINEDBLACKLISTSDBB | cut -d " " -f 1`")
+EDITEDALLPARSEDHOWMANYLINES=$(echo -e "`wc -l $FILETEMP | cut -d " " -f 1`")
 printf "$yellow"  "$EDITEDALLPARSEDHOWMANYLINES Lines After Compiling."
 echo "EDITEDALLPARSEDHOWMANYLINES="$EDITEDALLPARSEDHOWMANYLINES"" | tee --append $TEMPVARS &>/dev/null
+fi
+
+if
+[[ -f $COMBINEDBLACKLISTSDBB && "$EDITEDALLPARSEDSIZEBYTES" -gt 0 ]]
+then
+printf "$green"  "Old COMBINEDBLACKLISTSDBB File Removed."
+rm $COMBINEDBLACKLISTSDBB
 fi
 
 ## Github has a 100mb limit, and empty files are useless
@@ -87,15 +90,17 @@ if
 then
 printf "$red"     "File Empty"
 echo "* Allparsedlist list was an empty file $timestamp" | tee --append $RECENTRUN &>/dev/null
+mv $FILETEMP $COMBINEDBLACKLISTSDBB
 elif
 [[ "$EDITEDALLPARSEDSIZEMB" -ge "$GITHUBLIMITMB" ]]
 then
 printf "$red"     "Parsed File Too Large For Github. Deleting."
-rm $COMBINEDBLACKLISTSDBB
+rm $FILETEMP
 touch $COMBINEDBLACKLISTSDBB
 echo "* Allparsedlist list was too large to host on github. $EDITEDALLPARSEDSIZEMB bytes $timestamp" | tee --append $RECENTRUN &>/dev/null
 elif
-[[ "$EDITEDALLPARSEDSIZEMB" -lt "$GITHUBLIMITMB" && -f $COMBINEDBLACKLISTSDBB ]]
+[[ "$EDITEDALLPARSEDSIZEMB" -lt "$GITHUBLIMITMB" && -f $FILETEMP ]]
 then
+mv $FILETEMP $COMBINEDBLACKLISTSDBB
 printf "$yellow"  "Big List Edited Created Successfully."
 fi
