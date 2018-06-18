d62d6d70b152801cf84761e21f40914268b3461d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:52:47 -0400

    Update 68-Looking-At-What-Did-Not-Pass-TLD-Test.sh

--- scripts/TopLevelScripts/90-Completing-End-Tasks/68-Looking-At-What-Did-Not-Pass-TLD-Test.sh
+++ scripts/TopLevelScripts/90-Completing-End-Tasks/68-Looking-At-What-Did-Not-Pass-TLD-Test.sh
@@ -5,20 +5,19 @@
 SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
-if
-[[ -f $TRYNACATCHFIlES ]]
+if [[ -f $TRYNACATCHFIlES ]]
 then
-printf "$yellow"  "Removing duplicates."
-cat -s $TRYNACATCHFIlES | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' >> $FILETEMP
-HOWMANYLINES=$(echo -e "`wc -l $FILETEMP | cut -d " " -f 1`")
-printf "$yellow"    "$HOWMANYLINES Lines After Deduping."
-echo "* $HOWMANYLINES Lines After Deduping. $timestamp" | tee --append $RECENTRUN &>/dev/null
-echo "____________________________________________________" | tee --append $RECENTRUN &>/dev/null
-for source in `cat $FILETEMP`;
-do
-echo "* $source" | tee --append $RECENTRUN &>/dev/null
-done
-rm $FILETEMP
+  printf "$yellow"  "Removing duplicates."
+  cat -s $TRYNACATCHFIlES | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' >> $FILETEMP
+  HOWMANYLINES=$(echo -e "`wc -l $FILETEMP | cut -d " " -f 1`")
+  printf "$yellow"    "$HOWMANYLINES Lines After Deduping."
+  echo "* $HOWMANYLINES Lines After Deduping. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  echo "____________________________________________________" | tee --append $RECENTRUN &>/dev/null
+  for source in `cat $FILETEMP`;
+  do
+    echo "* $source" | tee --append $RECENTRUN &>/dev/null
+  done
+  rm $FILETEMP
 else
-printf "$yellow"  "Either all lines past TLD test, or the file is missing."
+  printf "$yellow"  "Either all lines past TLD test, or the file is missing."
 fi
