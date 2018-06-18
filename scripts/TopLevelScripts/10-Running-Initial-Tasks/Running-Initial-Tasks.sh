d9016b2302e28e0c7e731dff6627548cc5c0f7c3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 08:43:27 -0400

    Update Running-Initial-Tasks.sh

--- scripts/TopLevelScripts/10-Running-Initial-Tasks/Running-Initial-Tasks.sh
+++ scripts/TopLevelScripts/10-Running-Initial-Tasks/Running-Initial-Tasks.sh
@@ -11,66 +11,62 @@ INITIALTASKSSCRIPTSALL="$COMPLETEFOLDERPATH"/[0-9]*.sh
 for f in $INITIALTASKSSCRIPTSALL
 do
 
-timestamp=$(echo `date`)
-LOOPSTART=$(date +"%s")
+  timestamp=$(echo `date`)
+  LOOPSTART=$(date +"%s")
 
-INITIALTASK="$f"
-INITIALTASKBASENAME=$(echo `basename $INITIALTASK | cut -f 1 -d '.'`)
-INITIALTASKNAME=$(echo `basename $INITIALTASK | cut -f 1 -d '.' | sed 's/[0-9]/ /g' | sed 's/[\-]/ /'`)
-INITIALTASKSCRIPTTEXT=$(echo $INITIALTASKNAME | sed 's/[0-9\-]/ /g')
-INITIALTASKSREPOLOG="[Details If Any]("$TOPLEVELSCRIPTSLOGSDIRGIT""$SCRIPTBASEFOLDERNAME"/"$INITIALTASKBASENAME".md)"
+  INITIALTASK="$f"
+  INITIALTASKBASENAME=$(echo `basename $INITIALTASK | cut -f 1 -d '.'`)
+  INITIALTASKNAME=$(echo `basename $INITIALTASK | cut -f 1 -d '.' | sed 's/[0-9]/ /g' | sed 's/[\-]/ /'`)
+  INITIALTASKSCRIPTTEXT=$(echo $INITIALTASKNAME | sed 's/[0-9\-]/ /g')
+  INITIALTASKSREPOLOG="[Details If Any]("$TOPLEVELSCRIPTSLOGSDIRGIT""$SCRIPTBASEFOLDERNAME"/"$INITIALTASKBASENAME".md)"
 
-printf "$lightblue"    "$DIVIDERBARB"
-echo ""
+  printf "$lightblue"    "$DIVIDERBARB"
+  echo ""
 
-printf "$cyan"   "$INITIALTASKSCRIPTTEXT $timestamp"
-echo ""
+  printf "$cyan"   "$INITIALTASKSCRIPTTEXT $timestamp"
+  echo ""
 
-## Log Subsection
-echo "## $INITIALTASKSCRIPTTEXT $timestamp" | tee --append $RECENTRUN &>/dev/null
+  ## Log Subsection
+  echo "## $INITIALTASKSCRIPTTEXT $timestamp" | tee --append $RECENTRUN &>/dev/null
 
-## Clear Temp Before
-if
-[[ -f $DELETETEMPFILE ]]
-then
-bash $DELETETEMPFILE
-else
-echo "Error Deleting Temp Files."
-exit
-fi
+  ## Clear Temp Before
+  if [[ -f $DELETETEMPFILE ]]
+  then
+    bash $DELETETEMPFILE
+  else
+    echo "Error Deleting Temp Files."
+    exit
+  fi
 
-## Run Script
-bash $f
+  ## Run Script
+  bash $f
 
-## Clear Temp After
-if
-[[ -f $DELETETEMPFILE ]]
-then
-bash $DELETETEMPFILE
-else
-echo "Error Deleting Temp Files."
-exit
-fi
+  ## Clear Temp After
+  if [[ -f $DELETETEMPFILE ]]
+  then
+    bash $DELETETEMPFILE
+  else
+    echo "Error Deleting Temp Files."
+    exit
+  fi
 
-LOOPEND=$(date +"%s")
-DIFFTIMELOOPSEC=`expr $LOOPEND - $LOOPSTART`
-if
-[[ $DIFFTIMELOOPSEC -ge 60 ]]
-then
-DIFFTIMELOOPMIN=`expr $DIFFTIMELOOPSEC / 60`
-LOOPTIMEDIFF="$DIFFTIMELOOPMIN Minutes."
-elif
-[[ $DIFFTIMELOOPSEC -lt 60 ]]
-then
-LOOPTIMEDIFF="$DIFFTIMELOOPSEC Seconds."
-fi
+  LOOPEND=$(date +"%s")
+  DIFFTIMELOOPSEC=`expr $LOOPEND - $LOOPSTART`
+  if [[ $DIFFTIMELOOPSEC -ge 60 ]]
+  then
+    DIFFTIMELOOPMIN=`expr $DIFFTIMELOOPSEC / 60`
+    LOOPTIMEDIFF="$DIFFTIMELOOPMIN Minutes."
+  elif [[ $DIFFTIMELOOPSEC -lt 60 ]]
+  then
+    LOOPTIMEDIFF="$DIFFTIMELOOPSEC Seconds."
+  fi
 
-echo "Process Took $LOOPTIMEDIFF" | sudo tee --append $RECENTRUN &>/dev/null
-echo "$INITIALTASKSREPOLOG" | sudo tee --append $RECENTRUN &>/dev/null
-echo "" | tee --append $RECENTRUN
+  echo "Process Took $LOOPTIMEDIFF" | sudo tee --append $RECENTRUN &>/dev/null
+  echo "$INITIALTASKSREPOLOG" | sudo tee --append $RECENTRUN &>/dev/null
+  echo "" | tee --append $RECENTRUN
 
-printf "$orange" "$DIVIDERBARB"
-echo ""
+  printf "$orange" "$DIVIDERBARB"
+  echo ""
 
 ## End Of Loop
 done
