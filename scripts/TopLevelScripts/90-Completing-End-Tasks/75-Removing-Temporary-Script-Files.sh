99ca642bf8f10d4fde7d7b01964bc394f440dc7e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:55:01 -0400

    Update 75-Removing-Temporary-Script-Files.sh

--- scripts/TopLevelScripts/90-Completing-End-Tasks/75-Removing-Temporary-Script-Files.sh
+++ scripts/TopLevelScripts/90-Completing-End-Tasks/75-Removing-Temporary-Script-Files.sh
@@ -6,30 +6,26 @@ SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
 CHECKME=$TEMPVARS
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-rm $CHECKME
+  rm $CHECKME
 fi
 
 
 CHECKME=$TEMPCLEANUPONE
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-rm $CHECKME
+  rm $CHECKME
 fi
 
 CHECKME=$TEMPCLEANUPTWO
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-rm $CHECKME
+  rm $CHECKME
 fi
 
 CHECKME=$TEMPCLEANUPTHREE
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-rm $CHECKME
+  rm $CHECKME
 fi
