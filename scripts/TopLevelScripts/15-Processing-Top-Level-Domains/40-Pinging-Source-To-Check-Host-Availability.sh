5c70ff6674d597bb45005b9c4a103d04c5316975
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 09:45:20 -0400

    Update 40-Pinging-Source-To-Check-Host-Availability.sh

--- scripts/TopLevelScripts/15-Processing-Top-Level-Domains/40-Pinging-Source-To-Check-Host-Availability.sh
+++ scripts/TopLevelScripts/15-Processing-Top-Level-Domains/40-Pinging-Source-To-Check-Host-Availability.sh
@@ -6,36 +6,31 @@ SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
 ## If $SOURCEDOMAIN is set, Ping it
-if
-[[ -n $SOURCEDOMAIN ]]
+if [[ -n $SOURCEDOMAIN ]]
 then
-SOURCEIPFETCH=`ping -c 1 $SOURCEDOMAIN | gawk -F'[()]' '/PING/{print $2}'`
-SOURCEIP=`echo $SOURCEIPFETCH`
-echo "SOURCEIP="$SOURCEIP"" | tee --append $TEMPPARSEVARS &>/dev/null
+  SOURCEIPFETCH=`ping -c 1 $SOURCEDOMAIN | gawk -F'[()]' '/PING/{print $2}'`
+  SOURCEIP=`echo $SOURCEIPFETCH`
+  echo "SOURCEIP="$SOURCEIP"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
 
-if
-[[ -n $SOURCEIP ]]
+if [[ -n $SOURCEIP ]]
 then
-printf "$green"    "Ping Test Was A Success!"
-elif
-[[ -z $SOURCEIP ]]
+  printf "$green"    "Ping Test Was A Success!"
+elif [[ -z $SOURCEIP ]]
 then
-printf "$red"    "Ping Test Failed."
-PINGTESTFAILEDA=true
-echo "PINGTESTFAILEDA="$PINGTESTFAILEDA"" | tee --append $TEMPPARSEVARS &>/dev/null
+  printf "$red"    "Ping Test Failed."
+  PINGTESTFAILEDA=true
+  echo "PINGTESTFAILEDA="$PINGTESTFAILEDA"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
 
-if
-[[ -n $PINGTESTFAILEDA  ]]
+if [[ -n $PINGTESTFAILEDA  ]]
 then
-{ if
-[[ `wget -S --spider $source  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
-then
-printf "$green"  "Header Check Successful."
-else
-printf "$red"  "Header Check Unsuccessful."
-PINGTESTFAILED=true
-echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
-fi }
+  if [[ `wget -S --spider $source  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
+  then
+    printf "$green"  "Header Check Successful."
+  else
+    printf "$red"  "Header Check Unsuccessful."
+    PINGTESTFAILED=true
+    echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
+  fi
 fi
