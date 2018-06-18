bac16bf54f39cb3fdc7d7798ee243190215f8661
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 10:08:20 -0400

    Update 42-Checking-File-Header.sh

--- scripts/TopLevelScripts/26-Processing-External-Whitelists/42-Checking-File-Header.sh
+++ scripts/TopLevelScripts/26-Processing-External-Whitelists/42-Checking-File-Header.sh
@@ -8,32 +8,29 @@ source "$SCRIPTDIRA"/foldervars.var
 #HTTPCODECURL=`curl -s -o /dev/null -I -w "%{http_code}" $source`
 #HTTPCODEWGET=`wget --server-response $source 2>&1 | awk '/^  HTTP/{print $2}'`
 
-#if
-#[[ $HTTPCODECURL == 200 || $HTTPCODEWGET == 200 ]]
+#if [[ $HTTPCODECURL == 200 || $HTTPCODEWGET == 200 ]]
 #then
-#echo "yes"
+  #echo "yes"
 #else
-#echo "no"
+  #echo "no"
 #fi
 
 ## curl Header
-if
-[[ `curl -s -H "$AGENTDOWNLOAD" -o /dev/null -I -w "%{http_code}" $source | grep '200'` ]]
+if [[ `curl -s -H "$AGENTDOWNLOAD" -o /dev/null -I -w "%{http_code}" $source | grep '200'` ]]
 then
-printf "$green"  "Curl Header Check Successful."
+  printf "$green"  "Curl Header Check Successful."
 else
-printf "$red"  "Curl Header Check Unsuccessful."
-TESTCURLHEADERFAILED=true
-echo "TESTCURLHEADERFAILED="$TESTCURLHEADERFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
+  printf "$red"  "Curl Header Check Unsuccessful."
+  TESTCURLHEADERFAILED=true
+  echo "TESTCURLHEADERFAILED="$TESTCURLHEADERFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
 
 ## wget header
-if
-[[ `wget -S --header="$AGENTDOWNLOAD" --spider $source  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
+if [[ `wget -S --header="$AGENTDOWNLOAD" --spider $source  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
 then
-printf "$green"  "Wget Header Check Successful."
+  printf "$green"  "Wget Header Check Successful."
 else
-printf "$red"  "Wget Header Check Unsuccessful."
-TESTWGETHEADERFAILED=true
-echo "TESTWGETHEADERFAILED="$TESTWGETHEADERFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
+  printf "$red"  "Wget Header Check Unsuccessful."
+  TESTWGETHEADERFAILED=true
+  echo "TESTWGETHEADERFAILED="$TESTWGETHEADERFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
