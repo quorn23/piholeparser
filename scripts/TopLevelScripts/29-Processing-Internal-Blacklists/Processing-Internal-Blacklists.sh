b4d52e6ce77aa5ace154df30bf5a0cb959177326
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 10:52:03 -0400

    Update Processing-Internal-Blacklists.sh

--- scripts/TopLevelScripts/29-Processing-Internal-Blacklists/Processing-Internal-Blacklists.sh
+++ scripts/TopLevelScripts/29-Processing-Internal-Blacklists/Processing-Internal-Blacklists.sh
@@ -12,80 +12,76 @@ timestamp=$(echo `date`)
 
 SCRIPTTEXT="Checking For Script Blacklist File."
 printf "$cyan"    "$SCRIPTTEXT"
-echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
-if
-[[ -f $BLACKLISTTEMP ]]
+echo "### $SCRIPTTEXT" | tee --append $RECENTRUN &>/dev/null
+if [[ -f $BLACKLISTTEMP ]]
 then
-printf "$red"  "Removing Script Blacklist File."
-echo ""
-rm $BLACKLISTTEMP
-touch $BLACKLISTTEMP
-echo "* Blacklist File removed $timestamp" | tee --append $RECENTRUN &>/dev/null
+  printf "$red"  "Removing Script Blacklist File."
+  echo ""
+  rm $BLACKLISTTEMP
+  touch $BLACKLISTTEMP
+  echo "* Blacklist File removed $timestamp" | tee --append $RECENTRUN &>/dev/null
 else
-printf "$cyan"  "Script Blacklist File not there. Not Removing."
-echo ""
-touch $BLACKLISTTEMP
-echo "* Script Blacklist File not there, not removing. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  printf "$cyan"  "Script Blacklist File not there. Not Removing."
+  echo ""
+  touch $BLACKLISTTEMP
+  echo "* Script Blacklist File not there, not removing. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
 echo ""
 
 SCRIPTTEXT="Pulling Domains From Individual Lists."
 printf "$cyan"    "$SCRIPTTEXT"
-echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
+echo "### $SCRIPTTEXT" | tee --append $RECENTRUN &>/dev/null
 
 
-if
-ls $BLACKDOMAINSALL &> /dev/null;
+if ls $BLACKDOMAINSALL &> /dev/null;
 then
 
-for f in $BLACKDOMAINSALL
-do
+  for f in $BLACKDOMAINSALL
+  do
 
-BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
+    BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
 
-## Dedupe and Sort
-cat -s $f | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILEA
-mv $TEMPFILEA $f
+    ## Dedupe and Sort
+    cat -s $f | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILEA
+    mv $TEMPFILEA $f
 
-## Add to Script.domains
-cat $f >> $BLACKLISTTEMP
+    ## Add to Script.domains
+    cat $f >> $BLACKLISTTEMP
 
-HOWMANYLINES=$(echo -e "`wc -l $f | cut -d " " -f 1`")
-echo "$HOWMANYLINES In $BASEFILENAME" | sudo tee --append $RECENTRUN &>/dev/null
-printf "$yellow"  "$HOWMANYLINES In $BASEFILENAME"
+    HOWMANYLINES=$(echo -e "`wc -l $f | cut -d " " -f 1`")
+    echo "$HOWMANYLINES In $BASEFILENAME" | tee --append $RECENTRUN &>/dev/null
+    printf "$yellow"  "$HOWMANYLINES In $BASEFILENAME"
 
-done
+  done
 
-else
-echo "No Individual Blacklists Present."
-touch $BLACKLISTTEMP
+  else
+  echo "No Individual Blacklists Present."
+  touch $BLACKLISTTEMP
 fi
 
 ## Total Blacklist
-if
-[[ -f $BLACKLISTTEMP ]]
+if [[ -f $BLACKLISTTEMP ]]
 then
-echo ""
-HOWMANYLINES=$(echo -e "`wc -l $BLACKLISTTEMP | cut -d " " -f 1`")
-echo "$HOWMANYLINES To Blacklist" | sudo tee --append $RECENTRUN &>/dev/null
-printf "$yellow"  "$HOWMANYLINES To Blacklist"
+  echo ""
+  HOWMANYLINES=$(echo -e "`wc -l $BLACKLISTTEMP | cut -d " " -f 1`")
+  echo "$HOWMANYLINES To Blacklist" | tee --append $RECENTRUN &>/dev/null
+  printf "$yellow"  "$HOWMANYLINES To Blacklist"
 else
-touch $BLACKLISTTEMP
+  touch $BLACKLISTTEMP
 fi
 
 ## Dedupe merge
 SCRIPTTEXT="Deduplicating Merged List."
 printf "$cyan"    "$SCRIPTTEXT"
-echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
-if
-[[ -f $BLACKLISTTEMP ]]
+echo "### $SCRIPTTEXT" | tee --append $RECENTRUN &>/dev/null
+if [[ -f $BLACKLISTTEMP ]]
 then
-cat -s $BLACKLISTTEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILER
-rm $BLACKLISTTEMP
-mv $TEMPFILER $BLACKLISTTEMP
+  cat -s $BLACKLISTTEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $TEMPFILER
+  rm $BLACKLISTTEMP
+  mv $TEMPFILER $BLACKLISTTEMP
 else
-touch $BLACKLISTTEMP
+  touch $BLACKLISTTEMP
 fi
 HOWMANYLINES=$(echo -e "`wc -l $BLACKLISTTEMP | cut -d " " -f 1`")
-echo "$HOWMANYLINES After $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
+echo "$HOWMANYLINES After $SCRIPTTEXT" | tee --append $RECENTRUN &>/dev/null
 printf "$yellow"  "$HOWMANYLINES After $SCRIPTTEXT"
