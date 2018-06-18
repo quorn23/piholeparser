818889089332066ea6948a9d8faf44a4fc4a1efe
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:55:50 -0400

    Update 80-Pushing-Lists-To-Locally-Hosted-Location.sh

--- scripts/TopLevelScripts/90-Completing-End-Tasks/80-Pushing-Lists-To-Locally-Hosted-Location.sh
+++ scripts/TopLevelScripts/90-Completing-End-Tasks/80-Pushing-Lists-To-Locally-Hosted-Location.sh
@@ -8,60 +8,55 @@ source "$SCRIPTDIRA"/foldervars.var
 WHATITIS="Localhost Web Directory"
 CHECKME=$LOCALHOSTDIR
 timestamp=$(echo `date`)
-if
-[[ -z $CHECKME ]]
+if [[ -z $CHECKME ]]
 then
-echo "* $WHATITIS Not Set. Please Fix. $timestamp" | tee --append $RECENTRUN &>/dev/null
-echo "Local Host Web Directory Not Set."
-exit
+  echo "* $WHATITIS Not Set. Please Fix. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  echo "Local Host Web Directory Not Set."
+  exit
 fi
 if
-ls $CHECKME &> /dev/null;
+  ls $CHECKME &> /dev/null;
 then
-echo "* $WHATITIS Already there no need to mkdir. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  echo "* $WHATITIS Already there no need to mkdir. $timestamp" | tee --append $RECENTRUN &>/dev/null
 else
-echo "* $WHATITIS Not There. Please Fix. $timestamp" | tee --append $RECENTRUN &>/dev/null
-exit
+  echo "* $WHATITIS Not There. Please Fix. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  exit
 fi
 
 WHATITIS="Locally Hosted Biglist"
 CHECKME=$CLOCALHOST
 timestamp=$(echo `date`)
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-rm $CHECKME
-echo "* $WHATITIS Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  rm $CHECKME
+  echo "* $WHATITIS Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
 else
-echo "* $WHATITIS Not Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  echo "* $WHATITIS Not Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
 ## Copy it over
 CHECKME=$CLOCALHOST
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-cp -p $COMBINEDBLACKLISTS $CLOCALHOST
+  cp -p $COMBINEDBLACKLISTS $CLOCALHOST
 else
-echo "* $WHATITIS Not Created. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  echo "* $WHATITIS Not Created. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
 
 WHATITIS="Locally Hosted Biglist (Edited)"
 CHECKME=$DBBLOCALHOST
 timestamp=$(echo `date`)
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-rm $CHECKME
-echo "* $WHATITIS Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  rm $CHECKME
+  echo "* $WHATITIS Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
 else
-echo "* $WHATITIS Not Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  echo "* $WHATITIS Not Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
 ## Copy it over
 CHECKME=$DBBLOCALHOST
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-cp -p $COMBINEDBLACKLISTSDBB $DBBLOCALHOST
+  cp -p $COMBINEDBLACKLISTSDBB $DBBLOCALHOST
 else
-echo "* $WHATITIS Not Created. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  echo "* $WHATITIS Not Created. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
