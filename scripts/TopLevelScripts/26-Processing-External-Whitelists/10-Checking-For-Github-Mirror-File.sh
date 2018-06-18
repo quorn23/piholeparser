f22251b675f0bea723c1c26835d2413781b936d8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 10:05:30 -0400

    Update 10-Checking-For-Github-Mirror-File.sh

--- scripts/TopLevelScripts/26-Processing-External-Whitelists/10-Checking-For-Github-Mirror-File.sh
+++ scripts/TopLevelScripts/26-Processing-External-Whitelists/10-Checking-For-Github-Mirror-File.sh
@@ -5,12 +5,11 @@
 SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
-if
-[[ `wget -S --spider $MIRROREDFILEDL  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
+if [[ `wget -S --spider $MIRROREDFILEDL  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
 then
-printf "$green"  "Github Mirror File Currently Available."
-GITFILEONLINE=true
-echo "GITFILEONLINE="$GITFILEONLINE"" | tee --append $TEMPPARSEVARS &>/dev/null
+  printf "$green"  "Github Mirror File Currently Available."
+  GITFILEONLINE=true
+  echo "GITFILEONLINE="$GITFILEONLINE"" | tee --append $TEMPPARSEVARS &>/dev/null
 else
-printf "$red"  "Github Mirror File Currently Unavailable."
+  printf "$red"  "Github Mirror File Currently Unavailable."
 fi
