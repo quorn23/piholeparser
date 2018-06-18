41065e4dff32abf431cdcd94800c9ad0baaaf385
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 14:03:29 -0400

    Update 90-Removing-Duplicate-Lines.sh

--- scripts/Parsing-Process/90-Removing-Duplicate-Lines.sh
+++ scripts/Parsing-Process/90-Removing-Duplicate-Lines.sh
@@ -5,14 +5,12 @@
 SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
-if
-[[ -f $BFILETEMP ]]
+if [[ -f $BFILETEMP ]]
 then
-cat -s $BFILETEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $BTEMPFILE
+  cat -s $BFILETEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' > $BTEMPFILE
 fi
 
-if
-[[ -f $BFILETEMP ]]
+if [[ -f $BFILETEMP ]]
 then
-rm $BFILETEMP
+  rm $BFILETEMP
 fi
