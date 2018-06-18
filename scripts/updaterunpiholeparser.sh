dd7f549276ecf0f2377e3263b41082704612f8b9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 08:26:58 -0400

    Update updaterunpiholeparser.sh

--- scripts/updaterunpiholeparser.sh
+++ scripts/updaterunpiholeparser.sh
@@ -7,34 +7,31 @@
 ## Variables
 REPONAME=piholeparser
 MAINVAR=/etc/"$REPONAME".var
-if
-[[ -f "$MAINVAR" ]]
+if [[ -f "$MAINVAR" ]]
 then
-echo "Main Vars Check Successful"
-source $MAINVAR
+  echo "Main Vars Check Successful"
+  source $MAINVAR
 else
-echo "Main Vars File Missing, Exiting."
-exit
+  echo "Main Vars File Missing, Exiting."
+  exit
 fi
 
-if
-[[ ! -d "$REPODIR" ]]
+if [[ ! -d "$REPODIR" ]]
 then
-echo ""
-echo "$REPONAME Directory Missing. Cloning Now."
-echo ""
-git clone $GITREPOSITORYURLB $REPODIR
+  echo ""
+  echo "$REPONAME Directory Missing. Cloning Now."
+  echo ""
+  git clone $GITREPOSITORYURLB $REPODIR
 fi
 
 SCRIPTVARSDIR="$REPODIR"scripts/scriptvars/
 STATICVARS="$SCRIPTVARSDIR"staticvariables.var
-if
-[[ -f $STATICVARS ]]
+if [[ -f $STATICVARS ]]
 then
-source $STATICVARS
+  source $STATICVARS
 else
-echo "Static Vars File Missing, Exiting."
-exit
+  echo "Static Vars File Missing, Exiting."
+  exit
 fi
 
 printf "$blue"    "$DIVIDERBAR"
@@ -47,11 +44,10 @@ printf "$magenta" "$DIVIDERBAR"
 echo ""
 
 ## RunParser
-if
-[[ -f $TOPRUNSCRIPT ]]
+if [[ -f $TOPRUNSCRIPT ]]
 then
-bash $TOPRUNSCRIPT
+  bash $TOPRUNSCRIPT
 else
-echo "$TOPRUNSCRIPT Missing, Exiting."
-exit
+  echo "$TOPRUNSCRIPT Missing, Exiting."
+  exit
 fi
