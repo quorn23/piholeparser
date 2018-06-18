a3149c7c48fc3145693405c4473e553945f27c7c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:29:25 -0400

    Update 95-Calculating-Parse-Time.sh

--- scripts/TopLevelScripts/30-Processing-External-Blacklists/95-Calculating-Parse-Time.sh
+++ scripts/TopLevelScripts/30-Processing-External-Blacklists/95-Calculating-Parse-Time.sh
@@ -10,10 +10,9 @@ DIFFTIMEPARSESEC=`expr $ENDPARSESTAMP - $STARTPARSESTAMP`
 DIFFTIMEPARSE=`expr $DIFFTIMEPARSESEC / 60`
 echo "$DIFFTIMEPARSESEC" | tee --append $PARSEAVERAGEFILETIME &>/dev/null
 
-if
-[[ $DIFFTIMEPARSESEC -gt 60 ]]
+if [[ $DIFFTIMEPARSESEC -gt 60 ]]
 then
-printf "$yellow"   "List took $DIFFTIMEPARSE Minutes To Parse."
+  printf "$yellow"   "List took $DIFFTIMEPARSE Minutes To Parse."
 else
-printf "$yellow"   "List took $DIFFTIMEPARSESEC Seconds To Parse."
+  printf "$yellow"   "List took $DIFFTIMEPARSESEC Seconds To Parse."
 fi
