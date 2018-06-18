4ca11fde0d454ad50154dfb4798b73198c48774b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:59:57 -0400

    Update 35-Trimming-HOSTS-Format-IP-Addresses.sh

--- scripts/Parsing-Process/35-Trimming-HOSTS-Format-IP-Addresses.sh
+++ scripts/Parsing-Process/35-Trimming-HOSTS-Format-IP-Addresses.sh
@@ -5,14 +5,12 @@
 SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
-if
-[[ -f $BFILETEMP ]]
+if [[ -f $BFILETEMP ]]
 then
-cat $BFILETEMP | sed 's/^PRIMARY\s\+[ \t]*//g; s/^localhost\s\+[ \t]*//g; s/blockeddomain.hosts\s\+[ \t]*//g; s/^0.0.0.0\s\+[ \t]*//g; s/^127.0.0.1\s\+[ \t]*//g; s/^::1\s\+[ \t]*//g' > $BTEMPFILE
+  cat $BFILETEMP | sed 's/^PRIMARY\s\+[ \t]*//g; s/^localhost\s\+[ \t]*//g; s/blockeddomain.hosts\s\+[ \t]*//g; s/^0.0.0.0\s\+[ \t]*//g; s/^127.0.0.1\s\+[ \t]*//g; s/^::1\s\+[ \t]*//g' > $BTEMPFILE
 fi
 
-if
-[[ -f $BFILETEMP ]]
+if [[ -f $BFILETEMP ]]
 then
-rm $BFILETEMP
+  rm $BFILETEMP
 fi
