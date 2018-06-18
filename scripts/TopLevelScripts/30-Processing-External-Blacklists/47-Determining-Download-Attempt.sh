7718cd7a67183f08a46708c57c7bebc58839c3ca
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:20:54 -0400

    Update 47-Determining-Download-Attempt.sh

--- scripts/TopLevelScripts/30-Processing-External-Blacklists/47-Determining-Download-Attempt.sh
+++ scripts/TopLevelScripts/30-Processing-External-Blacklists/47-Determining-Download-Attempt.sh
@@ -6,73 +6,58 @@ SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
 ## What type of source?
-if
-[[ -z $SKIPDOWNLOAD && $source == *.7z ]]
+if [[ -z $SKIPDOWNLOAD && $source == *.7z ]]
 then
-SOURCETYPE=sevenzip
-elif
-[[ -z $SKIPDOWNLOAD && $source == *.tar.gz ]]
+  SOURCETYPE=sevenzip
+elif [[ -z $SKIPDOWNLOAD && $source == *.tar.gz ]]
 then
-SOURCETYPE=tar
-elif
-[[ -z $SKIPDOWNLOAD && $source == *.zip ]]
+  SOURCETYPE=tar
+elif [[ -z $SKIPDOWNLOAD && $source == *.zip ]]
 then
-SOURCETYPE=zip
-elif
-[[ -z $SKIPDOWNLOAD && $source == "file:///"* ]]
+  SOURCETYPE=zip
+elif [[ -z $SKIPDOWNLOAD && $source == "file:///"* ]]
 then
-SOURCETYPE=localfile
-elif
-[[ -z $SKIPDOWNLOAD && $source == *"?"* ]]
+  SOURCETYPE=localfile
+elif [[ -z $SKIPDOWNLOAD && $source == *"?"* ]]
 then
-SOURCETYPE=webpage
-elif
-[[ -z $SKIPDOWNLOAD && $source == *"="* ]]
+  SOURCETYPE=webpage
+elif [[ -z $SKIPDOWNLOAD && $source == *"="* ]]
 then
-SOURCETYPE=webpage
-elif
-[[ -z $SKIPDOWNLOAD && $source == *.php ]]
+  SOURCETYPE=webpage
+elif [[ -z $SKIPDOWNLOAD && $source == *.php ]]
 then
-SOURCETYPE=webpage
-elif
-[[ -z $SKIPDOWNLOAD && $source == *.htm ]]
+  SOURCETYPE=webpage
+elif [[ -z $SKIPDOWNLOAD && $source == *.htm ]]
 then
-SOURCETYPE=webpage
-elif
-[[ -z $SKIPDOWNLOAD && $source == *.html ]]
+  SOURCETYPE=webpage
+elif [[ -z $SKIPDOWNLOAD && $source == *.html ]]
 then
-SOURCETYPE=webpage
-elif
-[[ -z $SKIPDOWNLOAD && $source == *.txt ]]
+  SOURCETYPE=webpage
+elif [[ -z $SKIPDOWNLOAD && $source == *.txt ]]
 then
-SOURCETYPE=plaintext
-elif
-[[ -z $SKIPDOWNLOAD && $source == *.csv ]]
+  SOURCETYPE=plaintext
+elif [[ -z $SKIPDOWNLOAD && $source == *.csv ]]
 then
-SOURCETYPE=plaintext
-elif
-[[ -z $SKIPDOWNLOAD && $source == *hosts ]]
+  SOURCETYPE=plaintext
+elif [[ -z $SKIPDOWNLOAD && $source == *hosts ]]
 then
-SOURCETYPE=plaintext
+  SOURCETYPE=plaintext
 fi
 
 ## use mirror
-if
-[[ -n $SKIPDOWNLOAD ]]
+if [[ -n $SKIPDOWNLOAD ]]
 then
-SOURCETYPE=usemirrorfile
+  SOURCETYPE=usemirrorfile
 fi
 
 ## set the sourcetype if not
-if
-[[ -z $SOURCETYPE ]]
+if [[ -z $SOURCETYPE ]]
 then
-SOURCETYPE=unknown
+  SOURCETYPE=unknown
 fi
 
-if
-[[ -n $SOURCETYPE ]]
+if [[ -n $SOURCETYPE ]]
 then
-printf "$yellow"    "The Download Should use the $SOURCETYPE Preset."
-echo "SOURCETYPE="$SOURCETYPE"" | tee --append $TEMPPARSEVARS &>/dev/null
+  printf "$yellow"    "The Download Should use the $SOURCETYPE Preset."
+  echo "SOURCETYPE="$SOURCETYPE"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
