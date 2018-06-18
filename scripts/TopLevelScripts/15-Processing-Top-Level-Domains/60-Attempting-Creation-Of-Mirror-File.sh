a5fc5355c7525a424601f261a9ceee7ee9eed3e5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 09:50:27 -0400

    Update 60-Attempting-Creation-Of-Mirror-File.sh

--- scripts/TopLevelScripts/15-Processing-Top-Level-Domains/60-Attempting-Creation-Of-Mirror-File.sh
+++ scripts/TopLevelScripts/15-Processing-Top-Level-Domains/60-Attempting-Creation-Of-Mirror-File.sh
@@ -5,27 +5,23 @@
 SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
-if
-[[ -n $FULLSKIPPARSING ]]
+if [[ -n $FULLSKIPPARSING ]]
 then
-printf "$green"  "Old Mirror File Retained."
+  printf "$green"  "Old Mirror File Retained."
 fi
 
 ## This helps when replacing the mirrored file
-if 
-[[ -z $FULLSKIPPARSING && -z $FILESIZEZERO && -f $CURRENTTLDLIST ]]
+if  [[ -z $FULLSKIPPARSING && -z $FILESIZEZERO && -f $CURRENTTLDLIST ]]
 then
-printf "$green"  "Old Mirror File Removed"
-rm $CURRENTTLDLIST
+  printf "$green"  "Old Mirror File Removed"
+  rm $CURRENTTLDLIST
 fi
 
-if 
-[[ -z $FULLSKIPPARSING && -z $FILESIZEZERO ]]
+if  [[ -z $FULLSKIPPARSING && -z $FILESIZEZERO ]]
 then
-printf "$green"  "Creating Mirror Of Unparsed File."
-mv $BTEMPFILE $CURRENTTLDLIST
-elif 
-[[ -z $FULLSKIPPARSING && -n $FILESIZEZERO ]]
+  printf "$green"  "Creating Mirror Of Unparsed File."
+  mv $BTEMPFILE $CURRENTTLDLIST
+elif [[ -z $FULLSKIPPARSING && -n $FILESIZEZERO ]]
 then
-rm $BTEMPFILE
+  rm $BTEMPFILE
 fi
