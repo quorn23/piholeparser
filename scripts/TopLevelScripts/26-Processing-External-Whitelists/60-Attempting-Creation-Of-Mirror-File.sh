448b9cc396541021e8713ec77d3566342d1572a2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 10:18:18 -0400

    Update 60-Attempting-Creation-Of-Mirror-File.sh

--- scripts/TopLevelScripts/26-Processing-External-Whitelists/60-Attempting-Creation-Of-Mirror-File.sh
+++ scripts/TopLevelScripts/26-Processing-External-Whitelists/60-Attempting-Creation-Of-Mirror-File.sh
@@ -6,26 +6,22 @@ SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
 ## This helps when replacing the mirrored file
-if 
-[[ -n $ORIGFILESIZENOTZERO && -f $MIRROREDFILE ]]
+if  [[ -n $ORIGFILESIZENOTZERO && -f $MIRROREDFILE ]]
 then
-printf "$green"  "Old Mirror File Removed"
-rm $MIRROREDFILE
+  printf "$green"  "Old Mirror File Removed"
+  rm $MIRROREDFILE
 fi
 
 ## Github has a 100mb limit, and empty files are useless
-if 
-[[ "$ORIGFILESIZEBYTES" -eq 0 ]]
+if  [[ "$ORIGFILESIZEBYTES" -eq 0 ]]
 then
-printf "$red"     "Not Creating Mirror File. Nothing To Create!"
-elif
-[[ "$ORIGFILESIZEMB" -ge "$GITHUBLIMITMB" ]]
+  printf "$red"     "Not Creating Mirror File. Nothing To Create!"
+elif [[ "$ORIGFILESIZEMB" -ge "$GITHUBLIMITMB" ]]
 then
-printf "$red"     "Mirror File Too Large For Github."
-echo "* $BASEFILENAME list was $FETCHFILESIZEMB MB, and too large to mirror on github. $timestamp" | tee --append $RECENTRUN &>/dev/null
-elif
-[[ "$FETCHFILESIZEMB" -lt "$GITHUBLIMITMB" ]]
+  printf "$red"     "Mirror File Too Large For Github."
+  echo "* $BASEFILENAME list was $FETCHFILESIZEMB MB, and too large to mirror on github. $timestamp" | tee --append $RECENTRUN &>/dev/null
+elif [[ "$FETCHFILESIZEMB" -lt "$GITHUBLIMITMB" ]]
 then
-printf "$green"  "Creating Mirror Of Unparsed File."
-cp $WORIGINALFILETEMP $MIRROREDFILE
+  printf "$green"  "Creating Mirror Of Unparsed File."
+  cp $WORIGINALFILETEMP $MIRROREDFILE
 fi
