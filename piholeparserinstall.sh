681e195e0166b95dbc8ecf3f6e313c30a774e6b1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 08:17:21 -0400

    Update piholeparserinstall.sh

--- piholeparserinstall.sh
+++ piholeparserinstall.sh
@@ -41,7 +41,7 @@ fi
 ## Remove Prior install
 if [[ -n $PREVIOUSINSTALL ]]
 then
-  if (whiptail --title "$REPONAME" --yes-button "Remove beore install" --no-button "Abort" --yesno "$REPONAME is already installed?" 10 80) 
+  if (whiptail --title "$REPONAME" --yes-button "Remove beore install" --no-button "Abort" --yesno "$REPONAME is already installed?" 10 80)
   then
     rm -r /etc/"$REPONAME"
     rm /etc/updaterun"$REPONAME".sh
@@ -52,7 +52,7 @@ then
 fi
 
 ## obvious question
-if (whiptail --title ""$REPONAME"" --yes-button "yes" --no-button "no" --yesno "Do You want to install "$REPONAME"?" 10 80) 
+if (whiptail --title ""$REPONAME"" --yes-button "yes" --no-button "no" --yesno "Do You want to install "$REPONAME"?" 10 80)
 then
   git clone https://github.com/"$REPOOWNER"/"$REPONAME".git /etc/"$REPONAME"/
   cp /etc/"$REPONAME"/scripts/updaterun"$REPONAME".sh /etc/updaterun"$REPONAME".sh
@@ -64,7 +64,7 @@ fi
 ## Save a pervious config?
 if [[ -n $PREVIOUSINSTALL ]]
 then
-  if (whiptail --title ""$REPONAME"" --yes-button "keep config" --no-button "create new config" --yesno "Keep a previous config?" 10 80) 
+  if (whiptail --title ""$REPONAME"" --yes-button "keep config" --no-button "create new config" --yesno "Keep a previous config?" 10 80)
   then
     echo "keeping old config"
   else
@@ -74,7 +74,7 @@ then
 fi
 
 ## What version?
-if [[ -n $PREVIOUSINSTALL ]]
+if [[ -z $PREVIOUSINSTALL ]]
 then
   cp /etc/"$REPONAME"/scriptvars/"$REPONAME".var /etc/"$REPONAME".var
   if (whiptail --title ""$REPONAME"" --yes-button "Local Only" --no-button "I'll be uploading to Github" --yesno "What Version of "$REPONAME" to install?" 10 80) 
