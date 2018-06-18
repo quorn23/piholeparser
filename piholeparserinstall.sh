8e4202ee618d668ce972256a17e3d05aa3291520
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 07:53:51 -0400

    Update piholeparserinstall.sh

--- piholeparserinstall.sh
+++ piholeparserinstall.sh
@@ -14,76 +14,67 @@ REPOOWNER=deathbybandaid
 INSTALLPLACE=/etc/"$REPONAME"/
 
 ## Check for whiptail
-{ if
-which whiptail >/dev/null;
+if which whiptail >/dev/null;
 then
-:
+  :
 else
-apt-get install -y whiptail
-fi }
+  apt-get install -y whiptail
+fi
 
 ## Check for previous install
-if
-[[ -f $INSTALLPLACE ]]
+if [[ -f $INSTALLPLACE ]]
 then
-PREVIOUSINSTALL=true
+  PREVIOUSINSTALL=true
 fi
 
 ## Remove Prior install
-if
-[[ -z $PREVIOUSINSTALL ]]
+if [[ -z $PREVIOUSINSTALL ]]
 then
-{ if
-(whiptail --title "$REPONAME" --yes-button "Remove beore install" --no-button "Abort" --yesno "$REPONAME is already installed?" 10 80) 
-then
-rm -r /etc/"$REPONAME"
-rm /etc/updaterun"$REPONAME".sh
-crontab -l | grep -v 'sudo bash /etc/"$REPONAME".sh'  | crontab -
-else
-exit
-fi }
+  if (whiptail --title "$REPONAME" --yes-button "Remove beore install" --no-button "Abort" --yesno "$REPONAME is already installed?" 10 80) 
+  then
+    rm -r /etc/"$REPONAME"
+    rm /etc/updaterun"$REPONAME".sh
+    crontab -l | grep -v 'sudo bash /etc/"$REPONAME".sh'  | crontab -
+  else
+    exit
+  fi
 fi
 
 ## obvious question
-{ if
-(whiptail --title ""$REPONAME"" --yes-button "yes" --no-button "no" --yesno "Do You want to install "$REPONAME"?" 10 80) 
+if (whiptail --title ""$REPONAME"" --yes-button "yes" --no-button "no" --yesno "Do You want to install "$REPONAME"?" 10 80) 
 then
-git clone https://github.com/"$REPOOWNER"/"$REPONAME".git /etc/"$REPONAME"/
-cp /etc/"$REPONAME"/scripts/updaterun"$REPONAME".sh /etc/updaterun"$REPONAME".sh
-(crontab -l ; echo "20 0 * * * sudo bash /updaterun"$REPONAME".sh") | crontab -
+  git clone https://github.com/"$REPOOWNER"/"$REPONAME".git /etc/"$REPONAME"/
+  cp /etc/"$REPONAME"/scripts/updaterun"$REPONAME".sh /etc/updaterun"$REPONAME".sh
+  (crontab -l ; echo "20 0 * * * sudo bash /updaterun"$REPONAME".sh") | crontab -
 else
-exit
-fi }
+  exit
+fi
 
 ## Save a pervious config?
-if
-[[ -z $PREVIOUSINSTALL ]]
+if [[ -z $PREVIOUSINSTALL ]]
 then
-{ if
-(whiptail --title ""$REPONAME"" --yes-button "keep config" --no-button "create new config" --yesno "Keep a previous config?" 10 80) 
-then
-echo "keeping old config"
-else
-rm /etc/"$REPONAME".var
-cp /etc/"$REPONAME"/scriptvars/"$REPONAME".var /etc/"$REPONAME".var
-fi }
+  if (whiptail --title ""$REPONAME"" --yes-button "keep config" --no-button "create new config" --yesno "Keep a previous config?" 10 80) 
+  then
+    echo "keeping old config"
+  else
+    rm /etc/"$REPONAME".var
+    cp /etc/"$REPONAME"/scriptvars/"$REPONAME".var /etc/"$REPONAME".var
+  fi
 fi
 
 ## What version?
-if
-[[ -n $PREVIOUSINSTALL ]]
+if [[ -n $PREVIOUSINSTALL ]]
 then
-{ if 
-(whiptail --title ""$REPONAME"" --yes-button "Local Only" --no-button "I'll be uploading to Github" --yesno "What Version of "$REPONAME" to install?" 10 80) 
-then
-echo "version=local" | tee --append /etc/"$REPONAME".var
-else
-echo "version=github" | tee --append /etc/"$REPONAME".var
-GITHUBUSERNAME=$(whiptail --inputbox "Github Username" 10 80 "" 3>&1 1>&2 2>&3)
-GITHUBPASSWORD=$(whiptail --inputbox "Github Password" 10 80 "" 3>&1 1>&2 2>&3)
-GITHUBEMAIL=$(whiptail --inputbox "Github Email Address" 10 80 "" 3>&1 1>&2 2>&3)
-echo "GITHUBUSERNAME="$GITHUBUSERNAME"" | tee --append /etc/"$REPONAME".var
-echo "GITHUBPASSWORD="$GITHUBPASSWORD"" | tee --append /etc/"$REPONAME".var
-echo "GITHUBEMAIL="$GITHUBEMAIL"" | tee --append /etc/"$REPONAME".var
-fi }
+  if (whiptail --title ""$REPONAME"" --yes-button "Local Only" --no-button "I'll be uploading to Github" --yesno "What Version of "$REPONAME" to install?" 10 80) 
+  then
+    echo "version=local" | tee --append /etc/"$REPONAME".var
+  else
+    echo "version=github" | tee --append /etc/"$REPONAME".var
+    GITHUBUSERNAME=$(whiptail --inputbox "Github Username" 10 80 "" 3>&1 1>&2 2>&3)
+    GITHUBPASSWORD=$(whiptail --inputbox "Github Password" 10 80 "" 3>&1 1>&2 2>&3)
+    GITHUBEMAIL=$(whiptail --inputbox "Github Email Address" 10 80 "" 3>&1 1>&2 2>&3)
+    echo "GITHUBUSERNAME="$GITHUBUSERNAME"" | tee --append /etc/"$REPONAME".var
+    echo "GITHUBPASSWORD="$GITHUBPASSWORD"" | tee --append /etc/"$REPONAME".var
+    echo "GITHUBEMAIL="$GITHUBEMAIL"" | tee --append /etc/"$REPONAME".var
+  fi
 fi
