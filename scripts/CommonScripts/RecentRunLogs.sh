70002b0015d651daa4f10e0527aaf5129abef69a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:57:38 -0400

    Update RecentRunLogs.sh

--- scripts/CommonScripts/RecentRunLogs.sh
+++ scripts/CommonScripts/RecentRunLogs.sh
@@ -11,57 +11,50 @@ timestamp=$(echo `date`)
 echo "## $SCRIPTTEXT $timestamp" | tee --append $TEMPLOGMAIN &>/dev/null
 
 CHECKME=$RECENTRUNLOGSDIRCLEAN
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-rm -rf $CHECKME
+  rm -rf $CHECKME
 fi
 echo ""
 
 CHECKME=$CLEANRECENTRUNLOGSONE
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-rm $CHECKME
+  rm $CHECKME
 fi
 echo ""
 
 CHECKME=$CLEANRECENTRUNLOGSTWO
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-rm $CHECKME
+  rm $CHECKME
 fi
 echo ""
 
 CHECKME=$CLEANRECENTRUNLOGSTHREE
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-rm $CHECKME
+  rm $CHECKME
 fi
 echo ""
 
-if
-[[ -f $MAINRECENTRUNLOGMD ]]
+if [[ -f $MAINRECENTRUNLOGMD ]]
 then
-echo "* Old Recent Run Log Purged." | tee --append $TEMPLOGMAIN &>/dev/null
-rm $MAINRECENTRUNLOGMD
+  echo "* Old Recent Run Log Purged." | tee --append $TEMPLOGMAIN &>/dev/null
+  rm $MAINRECENTRUNLOGMD
 fi
 
-if
-[[ -f $TEMPLOGMAIN ]]
+if [[ -f $TEMPLOGMAIN ]]
 then
-echo "* Recent Run Log Recreated." | tee --append $TEMPLOGMAIN &>/dev/null
-mv $TEMPLOGMAIN $MAINRECENTRUNLOGMD
+  echo "* Recent Run Log Recreated." | tee --append $TEMPLOGMAIN &>/dev/null
+  mv $TEMPLOGMAIN $MAINRECENTRUNLOGMD
 fi
 
 CHECKME=$TOPLEVELSCRIPTSLOGSDIR
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-:
+  :
 else
-mkdir $CHECKME
+  mkdir $CHECKME
 fi
 echo ""
