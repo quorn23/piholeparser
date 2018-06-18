553d634a3fbc60b97d9f669583ab9874a9d3fc9a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:34:40 -0400

    Update 60-Compiling-Combined-List.sh

--- scripts/TopLevelScripts/45-Compiling-Combined-Whitelist/60-Compiling-Combined-List.sh
+++ scripts/TopLevelScripts/45-Compiling-Combined-Whitelist/60-Compiling-Combined-List.sh
@@ -8,113 +8,99 @@ source "$SCRIPTDIRA"/foldervars.var
 
 WHATITIS="All Parsed List"
 timestamp=$(echo `date`)
-if
-[[ -f $COMBINEDWHITELISTS ]]
+if [[ -f $COMBINEDWHITELISTS ]]
 then
-rm $COMBINEDWHITELISTS
-echo "* $WHATITIS Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  rm $COMBINEDWHITELISTS
+  echo "* $WHATITIS Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
 else
-echo "* $WHATITIS Not Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  echo "* $WHATITIS Not Removed. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
 
-if
-[[ ! -f $WHITELISTTEMP ]]
+if [[ ! -f $WHITELISTTEMP ]]
 then
-printf "$red"  "Whitelist File Missing."
-MISSINGWHITE=true
+  printf "$red"  "Whitelist File Missing."
+  MISSINGWHITE=true
 fi
 
 ## Combine Small lists
 printf "$yellow"  "Merging Individual Lists."
-if
-ls $PARSEDWHITELISTSSUBALL &> /dev/null;
+if ls $PARSEDWHITELISTSSUBALL &> /dev/null;
 then
-cat $PARSEDWHITELISTSSUBALL >> $TEMPFILE
-echo -e "`wc -l $TEMPFILE | cut -d " " -f 1` lines after merging individual lists"
+  cat $PARSEDWHITELISTSSUBALL >> $TEMPFILE
+  echo -e "`wc -l $TEMPFILE | cut -d " " -f 1` lines after merging individual lists"
 else
-printf "$red"  "No Parsed Files Available To Merge."
-touch $TEMPFILE
-INDIVIDUALMERGEFAILED=true
+  printf "$red"  "No Parsed Files Available To Merge."
+  touch $TEMPFILE
+  INDIVIDUALMERGEFAILED=true
 fi
 echo ""
 
 ## Add Script.domains
-if
-[[ -z $MISSINGWHITE ]]
+if [[ -z $MISSINGWHITE ]]
 then
-printf "$yellow"  "Adding Whitelist Domains."
-cat $WHITELISTTEMP $TEMPFILE >> $FILETEMP
-rm $TEMPFILE
-echo -e "`wc -l $FILETEMP | cut -d " " -f 1` lines after whitelist"
-echo ""
+  printf "$yellow"  "Adding Whitelist Domains."
+  cat $WHITELISTTEMP $TEMPFILE >> $FILETEMP
+  rm $TEMPFILE
+  echo -e "`wc -l $FILETEMP | cut -d " " -f 1` lines after whitelist"
+  echo ""
 else
-cp $COMBINEDWHITELISTS $FILETEMP
+  cp $COMBINEDWHITELISTS $FILETEMP
 fi
 
 ## Duplicate Removal
-if
-[[ -f $FILETEMP ]]
+if [[ -f $FILETEMP ]]
 then
-printf "$yellow"  "Removing duplicates."
-cat -s $FILETEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' >> $TEMPFILE
-echo -e "`wc -l $TEMPFILE | cut -d " " -f 1` lines after deduping"
-rm $FILETEMP
-echo ""
+  printf "$yellow"  "Removing duplicates."
+  cat -s $FILETEMP | sort -u | gawk '{if (++dup[$0] == 1) print $0;}' >> $TEMPFILE
+  echo -e "`wc -l $TEMPFILE | cut -d " " -f 1` lines after deduping"
+  rm $FILETEMP
+  echo ""
 fi
 
-if
-[[ -f $TEMPFILE ]]
+if [[ -f $TEMPFILE ]]
 then
-ALLPARSEDSIZEBYTES=$(stat -c%s "$TEMPFILE")
-ALLPARSEDSIZEKB=`expr $ALLPARSEDSIZEBYTES / 1024`
-ALLPARSEDSIZEMB=`expr $ALLPARSEDSIZEBYTES / 1024 / 1024`
-echo "ALLPARSEDSIZEMB="$ALLPARSEDSIZEMB"" | tee --append $TEMPVARS &>/dev/null
+  ALLPARSEDSIZEBYTES=$(stat -c%s "$TEMPFILE")
+  ALLPARSEDSIZEKB=`expr $ALLPARSEDSIZEBYTES / 1024`
+  ALLPARSEDSIZEMB=`expr $ALLPARSEDSIZEBYTES / 1024 / 1024`
+  echo "ALLPARSEDSIZEMB="$ALLPARSEDSIZEMB"" | tee --append $TEMPVARS &>/dev/null
 fi
 
-if
-[[ "$ALLPARSEDSIZEMB" -gt 0 && "$ALLPARSEDSIZEKB" -gt 0 && "$ALLPARSEDSIZEBYTES" -gt 0 ]]
+if [[ "$ALLPARSEDSIZEMB" -gt 0 && "$ALLPARSEDSIZEKB" -gt 0 && "$ALLPARSEDSIZEBYTES" -gt 0 ]]
 then
-printf "$yellow"  "Size of $BASEFILENAME = $ALLPARSEDSIZEMB MB."
-elif
-[[ "$ALLPARSEDSIZEMB" -eq 0 && "$ALLPARSEDSIZEKB" -gt 0 && "$ALLPARSEDSIZEBYTES" -gt 0 ]]
+  printf "$yellow"  "Size of $BASEFILENAME = $ALLPARSEDSIZEMB MB."
+elif [[ "$ALLPARSEDSIZEMB" -eq 0 && "$ALLPARSEDSIZEKB" -gt 0 && "$ALLPARSEDSIZEBYTES" -gt 0 ]]
 then
-printf "$yellow"  "Size of $BASEFILENAME = $ALLPARSEDSIZEKB KB."
-elif
-[[ "$ALLPARSEDSIZEMB" -eq 0 && "$ALLPARSEDSIZEKB" -eq 0 && "$ALLPARSEDSIZEBYTES" -gt 0 ]]
+  printf "$yellow"  "Size of $BASEFILENAME = $ALLPARSEDSIZEKB KB."
+elif [[ "$ALLPARSEDSIZEMB" -eq 0 && "$ALLPARSEDSIZEKB" -eq 0 && "$ALLPARSEDSIZEBYTES" -gt 0 ]]
 then
-printf "$yellow"  "Size of List = $ALLPARSEDSIZEBYTES Bytes."
+  printf "$yellow"  "Size of List = $ALLPARSEDSIZEBYTES Bytes."
 fi
 
-if
-[[ "$ALLPARSEDSIZEBYTES" -gt 0 ]]
+if [[ "$ALLPARSEDSIZEBYTES" -gt 0 ]]
 then
-PARSEDHOWMANYLINES=$(echo -e "`wc -l $TEMPFILE | cut -d " " -f 1`")
-printf "$yellow"  "$PARSEDHOWMANYLINES Lines After Compiling."
+  PARSEDHOWMANYLINES=$(echo -e "`wc -l $TEMPFILE | cut -d " " -f 1`")
+  printf "$yellow"  "$PARSEDHOWMANYLINES Lines After Compiling."
 fi
 
-if
-[[ -f $COMBINEDWHITELISTS && "$ALLPARSEDSIZEBYTES" -gt 0 ]]
+if [[ -f $COMBINEDWHITELISTS && "$ALLPARSEDSIZEBYTES" -gt 0 ]]
 then
-printf "$green"  "Old COMBINEDWHITELISTS File Removed."
-rm $PARSEDFILE
+  printf "$green"  "Old COMBINEDWHITELISTS File Removed."
+  rm $PARSEDFILE
 fi
 
 ## Github has a 100mb limit, and empty files are useless
-if 
-[[ "$ALLPARSEDSIZEBYTES" -eq 0 ]]
+if  [[ "$ALLPARSEDSIZEBYTES" -eq 0 ]]
 then
-printf "$red"     "File Empty"
-echo "* Allparsedlist list was an empty file $timestamp" | tee --append $RECENTRUN &>/dev/null
-mv $TEMPFILE $COMBINEDWHITELISTS
-elif
-[[ "$ALLPARSEDSIZEMB" -ge "$GITHUBLIMITMB" ]]
+  printf "$red"     "File Empty"
+  echo "* Allparsedlist list was an empty file $timestamp" | tee --append $RECENTRUN &>/dev/null
+  mv $TEMPFILE $COMBINEDWHITELISTS
+elif [[ "$ALLPARSEDSIZEMB" -ge "$GITHUBLIMITMB" ]]
 then
-printf "$red"     "Parsed File Too Large For Github. Deleting."
-echo "* Allparsedlist list was too large to host on github. $FETCHFILESIZE bytes $timestamp" | tee --append $RECENTRUN &>/dev/null
-mv $TEMPFILE $COMBINEDWHITELISTS
-elif
-[[ "$ALLPARSEDSIZEMB" -lt "$GITHUBLIMITMB" && -f $TEMPFILE ]]
+  printf "$red"     "Parsed File Too Large For Github. Deleting."
+  echo "* Allparsedlist list was too large to host on github. $FETCHFILESIZE bytes $timestamp" | tee --append $RECENTRUN &>/dev/null
+  mv $TEMPFILE $COMBINEDWHITELISTS
+elif [[ "$ALLPARSEDSIZEMB" -lt "$GITHUBLIMITMB" && -f $TEMPFILE ]]
 then
-mv $TEMPFILE $COMBINEDWHITELISTS
-printf "$yellow"  "Big List Created Successfully."
+  mv $TEMPFILE $COMBINEDWHITELISTS
+  printf "$yellow"  "Big List Created Successfully."
 fi
