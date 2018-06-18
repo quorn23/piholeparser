41f74f7a511f9ddcc179005a9c2d11360c7a2242
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 09:42:55 -0400

    Update 08-Checking-For-HTTPS.sh

--- scripts/TopLevelScripts/15-Processing-Top-Level-Domains/08-Checking-For-HTTPS.sh
+++ scripts/TopLevelScripts/15-Processing-Top-Level-Domains/08-Checking-For-HTTPS.sh
@@ -6,10 +6,9 @@ SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
 ## Is source not using https
-if
-[[ $source != https* ]]
+if [[ $source != https* ]]
 then
-printf "$red"    "$BASEFILENAME List Does NOT Use https."
+  printf "$red"    "$BASEFILENAME List Does NOT Use https."
 else
-printf "$green"    "$BASEFILENAME List Does Use https."
+  printf "$green"    "$BASEFILENAME List Does Use https."
 fi
