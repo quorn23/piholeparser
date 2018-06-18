0b6d5ebe527b92aaa8ef282d3c0b341636e24283
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 08:51:13 -0400

    Update 08-Recreating-TempVar-File.sh

--- scripts/TopLevelScripts/10-Running-Initial-Tasks/08-Recreating-TempVar-File.sh
+++ scripts/TopLevelScripts/10-Running-Initial-Tasks/08-Recreating-TempVar-File.sh
@@ -7,30 +7,28 @@ source "$SCRIPTDIRA"/foldervars.var
 
 SCRIPTTEXT="Checking For Old TempVars File."
 printf "$cyan"    "$SCRIPTTEXT"
-echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
-if
-[[ -f $TEMPVARS ]]
+echo "### $SCRIPTTEXT" | tee --append $RECENTRUN &>/dev/null
+if [[ -f $TEMPVARS ]]
 then
-rm $TEMPVARS
-printf "$red"   "Purging Old TempVars File."
-echo "* Old TempVars File Purged." | tee --append $RECENTRUN &>/dev/null
+  rm $TEMPVARS
+  printf "$red"   "Purging Old TempVars File."
+  echo "* Old TempVars File Purged." | tee --append $RECENTRUN &>/dev/null
 else
-echo "* Old TempVars File Not Present." | tee --append $RECENTRUN &>/dev/null
+  echo "* Old TempVars File Not Present." | tee --append $RECENTRUN &>/dev/null
 fi
 echo "" | tee --append $RECENTRUN &>/dev/null
 echo ""
 
 SCRIPTTEXT="Checking For Other Temp Vars Files."
 printf "$cyan"    "$SCRIPTTEXT"
-echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
+echo "### $SCRIPTTEXT" | tee --append $RECENTRUN &>/dev/null
 CHECKME=$TEMPCLEANUPTHREE
-if
-ls $CHECKME &> /dev/null;
+if ls $CHECKME &> /dev/null;
 then
-echo "* Other Temp Vars Files Purged." | tee --append $RECENTRUN &>/dev/null
-rm $CHECKME
+  echo "* Other Temp Vars Files Purged." | tee --append $RECENTRUN &>/dev/null
+  rm $CHECKME
 else
-echo "* Other Temp Vars Files Purged." | tee --append $RECENTRUN &>/dev/null
+  echo "* Other Temp Vars Files Purged." | tee --append $RECENTRUN &>/dev/null
 fi
 echo "" | tee --append $RECENTRUN &>/dev/null
 echo ""
@@ -39,14 +37,13 @@ echo "## Vars that we don't keep" | tee --append $TEMPVARS &>/dev/null
 
 SCRIPTTEXT="Checking For TempVars File."
 printf "$cyan"    "$SCRIPTTEXT"
-echo "### $SCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
-if
-[[ -f $TEMPVARS ]]
+echo "### $SCRIPTTEXT" | tee --append $RECENTRUN &>/dev/null
+if [[ -f $TEMPVARS ]]
 then
-printf "$yellow"   "TempVars File Recreated."
-echo "* TempVars File Recreated." | tee --append $RECENTRUN &>/dev/null
+  printf "$yellow"   "TempVars File Recreated."
+  echo "* TempVars File Recreated." | tee --append $RECENTRUN &>/dev/null
 else
-printf "$red"   "TempVars File Missing, Exiting."
-echo "* TempVars Files Missing, Script Exited." | tee --append $RECENTRUN &>/dev/null
-exit
+  printf "$red"   "TempVars File Missing, Exiting."
+  echo "* TempVars Files Missing, Script Exited." | tee --append $RECENTRUN &>/dev/null
+  exit
 fi
