641e1f9997d30af71f844fc377efac15f6aa3c02
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 09:47:02 -0400

    Update 47-Determining-Download-Attempt.sh

--- scripts/TopLevelScripts/15-Processing-Top-Level-Domains/47-Determining-Download-Attempt.sh
+++ scripts/TopLevelScripts/15-Processing-Top-Level-Domains/47-Determining-Download-Attempt.sh
@@ -6,71 +6,58 @@ SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
 ## What type of source?
-if
-[[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.7z ]]
+if [[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.7z ]]
 then
-SOURCETYPE=sevenzip
-elif
-[[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.tar.gz ]]
+  SOURCETYPE=sevenzip
+elif [[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.tar.gz ]]
 then
-SOURCETYPE=tar
-elif
-[[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.zip ]]
+  SOURCETYPE=tar
+elif [[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.zip ]]
 then
-SOURCETYPE=zip
-elif
-[[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.php ]]
+  SOURCETYPE=zip
+elif [[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.php ]]
 then
-SOURCETYPE=php
-elif
-[[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.htm ]]
+  SOURCETYPE=php
+elif [[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.htm ]]
 then
-SOURCETYPE=htm
-elif
-[[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.html ]]
+  SOURCETYPE=htm
+elif [[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.html ]]
 then
-SOURCETYPE=html
-elif
-[[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.txt ]]
+  SOURCETYPE=html
+elif [[ -z $FULLSKIPPARSING && -z $PINGTESTFAILED && $source == *.txt ]]
 then
-SOURCETYPE=text
+  SOURCETYPE=text
 fi
 
 ## use mirror
-if
-[[ -z $FULLSKIPPARSING && -n $PINGTESTFAILED && -f $CURRENTTLDLIST ]]
+if [[ -z $FULLSKIPPARSING && -n $PINGTESTFAILED && -f $CURRENTTLDLIST ]]
 then
-SOURCETYPE=usemirrorfile
+  SOURCETYPE=usemirrorfile
 fi
 
 ## set the sourcetype if not
-if
-[[ -z $FULLSKIPPARSING && -z $SOURCETYPE ]]
+if [[ -z $FULLSKIPPARSING && -z $SOURCETYPE ]]
 then
-SOURCETYPE=unknown
-elif
-[[ -n $FULLSKIPPARSING && -z $SOURCETYPE ]]
+  SOURCETYPE=unknown
+elif [[ -n $FULLSKIPPARSING && -z $SOURCETYPE ]]
 then
-SOURCETYPE=skip
+  SOURCETYPE=skip
 fi
 
 ## Add to tempvars
-if
-[[ -z $FULLSKIPPARSING && -n $SOURCETYPE ]]
+if [[ -z $FULLSKIPPARSING && -n $SOURCETYPE ]]
 then
-echo "SOURCETYPE="$SOURCETYPE"" | tee --append $TEMPPARSEVARS &>/dev/null
+  echo "SOURCETYPE="$SOURCETYPE"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
 
 ## Terminal Display
 printf "$yellow"    "The Download Should use the $SOURCETYPE Preset."
 timestamp=$(echo `date`)
-if
-[[ -z $FULLSKIPPARSING && -n $SOURCEIP && -n $SOURCEDOMAIN && -n $SOURCETYPE ]]
+if [[ -z $FULLSKIPPARSING && -n $SOURCEIP && -n $SOURCEDOMAIN && -n $SOURCETYPE ]]
 then
-printf "$cyan"    "Fetching $SOURCETYPE List From $SOURCEDOMAIN Located At The IP address Of "$SOURCEIP"."
-elif
-[[ -z $FULLSKIPPARSING && $SOURCETYPE == usemirrorfile ]]
+  printf "$cyan"    "Fetching $SOURCETYPE List From $SOURCEDOMAIN Located At The IP address Of "$SOURCEIP"."
+elif [[ -z $FULLSKIPPARSING && $SOURCETYPE == usemirrorfile ]]
 then
-printf "$cyan"    "Attempting To Fetch List From Git Repo Mirror."
-echo "* $BASEFILENAME List Unavailable To Download. Attempted to use Mirror. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  printf "$cyan"    "Attempting To Fetch List From Git Repo Mirror."
+  echo "* $BASEFILENAME List Unavailable To Download. Attempted to use Mirror. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
