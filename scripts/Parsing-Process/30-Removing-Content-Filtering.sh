69aefae468800f458984dde8e6c74424b1e1fee1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:59:38 -0400

    Update 30-Removing-Content-Filtering.sh

--- scripts/Parsing-Process/30-Removing-Content-Filtering.sh
+++ scripts/Parsing-Process/30-Removing-Content-Filtering.sh
@@ -5,14 +5,12 @@
 SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
-if
-[[ -f $BFILETEMP ]]
+if [[ -f $BFILETEMP ]]
 then
-cat $BFILETEMP | sed '/^https\:\/\//d; /^http\:\/\//d; s/[\^]third-party$//g; s/[\^]popup$//g; s/[\^]important$//g; s/[\^]subdocument$//g; s/[\^]websocket$//g; s/^||//; s/[\^]$//g' > $BTEMPFILE
+  cat $BFILETEMP | sed '/^https\:\/\//d; /^http\:\/\//d; s/[\^]third-party$//g; s/[\^]popup$//g; s/[\^]important$//g; s/[\^]subdocument$//g; s/[\^]websocket$//g; s/^||//; s/[\^]$//g' > $BTEMPFILE
 fi
 
-if
-[[ -f $BFILETEMP ]]
+if [[ -f $BFILETEMP ]]
 then
-rm $BFILETEMP
+  rm $BFILETEMP
 fi
