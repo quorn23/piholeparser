1a4d4f8525c313099e42aabdf2a29b0ab544b19b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:27:31 -0400

    Update 65-Parsing-List.sh

--- scripts/TopLevelScripts/30-Processing-External-Blacklists/65-Parsing-List.sh
+++ scripts/TopLevelScripts/30-Processing-External-Blacklists/65-Parsing-List.sh
@@ -10,36 +10,31 @@ STARTPARSESTAMP=$(date +"%s")
 echo "STARTPARSESTAMP="$STARTPARSESTAMP"" | tee --append $TEMPPARSEVARS &>/dev/null
 
 ## Cheap error handling
-if
-[[ -f $BFILETEMP ]]
+if [[ -f $BFILETEMP ]]
 then
-rm $BFILETEMP
+  rm $BFILETEMP
 fi
-if
-[[ -f $BTEMPFILE ]]
+if [[ -f $BTEMPFILE ]]
 then
-rm $BTEMPFILE
+  rm $BTEMPFILE
 fi
 
 ## Transition File for Processing
-if
-[[ -f $BORIGINALFILETEMP ]]
+if [[ -f $BORIGINALFILETEMP ]]
 then
-cp $BORIGINALFILETEMP $BFILETEMP
+  cp $BORIGINALFILETEMP $BFILETEMP
 fi
 
-if
-[[ ! -f $BFILETEMP ]]
+if [[ ! -f $BFILETEMP ]]
 then
-touch $BFILETEMP
+  touch $BFILETEMP
 fi
 
 HOWMANYLINES=$(echo -e "`wc -l $BFILETEMP | cut -d " " -f 1`")
-if
-[[ $HOWMANYLINES -eq 0 ]]
+if [[ $HOWMANYLINES -eq 0 ]]
 then
-GOTOENDPARSING=true
-echo "GOTOENDPARSING="$GOTOENDPARSING"" | tee --append $TEMPPARSEVARS &>/dev/null
+  GOTOENDPARSING=true
+  echo "GOTOENDPARSING="$GOTOENDPARSING"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
 
 ## Start File Loop
@@ -48,46 +43,41 @@ echo ""
 for p in $PARSINGPROCESSSCRIPTSALL
 do
 
-PBASEFILENAME=$(echo `basename $p | cut -f 1 -d '.'`)
-PBASEFILENAMEDASHNUM=$(echo $PBASEFILENAME | sed 's/[0-9\-]/ /g')
-PBNAMEPRETTYSCRIPTTEXT=$(echo $PBASEFILENAMEDASHNUM)
+  PBASEFILENAME=$(echo `basename $p | cut -f 1 -d '.'`)
+  PBASEFILENAMEDASHNUM=$(echo $PBASEFILENAME | sed 's/[0-9\-]/ /g')
+  PBNAMEPRETTYSCRIPTTEXT=$(echo $PBASEFILENAMEDASHNUM)
 
-if
-[[ -f $TEMPPARSEVARS ]]
-then
-source $TEMPPARSEVARS
-fi
+  if [[ -f $TEMPPARSEVARS ]]
+  then
+    source $TEMPPARSEVARS
+  fi
 
-if
-[[ -z $GOTOENDPARSING ]]
-then
-printf "$cyan"  "$PBNAMEPRETTYSCRIPTTEXT"
-echo "### $PBNAMEPRETTYSCRIPTTEXT" | sudo tee --append $RECENTRUN &>/dev/null
-HOWMANYLINESSTART=$(echo -e "`wc -l $BFILETEMP | cut -d " " -f 1`")
-bash $p
-HOWMANYLINES=$(echo -e "`wc -l $BTEMPFILE | cut -d " " -f 1`")
-ENDCOMMENT="$HOWMANYLINES Lines After $PBNAMEPRETTYSCRIPTTEXT"
-echo "$ENDCOMMENT" | sudo tee --append $RECENTRUN &>/dev/null
+  if [[ -z $GOTOENDPARSING ]]
+  then
+    printf "$cyan"  "$PBNAMEPRETTYSCRIPTTEXT"
+    echo "### $PBNAMEPRETTYSCRIPTTEXT" | tee --append $RECENTRUN &>/dev/null
+    HOWMANYLINESSTART=$(echo -e "`wc -l $BFILETEMP | cut -d " " -f 1`")
+    bash $p
+    HOWMANYLINES=$(echo -e "`wc -l $BTEMPFILE | cut -d " " -f 1`")
+    ENDCOMMENT="$HOWMANYLINES Lines After $PBNAMEPRETTYSCRIPTTEXT"
+    echo "$ENDCOMMENT" | tee --append $RECENTRUN &>/dev/null
 
-{ if
-[[ $HOWMANYLINES -eq 0 && $HOWMANYLINESSTART -lt $HOWMANYLINES ]]
-then
-printf "$red"  "$ENDCOMMENT List File Now Empty."
-GOTOENDPARSING=true
-echo "GOTOENDPARSING="$GOTOENDPARSING"" | tee --append $TEMPPARSEVARS &>/dev/null
-elif
-[[ $HOWMANYLINES -gt 0 && $HOWMANYLINES -eq $HOWMANYLINESSTART ]]
-then
-printf "$yellow"    "$ENDCOMMENT"
-elif
-[[ $HOWMANYLINES -gt 0 && $HOWMANYLINES -lt $HOWMANYLINESSTART ]]
-then
-printf "$green"    "$ENDCOMMENT"
-fi }
+    if [[ $HOWMANYLINES -eq 0 && $HOWMANYLINESSTART -lt $HOWMANYLINES ]]
+    then
+      printf "$red"  "$ENDCOMMENT List File Now Empty."
+      GOTOENDPARSING=true
+      echo "GOTOENDPARSING="$GOTOENDPARSING"" | tee --append $TEMPPARSEVARS &>/dev/null
+    elif [[ $HOWMANYLINES -gt 0 && $HOWMANYLINES -eq $HOWMANYLINESSTART ]]
+    then
+      printf "$yellow"    "$ENDCOMMENT"
+    elif [[ $HOWMANYLINES -gt 0 && $HOWMANYLINES -lt $HOWMANYLINESSTART ]]
+    then
+      printf "$green"    "$ENDCOMMENT"
+    fi
 
-mv $BTEMPFILE $BFILETEMP
-echo ""
-fi
+    mv $BTEMPFILE $BFILETEMP
+    echo ""
+  fi
 
 done
 
@@ -96,30 +86,25 @@ ENDPARSESTAMP=$(date +"%s")
 echo "ENDPARSESTAMP="$ENDPARSESTAMP"" | tee --append $TEMPPARSEVARS &>/dev/null
 
 ## Prepare for next step
-if
-[[ -f $BFILETEMP ]]
+if [[ -f $BFILETEMP ]]
 then
 HOWMANYLINES=$(echo -e "`wc -l $BFILETEMP | cut -d " " -f 1`")
-{ if
-[[ $HOWMANYLINES -eq 0 ]]
-then
-PARSINGEMPTIEDFILE=true
-echo "PARSINGEMPTIEDFILE="$PARSINGEMPTIEDFILE"" | tee --append $TEMPPARSEVARS &>/dev/null
-elif
-[[ $HOWMANYLINES -ge 1 ]]
-then
-cp $BFILETEMP $BPARSEDFILETEMP
-fi }
+  if [[ $HOWMANYLINES -eq 0 ]]
+  then
+    PARSINGEMPTIEDFILE=true
+    echo "PARSINGEMPTIEDFILE="$PARSINGEMPTIEDFILE"" | tee --append $TEMPPARSEVARS &>/dev/null
+  elif [[ $HOWMANYLINES -ge 1 ]]
+  then
+    cp $BFILETEMP $BPARSEDFILETEMP
+  fi
 fi
 
 ## Cheap error handling
-if
-[[ -f $BFILETEMP ]]
+if [[ -f $BFILETEMP ]]
 then
-rm $BFILETEMP
+  rm $BFILETEMP
 fi
-if
-[[ -f $BTEMPFILE ]]
+if [[ -f $BTEMPFILE ]]
 then
-rm $BTEMPFILE
+  rm $BTEMPFILE
 fi
