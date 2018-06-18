833f947dc60dd3450812145ca8a00b6d08672558
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:18:53 -0400

    Update 44-Determining-Host-Availability.sh

--- scripts/TopLevelScripts/30-Processing-External-Blacklists/44-Determining-Host-Availability.sh
+++ scripts/TopLevelScripts/30-Processing-External-Blacklists/44-Determining-Host-Availability.sh
@@ -6,34 +6,30 @@ SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
 ## host unavailable
-if
-[[ -n $TESTPINGFAILED && -n $TESTCURLHEADERFAILED && -n $TESTWGETHEADERFAILED ]]
+if [[ -n $TESTPINGFAILED && -n $TESTCURLHEADERFAILED && -n $TESTWGETHEADERFAILED ]]
 then
-printf "$red"  "Host Unavailable."
-PINGTESTFAILED=true
-echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
+  printf "$red"  "Host Unavailable."
+  PINGTESTFAILED=true
+  echo "PINGTESTFAILED="$PINGTESTFAILED"" | tee --append $TEMPPARSEVARS &>/dev/null
 else
-printf "$green"  "Host Available."
+  printf "$green"  "Host Available."
 fi
 
 ## Dead List?
 timestamp=$(echo `date`)
-if
-[[ -n $PINGTESTFAILED && $FILEBEINGPROCESSED != $BDEADPARSELIST ]]
+if [[ -n $PINGTESTFAILED && $FILEBEINGPROCESSED != $BDEADPARSELIST ]]
 then
-printf "$red"  "List Marked As Dead."
-echo "* $BASEFILENAME List Marked As Dead. $timestamp" | tee --append $RECENTRUN &>/dev/null
-mv $FILEBEINGPROCESSED $BDEADPARSELIST
-elif
-[[ -n $PINGTESTFAILED && $FILEBEINGPROCESSED == $BDEADPARSELIST ]]
+  printf "$red"  "List Marked As Dead."
+  echo "* $BASEFILENAME List Marked As Dead. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  mv $FILEBEINGPROCESSED $BDEADPARSELIST
+elif [[ -n $PINGTESTFAILED && $FILEBEINGPROCESSED == $BDEADPARSELIST ]]
 then
-printf "$red"  "List Already Marked As Dead."
-echo "* $BASEFILENAME List Already Marked As Dead. $timestamp" | tee --append $RECENTRUN &>/dev/null
-elif
-[[ -z $PINGTESTFAILED && $FILEBEINGPROCESSED == $BDEADPARSELIST ]]
+  printf "$red"  "List Already Marked As Dead."
+  echo "* $BASEFILENAME List Already Marked As Dead. $timestamp" | tee --append $RECENTRUN &>/dev/null
+elif [[ -z $PINGTESTFAILED && $FILEBEINGPROCESSED == $BDEADPARSELIST ]]
 then
-printf "$green"  "List Was Marked As Dead, But Now Works."
-UNDEADLIST=true
-echo "UNDEADLIST="$UNDEADLIST"" | tee --append $TEMPPARSEVARS &>/dev/null
-echo "* $BASEFILENAME List Unavailable To Download. $timestamp" | tee --append $RECENTRUN &>/dev/null
+  printf "$green"  "List Was Marked As Dead, But Now Works."
+  UNDEADLIST=true
+  echo "UNDEADLIST="$UNDEADLIST"" | tee --append $TEMPPARSEVARS &>/dev/null
+  echo "* $BASEFILENAME List Unavailable To Download. $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
