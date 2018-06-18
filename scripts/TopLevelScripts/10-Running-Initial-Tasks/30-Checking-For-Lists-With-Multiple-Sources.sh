2fefb9bf6c33bfe9b87d6173644cc181ff73e346
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 08:58:29 -0400

    Update 30-Checking-For-Lists-With-Multiple-Sources.sh

--- scripts/TopLevelScripts/10-Running-Initial-Tasks/30-Checking-For-Lists-With-Multiple-Sources.sh
+++ scripts/TopLevelScripts/10-Running-Initial-Tasks/30-Checking-For-Lists-With-Multiple-Sources.sh
@@ -6,44 +6,41 @@ SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
 ## Process Every .lst file within the List Directories
-if
-ls $BLACKLSTALL &> /dev/null;
+if ls $BLACKLSTALL &> /dev/null;
 then
-for f in $BLACKLSTALL
-do
+  for f in $BLACKLSTALL
+  do
 
-BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
+  BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
 
-## Amount of sources greater than one?
-timestamp=$(echo `date`)
-HOWMANYLINES=$(echo -e "`wc -l $f | cut -d " " -f 1`")
-if
-[[ "$HOWMANYLINES" -gt 1 ]]
-then
-echo "* $BASEFILENAME Has $HOWMANYLINES sources. $timestamp" | tee --append $MORETHANONESOURCE &>/dev/null
+  ## Amount of sources greater than one?
+  timestamp=$(echo `date`)
+  HOWMANYLINES=$(echo -e "`wc -l $f | cut -d " " -f 1`")
+  if [[ "$HOWMANYLINES" -gt 1 ]]
+  then
+    echo "* $BASEFILENAME Has $HOWMANYLINES sources. $timestamp" | tee --append $MORETHANONESOURCE &>/dev/null
+  fi
+
+  done
+else
+  printf "$green"   "No Lists to Process."
+  echo "No Lists to Process." | tee --append $RECENTRUN &>/dev/null
 fi
 
-done
-else
-printf "$green"   "No Lists to Process."
-echo "No Lists to Process." | tee --append $RECENTRUN &>/dev/null
-fi
-
-if 
-[[ -f $MORETHANONESOURCE ]]
+if  [[ -f $MORETHANONESOURCE ]]
 then
-printf "$yellow"   "Multi-Source List Recreated."
-echo "* Multi-Source List Recreated" | tee --append $RECENTRUN &>/dev/null
-HOWMANYLISTSWITHMULTSOURCE=$(echo -e "`wc -l $MORETHANONESOURCE | cut -d " " -f 1`")
-HOWMANYLISTSWITHMULTSOURCEB=$(expr $HOWMANYLISTSWITHMULTSOURCE - 1)
-echo ""
-printf "$yellow"    "$HOWMANYLISTSWITHMULTSOURCEB Lists With More Than One Source. See Log For Details."
-echo "$HOWMANYLISTSWITHMULTSOURCEB Lists With More Than One Source." | tee --append $RECENTRUN &>/dev/null
-echo "_________________________________________" | tee --append $RECENTRUN &>/dev/null
-cat $MORETHANONESOURCE >> $RECENTRUN
-rm $MORETHANONESOURCE
+  printf "$yellow"   "Multi-Source List Recreated."
+  echo "* Multi-Source List Recreated" | tee --append $RECENTRUN &>/dev/null
+  HOWMANYLISTSWITHMULTSOURCE=$(echo -e "`wc -l $MORETHANONESOURCE | cut -d " " -f 1`")
+  HOWMANYLISTSWITHMULTSOURCEB=$(expr $HOWMANYLISTSWITHMULTSOURCE - 1)
+  echo ""
+  printf "$yellow"    "$HOWMANYLISTSWITHMULTSOURCEB Lists With More Than One Source. See Log For Details."
+  echo "$HOWMANYLISTSWITHMULTSOURCEB Lists With More Than One Source." | tee --append $RECENTRUN &>/dev/null
+  echo "_________________________________________" | tee --append $RECENTRUN &>/dev/null
+  cat $MORETHANONESOURCE >> $RECENTRUN
+  rm $MORETHANONESOURCE
 else
-echo "_________________________________________" | tee --append $RECENTRUN &>/dev/null
-printf "$green"   "All Lists Only Have One Source."
-echo "All Lists Only Have One Source." | tee --append $RECENTRUN &>/dev/null
+  echo "_________________________________________" | tee --append $RECENTRUN &>/dev/null
+  printf "$green"   "All Lists Only Have One Source."
+  echo "All Lists Only Have One Source." | tee --append $RECENTRUN &>/dev/null
 fi
