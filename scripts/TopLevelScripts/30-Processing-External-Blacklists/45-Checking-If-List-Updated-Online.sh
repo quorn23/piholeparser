431b41d5e30c5391463d873586ee5135415d34d0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-18 13:19:48 -0400

    Update 45-Checking-If-List-Updated-Online.sh

--- scripts/TopLevelScripts/30-Processing-External-Blacklists/45-Checking-If-List-Updated-Online.sh
+++ scripts/TopLevelScripts/30-Processing-External-Blacklists/45-Checking-If-List-Updated-Online.sh
@@ -5,64 +5,55 @@
 SCRIPTDIRA=$(dirname $0)
 source "$SCRIPTDIRA"/foldervars.var
 
-if 
-[[ -f $MIRROREDFILE && -z $PINGTESTFAILED ]]
+if  [[ -f $MIRROREDFILE && -z $PINGTESTFAILED ]]
 then
-GOAHEADANDTEST=true
-elif
-[[ ! -f $MIRROREDFILE && -z $PINGTESTFAILED ]]
+  GOAHEADANDTEST=true
+elif [[ ! -f $MIRROREDFILE && -z $PINGTESTFAILED ]]
 then
-printf "$red"    "No Mirror File To Compare With, Proceeding with Download."
+  printf "$red"    "No Mirror File To Compare With, Proceeding with Download."
 fi
 
 ## Check if file is modified since last download
-if 
-[[ -n $GOAHEADANDTEST ]]
+if  [[ -n $GOAHEADANDTEST ]]
 then
-MIRROREDFILESIZE=$(stat -c%s "$MIRROREDFILE")
-CURLFILESIZE=$(curl -s -H "$AGENTDOWNLOAD" $source | wc -c)
+  MIRROREDFILESIZE=$(stat -c%s "$MIRROREDFILE")
+  CURLFILESIZE=$(curl -s -H "$AGENTDOWNLOAD" $source | wc -c)
 fi
 
-if
-[[ -n $GOAHEADANDTEST && `curl -s -H "$AGENTDOWNLOAD" -z "$MIRROREDFILE" -o /dev/null -I -w "%{http_code}" "$source" | grep '304'` ]]
+if [[ -n $GOAHEADANDTEST && `curl -s -H "$AGENTDOWNLOAD" -z "$MIRROREDFILE" -o /dev/null -I -w "%{http_code}" "$source" | grep '304'` ]]
 then
-printf "$green"    "Source Date Not Newer."
-TESTCURLDATESAME=true
+  printf "$green"    "Source Date Not Newer."
+  TESTCURLDATESAME=true
 else
-printf "$red"    "Source Date Is Newer."
+  printf "$red"    "Source Date Is Newer."
 fi
 
-if
-[[ -n $GOAHEADANDTEST && $MIRROREDFILESIZE -ge $CURLFILESIZE ]]
+if [[ -n $GOAHEADANDTEST && $MIRROREDFILESIZE -ge $CURLFILESIZE ]]
 then
-printf "$green"    "File Size Is The Same."
-TESTCURLSIZESAME=true
-elif
-[[ -n $GOAHEADANDTEST && $MIRROREDFILESIZE != $CURLFILESIZE ]]
+  printf "$green"    "File Size Is The Same."
+  TESTCURLSIZESAME=true
+elif [[ -n $GOAHEADANDTEST && $MIRROREDFILESIZE != $CURLFILESIZE ]]
 then
-printf "$red"    "File Size Is Different."
+  printf "$red"    "File Size Is Different."
 fi
 
-if
-[[ -n $TESTCURLSIZESAME || -n $TESTCURLDATESAME ]]
+if [[ -n $TESTCURLSIZESAME || -n $TESTCURLDATESAME ]]
 then
-SKIPDOWNLOAD=true
+  SKIPDOWNLOAD=true
 fi
 
 echo ""
-if
-[[ -n $GOAHEADANDTEST && -n $SKIPDOWNLOAD ]]
+if [[ -n $GOAHEADANDTEST && -n $SKIPDOWNLOAD ]]
 then
-printf "$green"    "File Not Updated Online. No Need To Download."
-echo "SKIPDOWNLOAD="$SKIPDOWNLOAD"" | tee --append $TEMPPARSEVARS &>/dev/null
+  printf "$green"    "File Not Updated Online. No Need To Download."
+  echo "SKIPDOWNLOAD="$SKIPDOWNLOAD"" | tee --append $TEMPPARSEVARS &>/dev/null
 else
-printf "$yellow"    "File Has Changed Online."
+  printf "$yellow"    "File Has Changed Online."
 fi
 
-if
-[[ -f $MIRROREDFILE && -f $PARSEDFILE && -n $SKIPDOWNLOAD ]]
+if [[ -f $MIRROREDFILE && -f $PARSEDFILE && -n $SKIPDOWNLOAD ]]
 then
-printf "$green"    "Since Parsed File is Present, There Is No Need To Process."
-FULLSKIPPARSING=true
-echo "FULLSKIPPARSING="$FULLSKIPPARSING"" | tee --append $TEMPPARSEVARS &>/dev/null
+  printf "$green"    "Since Parsed File is Present, There Is No Need To Process."
+  FULLSKIPPARSING=true
+  echo "FULLSKIPPARSING="$FULLSKIPPARSING"" | tee --append $TEMPPARSEVARS &>/dev/null
 fi
