b652ed70113f7053e342c81ab8e1c3b9a1106b66
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-19 12:11:07 -0400

    Update whitelist-test.sh

--- scripts/tools/whitelist-test.sh
+++ scripts/tools/whitelist-test.sh
@@ -134,18 +134,6 @@ then
   rm $FILETEMP
   echo ""
   
-  echo "Using Method fc"
-  fc $COMBINEDBLACKLISTS $COMBINEDWHITELISTS > $FILETEMP
-  METHODHOWMANYLINES=$(echo -e "`wc -l $FILETEMP | cut -d " " -f 1`")
-  echo "new file is $METHODHOWMANYLINES lines"
-  if grep -q $DOMAINTOLOOKFOR "$FILETEMP"
-  then
-    echo "$DOMAINTOLOOKFOR in file."
-  else
-    echo "$DOMAINTOLOOKFOR not in file."
-  fi
-  rm $FILETEMP
-  echo ""
   
   #echo "Using Method loop"
   #cat $COMBINEDBLACKLISTS | while read line1
