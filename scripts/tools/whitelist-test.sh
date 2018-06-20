dc65c4257271821f06b58ed707113099881e9ef3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-20 08:29:46 -0400

    Update whitelist-test.sh

--- scripts/tools/whitelist-test.sh
+++ scripts/tools/whitelist-test.sh
@@ -24,13 +24,13 @@ BLACKHOWMANYLINES=$(echo -e "`wc -l $COMBINEDBLACKLISTS | cut -d " " -f 1`")
 echo "Black file is $BLACKHOWMANYLINES lines"
 WHITEHOWMANYLINES=$(echo -e "`wc -l $COMBINEDWHITELISTS | cut -d " " -f 1`")
 echo "White file is $WHITEHOWMANYLINES lines"
-if grep -Fxq $DOMAINTOLOOKFOR "$COMBINEDBLACKLISTS"
+if grep -q $DOMAINTOLOOKFOR "$COMBINEDBLACKLISTS"
 then
   echo "Found on Big BlackList"
 else
   echo "Not Found on Big BlackList"
 fi
-if grep -Fxq $DOMAINTOLOOKFOR "$COMBINEDWHITELISTS"
+if grep -q $DOMAINTOLOOKFOR "$COMBINEDWHITELISTS"
 then
   echo "Found on Big WhiteList"
 else
@@ -38,7 +38,7 @@ else
 fi
 
 echo " "
-if grep -Fxq $DOMAINTOLOOKFOR "$COMBINEDWHITELISTS" && grep -Fxq $DOMAINTOLOOKFOR "$COMBINEDBLACKLISTS"
+if grep -q $DOMAINTOLOOKFOR "$COMBINEDWHITELISTS" && grep -q $DOMAINTOLOOKFOR "$COMBINEDBLACKLISTS"
 then
   echo "Found on Both Lists"
 
