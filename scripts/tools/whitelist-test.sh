ccf52cd9751de30d36757a996800bc788884e7ee
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-20 08:18:34 -0400

    Update whitelist-test.sh

--- scripts/tools/whitelist-test.sh
+++ scripts/tools/whitelist-test.sh
@@ -83,7 +83,7 @@ then
   echo ""
   
   echo "Using Method diff"
-  diff -a -y --speed-large-files --unchanged-line-format="" --old-line-format="%L" --new-line-format="" $COMBINEDBLACKLISTS $COMBINEDWHITELISTS | grep "<" | sed 's/^<//g'  > $FILETEMP
+  diff --unchanged-line-format="" --old-line-format="%L" --new-line-format="" $COMBINEDBLACKLISTS $COMBINEDWHITELISTS | grep "<" | sed 's/^<//g'  > $FILETEMP
   METHODHOWMANYLINES=$(echo -e "`wc -l $FILETEMP | cut -d " " -f 1`")
   echo "new file is $METHODHOWMANYLINES lines"
   if grep -q $DOMAINTOLOOKFOR "$FILETEMP"
